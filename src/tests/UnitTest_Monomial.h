552ab0542d5f7e9ea72ad3ddfc793eca29e94329
Sebastian Junges <sebastian.junges@rwth-aachen.de>
2013-04-24 10:33:39 +0200

    First unittests...

---
+++ src/tests/UnitTest_Monomial.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <cppunit/extensions/HelperMacros.h>
+
+class UnitTest_Monomial : public CPPUNIT_NS::TestFixture
+{
+    CPPUNIT_TEST_SUITE( UnitTest_Monomial );
+    CPPUNIT_TEST( testMethod );
+    CPPUNIT_TEST( testFailedMethod );
+    CPPUNIT_TEST_SUITE_END( );
+
+public:
+    UnitTest_Monomial( );
+    virtual ~UnitTest_Monomial( );
+    void setUp( );
+    void tearDown( );
+
+private:
+    void testMethod( );
+    void testFailedMethod( );
+};
+
