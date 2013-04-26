f3cce8bb191c0362fa90139cc60cf97804ba9637
Sebastian Junges <sebastian.junges@rwth-aachen.de>
2013-04-26 20:07:29 +0200

    updates in cmake lists and test-logging in the testsuite.

--- src/tests/test.cpp
+++ src/tests/test.cpp
@@ -3,6 +3,9 @@
 #include <cppunit/extensions/TestFactoryRegistry.h>
 #include <cppunit/ui/text/TestRunner.h>
 
+#include "../lib/core/initialize.h"
+#include "../lib/core/logging.h"
+
 int main( int argc, char** argv )
 {
     // Get the top level suite from the registry
@@ -13,6 +16,8 @@ int main( int argc, char** argv )
     // Change the default outputter to a compiler error format outputter
     runner.setOutputter( new CppUnit::CompilerOutputter( &runner.result(), std::cerr ) );
 
+    LOGMSG_DEBUG(getLog("arithmetic.tests"), "Logging up and running");
+    
     // Run the tests. Return 1 if a test didnt pass.
     return !runner.run();
 
