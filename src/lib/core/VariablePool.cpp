48b904aea7e9c873f5b82a48047be07435a3d0b4
Sebastian Junges <sebastian.junges@rwth-aachen.de>
2013-04-30 14:43:04 +0200

    Static instances were not initialized.

--- src/lib/core/VariablePool.cpp
+++ src/lib/core/VariablePool.cpp
@@ -8,13 +8,15 @@
 #include <mutex>
 
 #include "logging.h"
+#include <iostream>
 
 namespace arithmetic
 {
 
 VariablePool::VariablePool()
 {
-    
+    std::cout << "output" << std::endl;
+    LOGMSG_INFO("arithmetic.varpool", "Constructor called");
 }
 VariablePool::VariablePool(const VariablePool& rs) {
     instance  = rs.instance;
@@ -41,4 +43,16 @@ VariablePool& VariablePool::getInstance(  )
     return *VariablePool::instance;
 }
 
+std::shared_ptr< VariablePool > VariablePool::instance;
+    /**
+     * Making sure that creating a new instance is called only once.
+     * Note: According to the new C++11 standard this is not strictly necessary.
+     * However, this is currently only supported in gcc 4.8.1 and newer. Therefore,
+     * we decided to make explicit use of this flag, thereby supporting gcc 4.7 
+     * and some other compilers.
+     */
+std::once_flag VariablePool::only_one;
+    
+
 }
+
