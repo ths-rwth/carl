ce5cf8a14ffdecf982d24cc6fe54edaab9003175
Sebastian Junges <sebastian.junges@rwth-aachen.de>
2013-04-30 17:12:03 +0200

    Monomial constructors..

--- src/lib/core/Monomial.h
+++ src/lib/core/Monomial.h
@@ -13,18 +13,43 @@ namespace arithmetic
     
     struct VarExpPair
     {
+        VarExpPair(variable v, exponent e) :
+            var(v),
+            exp(e)
+        {}
+        
         variable var;
         exponent exp;
     };
     /**
      *  The general-purpose multivariate polynomial class.
      */
-    template<typename Coeff>
+    template<typename Coefficient>
     class Monomial
     {
         protected:
-            Coeff mCoefficient;
+            /// The coefficient of the monomial..
+            Coefficient mCoefficient;
+            /// A vector of variable-exponent vars (v_i^e_i) with nonzero exponents. 
             std::vector<VarExpPair> mExponents;
-            
+        public:
+            /**
+             */
+            Monomial();
+            /**
+             * Generate a constant
+             * @param c The value of the constant.
+             */
+            Monomial(const Coefficient& c);
+            /**
+             * A monomial over one variable.
+             * @param c The coefficient for the monomial.
+             * @param v The variable
+             * @param e The exponent of this variable
+             */
+            Monomial(const Coefficient& c, variable v, exponent e=1);
+                    
     };
 }
+
+#include "Monomial.tpp"
---
+++ src/lib/core/Monomial.tpp
@@ -0,0 +1,37 @@
+/**
+ * @file Monomial.tpp
+ * @author Sebastian Junges
+ */
+
+#pragma once
+
+// Include for code assistance.
+#include "Monomial.h"
+
+namespace arithmetic
+{
+template <typename Coefficient>
+Monomial<Coefficient>::Monomial() :
+    mCoefficient(), 
+    mExponents()
+{
+    
+}
+
+template <typename Coefficient>
+Monomial<Coefficient>::Monomial(const Coefficient& coeff) :
+    mCoefficient(coeff), 
+    mExponents()
+{
+    
+}
+
+template <typename Coefficient>
+Monomial<Coefficient>::Monomial(const Coefficient& coeff, variable v, exponent e) :
+    mCoefficient(coeff), 
+    mExponents(1, VarExpPair(v,e))
+{
+    
+}
+
+}
