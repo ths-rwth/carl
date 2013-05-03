431663247072c1fe184486676f6de1c2731dde28
Sebastian Junges <sebastian.junges@rwth-aachen.de>
2013-05-04 00:50:03 +0200

    Using googletest (currently only from system!) as testsuite.

--- src/tests/test.cpp
+++ src/tests/test.cpp
@@ -1,24 +1,8 @@
 #include <iostream>
-#include <cppunit/CompilerOutputter.h>
-#include <cppunit/extensions/TestFactoryRegistry.h>
-#include <cppunit/ui/text/TestRunner.h>
 
-#include "../lib/core/initialize.h"
-#include "../lib/core/logging.h"
+#include "gtest/gtest.h"
 
-int main( int argc, char** argv )
+TEST(sample_test_case, sample_test)
 {
-    // Get the top level suite from the registry
-    CppUnit::Test*              test_all = CppUnit::TestFactoryRegistry::getRegistry().makeTest();
-    CppUnit::TextUi::TestRunner runner;
-    runner.addTest( test_all );
-
-    // Change the default outputter to a compiler error format outputter
-    runner.setOutputter( new CppUnit::CompilerOutputter( &runner.result(), std::cerr ) );
-
-    LOGMSG_DEBUG(getLog("arithmetic.tests"), "Logging up and running");
-    
-    // Run the tests. Return 1 if a test didnt pass.
-    return !runner.run();
-
+    EXPECT_EQ(1, 1);
 }
