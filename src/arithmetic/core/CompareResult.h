c4879510656703622c2b5835e09d3167a5df10eb
Sebastian Junges <sebastian.junges@rwth-aachen.de>
2013-05-04 01:55:17 +0200

    Updates in (proposed) interface for monomial

---
+++ src/arithmetic/core/CompareResult.h
@@ -0,0 +1,8 @@
+/**
+ * @file CompareResult.h 
+ * @author Sebastian Junges
+ */
+
+#pragma once
+
+enum class CompareResult : int { LESS = -1, EQUAL = 0, GREATER = 1 };
--- src/arithmetic/core/Monomial.h
+++ src/arithmetic/core/Monomial.h
@@ -5,6 +5,7 @@
 
 #pragma once
 #include "Variable.h"
+#include "CompareResult.h"
 #include "VarExpPair.h"
 
 namespace arithmetic
@@ -25,6 +26,9 @@ namespace arithmetic
             /// Some applications performance depends on getting the degree of monomials very fast
             exponent mTotalDegree;
         public:
+            //
+            // Constructors
+            //
             /**
              */
             Monomial();
@@ -32,7 +36,13 @@ namespace arithmetic
              * Generate a constant
              * @param c The value of the constant.
              */
-            Monomial(const Coefficient& c);
+            explicit Monomial(const Coefficient& c);
+            /**
+             * Generate a monomial from a variable, with constant 1 (one).
+             * @param v The variable.
+             * @param e The exponent.
+             */
+            explicit Monomial(Variable v, exponent e=1);
             /**
              * A monomial over one variable.
              * @param c The coefficient for the monomial.
@@ -47,14 +57,56 @@ namespace arithmetic
              */
             Monomial(const Coefficient& c, const VarExpPair& varexp);
             
-            /*
-             * Basic operations.
-             */
+            const Coefficiet& coeff() const;
+            exponent tdeg() const;
+            
+            bool isConstant() const;
+            bool isLinear() const;
+            unsigned nrVariables() const;
             
             
-            /**
-             *
-             */
+            ///////////////////////////
+            // Orderings
+            ///////////////////////////
+            
+            static CompareResult compareLexical();
+            static CompareResult compareGradedLexical();
+            static CompareResult compareGradedReverseLexical();
+            
+            ///////////////////////////
+            // Operators
+            ///////////////////////////
+            
+            // Compare operators
+            template<typename C1, typename C2>
+            friend operator==(const Monomial<C1>& lhs, const Monomial<C2>& rhs);
+            template<typename C>
+            friend operator==(const Monomial<C>& lhs, const C& rhs);
+            template<typename C>
+            friend operator==(const C& lhs, const Monomial& rhs);
+            template<typename C>
+            friend operator==(const Monomial<C>& lhs, const Variable& rhs);
+            template<typename C>
+            friend operator==(const Variable& lhs, const Monomial& rhs);
+            
+            template<typename C1, typename C2>
+            friend operator!=(const Monomial<C1>& lhs, const Monomial<C2>& rhs);
+            template<typename C>
+            friend operator!=(const Monomial<C>& lhs, const C& rhs);
+            template<typename C>
+            friend operator!=(const C& lhs, const Monomial& rhs);
+            template<typename C>
+            friend operator!=(const Monomial<C>& lhs, const Variable& rhs);
+            template<typename C>
+            friend operator!=(const Variable& lhs, const Monomial& rhs);
+            
+            // For assignment, we provide constructors.
+            
+            // Addition and substraction make only very limited sense, we do not support that.            
+            // Multiplication and dividing
+            
+            
+            // Output
             template <typename C>
             friend std::ostream& operator <<( std::ostream& os, const Monomial<C>& rhs );
     };
