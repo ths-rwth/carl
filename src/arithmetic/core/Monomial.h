a28404bc263282c02e290708e0c7d8810e49efac
Sebastian Junges <sebastian.junges@rwth-aachen.de>
2013-05-04 16:26:32 +0200

    Several operations on monomials

--- src/arithmetic/core/Monomial.h
+++ src/arithmetic/core/Monomial.h
@@ -4,6 +4,8 @@
  */
 
 #pragma once
+#include <algorithm>
+
 #include "Variable.h"
 #include "CompareResult.h"
 #include "VarExpPair.h"
@@ -18,97 +20,158 @@ namespace arithmetic
     template<typename Coefficient>
     class Monomial
     {
-        protected:
-            /// The coefficient of the monomial..
-            Coefficient mCoefficient;
-            /// A vector of variable-exponent vars (v_i^e_i) with nonzero exponents. 
-            std::vector<VarExpPair> mExponents;
-            /// Some applications performance depends on getting the degree of monomials very fast
-            exponent mTotalDegree;
-        public:
-            //
-            // Constructors
-            //
-            /**
-             */
-            Monomial();
-            /**
-             * Generate a constant
-             * @param c The value of the constant.
-             */
-            explicit Monomial(const Coefficient& c);
-            /**
-             * Generate a monomial from a variable, with constant 1 (one).
-             * @param v The variable.
-             * @param e The exponent.
-             */
-            explicit Monomial(Variable v, exponent e=1);
-            /**
-             * A monomial over one variable.
-             * @param c The coefficient for the monomial.
-             * @param v The variable
-             * @param e The exponent of this variable
-             */
-            Monomial(const Coefficient& c, Variable v, exponent e=1);
-            /**
-             * 
-             * @param c
-             * @param varexp
-             */
-            Monomial(const Coefficient& c, const VarExpPair& varexp);
-            
-            const Coefficiet& coeff() const;
-            exponent tdeg() const;
-            
-            bool isConstant() const;
-            bool isLinear() const;
-            unsigned nrVariables() const;
-            
-            
-            ///////////////////////////
-            // Orderings
-            ///////////////////////////
-            
-            static CompareResult compareLexical();
-            static CompareResult compareGradedLexical();
-            static CompareResult compareGradedReverseLexical();
-            
-            ///////////////////////////
-            // Operators
-            ///////////////////////////
-            
-            // Compare operators
-            template<typename C1, typename C2>
-            friend operator==(const Monomial<C1>& lhs, const Monomial<C2>& rhs);
-            template<typename C>
-            friend operator==(const Monomial<C>& lhs, const C& rhs);
-            template<typename C>
-            friend operator==(const C& lhs, const Monomial& rhs);
-            template<typename C>
-            friend operator==(const Monomial<C>& lhs, const Variable& rhs);
-            template<typename C>
-            friend operator==(const Variable& lhs, const Monomial& rhs);
-            
-            template<typename C1, typename C2>
-            friend operator!=(const Monomial<C1>& lhs, const Monomial<C2>& rhs);
-            template<typename C>
-            friend operator!=(const Monomial<C>& lhs, const C& rhs);
-            template<typename C>
-            friend operator!=(const C& lhs, const Monomial& rhs);
-            template<typename C>
-            friend operator!=(const Monomial<C>& lhs, const Variable& rhs);
-            template<typename C>
-            friend operator!=(const Variable& lhs, const Monomial& rhs);
-            
-            // For assignment, we provide constructors.
-            
-            // Addition and substraction make only very limited sense, we do not support that.            
-            // Multiplication and dividing
-            
-            
-            // Output
-            template <typename C>
-            friend std::ostream& operator <<( std::ostream& os, const Monomial<C>& rhs );
+    private:
+        /**
+         * Set if we want to use binary search instead of linear search.
+         */
+        static const bool PREFERBINARYSEARCH = false;
+
+    protected:
+        /// The coefficient of the monomial..
+        Coefficient mCoefficient;
+        /// A vector of variable-exponent vars (v_i^e_i) with nonzero exponents. 
+        std::vector<VarExpPair> mExponents;
+        /// Some applications performance depends on getting the degree of monomials very fast
+        exponent mTotalDegree;
+        
+        typedef std::vector<VarExpPair>::iterator exponents_it;
+        typedef std::vector<VarExpPair>::const_iterator exponents_cIt;
+    public:
+        //
+        // Constructors
+        //
+        /**
+         */
+        Monomial();
+        /**
+         * Generate a constant
+         * @param c The value of the constant.
+         */
+        explicit Monomial(const Coefficient& c);
+        /**
+         * Generate a monomial from a variable, with constant 1 (one).
+         * @param v The variable.
+         * @param e The exponent.
+         */
+        explicit Monomial(Variable v, exponent e=1);
+        /**
+         * A monomial over one variable.
+         * @param c The coefficient for the monomial.
+         * @param v The variable
+         * @param e The exponent of this variable
+         */
+        Monomial(const Coefficient& c, Variable v, exponent e=1);
+        /**
+         * 
+         * @param c
+         * @param varexp
+         */
+        Monomial(const Coefficient& c, const VarExpPair& varexp);
+
+        /**
+         * Gives the coefficient, i.e. the constant factor.
+         * @return 
+         */
+        const Coefficient& coeff() const
+        {
+            return mCoefficient;
+        }
+        /**
+         * Gives the total degree, i.e. the sum of all exponents.
+         * @return 
+         */
+        exponent tdeg() const
+        {
+            return mTotalDegree;
+        }
+        /**
+         * Checks whether the monomial is a constant.
+         * @return 
+         */
+        bool isConstant() const
+        {
+            return mTotalDegree == 0;
+        }
+        /**
+         * Checks whether the monomial has exactly the degree one.
+         * @return 
+         */
+        bool isLinear() const
+        {
+            return mTotalDegree == 1;
+        }
+        bool isAtMostLinear() const
+        {
+            return mTotalDegree <= 1;
+        }
+        size_t nrVariables() const
+        {
+            return mExponents.size();
+        }
+
+        /**
+         * TODO: write code if binary search is preferred.
+         * @param v
+         * @return 
+         */
+        exponent exponentOfVariable(Variable::Arg v) const
+        {
+            // Linear implementation, as we expect very small monomials.
+            exponents_cIt it;
+            if((it = std::find(mExponents.cbegin(), mExponents.cend(), v)) == mExponents.cend())
+            {
+                return 0;
+            }
+            else
+            {
+                return it->exp;
+            }
+        }
+
+
+        ///////////////////////////
+        // Orderings
+        ///////////////////////////
+
+        static CompareResult compareLexical();
+        static CompareResult compareGradedLexical();
+        static CompareResult compareGradedReverseLexical();
+
+        ///////////////////////////
+        // Operators
+        ///////////////////////////
+
+        // Compare operators
+        template<typename C1, typename C2>
+        friend bool operator==(const Monomial<C1>& lhs, const Monomial<C2>& rhs);
+        template<typename C>
+        friend bool operator==(const Monomial<C>& lhs, const C& rhs);
+        template<typename C>
+        friend bool operator==(const C& lhs, const Monomial& rhs);
+        template<typename C>
+        friend bool operator==(const Monomial<C>& lhs, const Variable& rhs);
+        template<typename C>
+        friend bool operator==(const Variable& lhs, const Monomial& rhs);
+
+        template<typename C1, typename C2>
+        friend bool operator!=(const Monomial<C1>& lhs, const Monomial<C2>& rhs);
+        template<typename C>
+        friend bool operator!=(const Monomial<C>& lhs, const C& rhs);
+        template<typename C>
+        friend bool operator!=(const C& lhs, const Monomial& rhs);
+        template<typename C>
+        friend bool operator!=(const Monomial<C>& lhs, const Variable& rhs);
+        template<typename C>
+        friend bool operator!=(const Variable& lhs, const Monomial& rhs);
+
+
+        // Addition and substraction make only very limited sense, we do not support that.            
+        // Multiplication and dividing
+        Monomial& operator*=(const Variable& rhs);
+        Monomial& operator*=(const Coefficient& rhs);
+        // Output
+        template <typename C>
+        friend std::ostream& operator <<( std::ostream& os, const Monomial<C>& rhs );
     };
 }
 
--- src/arithmetic/core/Monomial.tpp
+++ src/arithmetic/core/Monomial.tpp
@@ -39,7 +39,59 @@ Monomial<Coefficient>::Monomial(const Coefficient& coeff, Variable v, exponent e
     
 }
 
-template<typename C>
+
+
+template <typename Coefficient>
+CompareResult Monomial<Coefficient>::compareGradedLexical()
+{
+    
+}
+
+template <typename Coefficient>
+Monomial<Coefficient>& Monomial<Coefficient>::operator*=(Variable::Arg v)
+{
+    // We use canonical representation for the zero.
+    if(mCoefficient == 0)
+    {
+        return *this;
+    }
+    // Linear, as we expect small monomials.
+    for(exponents_it it = mExponents.begin(); it != mExponents.end(); ++it)
+    {
+        // Variable is present
+        if(*it == v)
+        {
+            ++(it->exp);
+            return *this;
+        }
+        // Variable is not present, we have to insert v.
+        if(*it > v)
+        {
+            mExponents.emplace(it,v);
+        }
+    }
+    // Variable was not inserted, insert at end.
+    mExponents.emplace_back(v);
+    return *this;
+}
+
+template <typename Coefficient>
+Monomial<Coefficient>& Monomial<Coefficient>::operator*=(const Coefficient& rhs)
+{
+    if(rhs == 0)
+    {
+        mCoefficient = 0;
+        mExponents.clear();
+        mTotalDegree = 0;
+    }
+    else
+    {
+        mCoefficient *= rhs;
+    }
+    return *this;
+}
+
+template <typename C>
 std::ostream& operator <<( std::ostream& os, const Monomial<C>& rhs )
 {
     os << rhs.mCoefficient;
