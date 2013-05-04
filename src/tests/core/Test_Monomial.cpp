785c06d25885aad53140d88efed30f4166867840
Sebastian Junges <sebastian.junges@rwth-aachen.de>
2013-05-04 14:34:02 +0200

    Removed cppunit tests..

--- src/tests/core/Test_Monomial.cpp
+++ src/tests/core/Test_Monomial.cpp
@@ -1,1 +1,17 @@
+#include "gtest/gtest.h"
+#include "arithmetic/core/Monomial.h"
+
+using namespace arithmetic;
+
+TEST(Monomial, Constructor)
+{
+    Monomial<int> m1(1);
+    Monomial<unsigned> m2(3);
+    SUCCEED();
+}
+
+TEST(Monomial, Operators)
+{
+    
+}
 
