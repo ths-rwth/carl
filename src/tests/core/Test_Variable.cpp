433d3b8f8e301f4411647dbe0ff3cedcc6c2c88a
Sebastian Junges <sebastian.junges@rwth-aachen.de>
2013-05-04 14:29:23 +0200

    Set up preliminary tests

--- src/tests/core/Test_Variable.cpp
+++ src/tests/core/Test_Variable.cpp
@@ -1,1 +1,21 @@
 
+#include "gtest/gtest.h"
+#include "arithmetic/core/Variable.h"
+
+using namespace arithmetic;
+
+TEST(Variable, Constructor)
+{
+    
+}
+
+TEST(Variable, Operators)
+{
+    Variable v1(1,VT_INT);
+    Variable v2(2,VT_REAL);
+    EXPECT_EQ(VT_INT,v1.getType());
+    EXPECT_EQ(1,v1.getId());
+    EXPECT_EQ(VT_REAL,v2.getType());
+    EXPECT_TRUE(v1 != v2);
+    EXPECT_TRUE(v1 == v1);
+}
