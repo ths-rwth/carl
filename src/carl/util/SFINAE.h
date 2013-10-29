539dc6bd9f612f4b86b646fa27029bbd6517c2ee
Sebastian Junges <sebastian.junges@rwth-aachen.de>
2013-10-29 17:56:07 +0100

    Latest version of sfinae.h

--- src/carl/util/SFINAE.h
+++ src/carl/util/SFINAE.h
@@ -7,33 +7,39 @@
  */
 
 #pragma once
-
-/*
- * Obtained from: http://stackoverflow.com/questions/257288/is-it-possible-to-write-a-c-template-to-check-for-a-functions-existence/264088#264088
- */
-#define HAS_MEM_FUNC(func, name)                                        \
-    template<typename T, typename Sign>                                 \
-    struct name {                                                       \
-        typedef char yes[1];                                            \
-        typedef char no [2];                                            \
-        template <typename U, U> struct type_check;                     \
-        template <typename _1> static yes &chk(type_check<Sign, &_1::func> *); \
-        template <typename   > static no  &chk(...);                    \
-        static bool const value = sizeof(chk<T>(0)) == sizeof(yes);     \
-    }
+#include <type_traits>
 
 
-/*
- * The enable-if idiom.
- */
-template <bool, class T = void> 
-struct enable_if 
-{};
- 
-template <class T> 
-struct enable_if<true, T> 
-{ 
-  typedef T type; 
-};
+namespace carl
+{
+
+// Logical conjunction metafunction
+//template <typename... T>
+//struct All : std::true_type 
+//{ };
+//template <typename Head, typename... Tail>
+//struct All<Head, Tail...> : std::conditional<Head, All<Tail...>, std::false_type> { };
+
+	
+// http://flamingdangerzone.com/cxx11/2012/06/01/almost-static-if.html	
+namespace dtl
+{
+	enum class enabled {};
+}
+	
+template <typename Condition>
+using EnableIf = typename std::enable_if<Condition::value, dtl::enabled>::type;	
+template <typename Condition>
+using DisableIf = typename std::enable_if<!Condition::value, dtl::enabled>::type;	
+
+template<typename> struct Void { typedef void type; };
+
+#define has_method_struct(methodname) \
+template<typename T, typename SFINAE = void> \
+struct has_##methodname : std::false_type {}; \
+template<typename T> \
+struct has_##methodname<T, typename Void<decltype( std::declval<T&>().methodname() )>::type> : std::true_type {}; 
 
 
+has_method_struct(normalize);
+}
