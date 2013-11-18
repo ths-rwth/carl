6001225787809362346131d56455766ab683d075
Sebastian Junges <sebastian.junges@rwth-aachen.de>
2013-11-18 12:05:40 +0100

    logging.cpp with correct flags.

--- src/carl/core/logging.cpp
+++ src/carl/core/logging.cpp
@@ -2,15 +2,17 @@
 
 namespace carl
 {
+#ifdef LOGi2_USE_LOG4CPLUS
+
 void setInitialLogLevel()
 {
 	log4cplus::Logger logger = log4cplus::Logger::getInstance(LOG4CPLUS_TEXT("carl"));
 	// Set minimal loglovel
     logger.setLogLevel(log4cplus::INFO_LOG_LEVEL);
 	DEVLOGGER.setLogLevel(log4cplus::ERROR_LOG_LEVEL);
-	#ifdef LOGi2_USE_LOG4CPLUS
+	log4cplus::Logger::getInstance(LOG4CPLUS_TEXT("carl.core")).setLogLevel(log4cplus::TRACE_LOG_LEVEL);
 	log4cplus::Logger::getInstance(LOG4CPLUS_TEXT("carl.core.hensel")).setLogLevel(log4cplus::TRACE_LOG_LEVEL);
 	log4cplus::Logger::getInstance(LOG4CPLUS_TEXT("carl.gb")).setLogLevel(log4cplus::TRACE_LOG_LEVEL);
-	#endif
 }
+#endif
 }
