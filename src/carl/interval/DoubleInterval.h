bc98b27f83257ad83d2632faf632f22ccd92e0b5
stefan <stefan@pegasos>
2013-11-29 15:09:10 +0100

    Added sin and cos for Intervals.

--- src/carl/interval/DoubleInterval.h
+++ src/carl/interval/DoubleInterval.h
@@ -168,11 +168,6 @@ public:
 	{
             mInterval.set(roundDown(l), mInterval.upper());
 	}
-        
-        void setLeft(const DoubleInterval& _interval)
-        {
-            mInterval.set(_interval.left(), mInterval.upper());
-        }
 
 	/**
 	 * Set new left bound type for the interval.
@@ -200,11 +195,6 @@ public:
 	{
 		mInterval.set(left(), roundUp(r));
 	}
-        
-        void setRight(const DoubleInterval& _interval)
-        {
-            mInterval.set(mInterval.lower(), _interval.right());
-        }
 
 	/**
 	 * Set new right bound type for the interval.
@@ -345,6 +335,18 @@ public:
          * @return 
          */
         DoubleInterval log() const;
+        
+        /**
+         * Computes the sinus of the interval.
+         * @return 
+         */
+        DoubleInterval sin() const;
+        
+        /**
+         * Computes the cosinus of the interval.
+         * @return 
+         */
+        DoubleInterval cos() const;
 
 	/**
 	 * Calculates the diameter of the interval
