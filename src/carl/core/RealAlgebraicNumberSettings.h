02933de4cf5ca24c1f8e0b6cd10c4323cb23e33b
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2013-12-18 10:34:58 +0100

    Removed IsolationStrategy from RealAlgebraicNumberSettings, will be in rootfinder namespace.

--- src/carl/core/RealAlgebraicNumberSettings.h
+++ src/carl/core/RealAlgebraicNumberSettings.h
@@ -22,21 +22,6 @@ enum class RefinementStrategy : unsigned {
 /// Maximum number of refinements in which the sample() value should be computed for splitting. Otherwise the midpoint is taken.
 static const unsigned MAXREFINE = 8;
 
-/// Predefined flags for different real root isolation strategies in RealAlgebraicNumberFactory::realRoots.
-enum class IsolationStrategy : unsigned  {
-	/// Performs just interval splitting by OpenInterval::midpoint.
-	SIMPLE,
-	/// Performs interval splitting by OpenInterval::midpoint. If the midpoint happens to be the root itself, it is stored in RealAlgebraicNumberIR::mValue.
-	GENERIC,
-	/// During the splitting process, the midpoint is checked for being a root first. Then, OpenInterval::sample is checked. If it didn't prove to be a root, it is used to dissect the interval. Otherwise it is stored in RealAlgebraicNumberIR::mValue.
-	BINARYSAMPLE,
-	/// During the splitting process, the midpoint is checked for being a root first. Then, OpenInterval::sample is checked. If it didn't prove to be a root, both the midpoint and the sample point are used to split the interval.
-	TERNARYSAMPLE,
-	/// During the splitting process, the midpoint is checked for being a root first. Then, OpenInterval::sample is checked. If it didn't prove to be a root, both the midpoint and the sample point are used to split the interval.
-	TERNARYNEWTON,
-	DEFAULT = TERNARYSAMPLE
-};
-
 /// Maximum bound of an isolating interval so that the OpenInterval::sample method is used for splitting point selection.
 static const long MAX_FASTSAMPLE_BOUND = SHRT_MAX;
 /// Maximum denominator for the sample search is bounded to the square of the common denominator of the bounds; anything above that value is disregarded and a maybe non-optimal, intermediate value is returned instead
