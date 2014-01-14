18084870b7c78ca2d8dea84b03c049cdb8775919
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2014-01-14 13:02:26 +0100

    Removed ifdefs for clang from RootFinder tests.

--- src/tests/core/Test_RootFinder.cpp
+++ src/tests/core/Test_RootFinder.cpp
@@ -31,28 +31,22 @@ TEST(RootFinder, realRoots)
 		UPolynomial p(x, {(cln::cl_RA)-1, (cln::cl_RA)0, (cln::cl_RA)0, (cln::cl_RA)1});
 		auto roots = carl::rootfinder::realRoots(p);
 		ASSERT_TRUE(roots.size() == 1);
-#ifdef __CLANG
 		ASSERT_TRUE(represents(roots.front(), (cln::cl_RA)1));
-#endif
 	}
 
 	{
 		UMPolynomial p(x, {MPolynomial(-1), MPolynomial(0), MPolynomial(0), MPolynomial(1)});
 		auto roots = carl::rootfinder::realRoots(p);
 		ASSERT_TRUE(roots.size() == 1);
-#ifdef __CLANG
 		ASSERT_TRUE(represents(roots.front(), (cln::cl_RA)1));
-#endif
 	}
 
 	{
 		UMPolynomial p(x, {MPolynomial(-1), MPolynomial(0), MPolynomial(1)});
 		auto roots = carl::rootfinder::realRoots(p);
 		ASSERT_TRUE(roots.size() == 2);
-#ifdef __CLANG
 		ASSERT_TRUE(represents(roots.front(), (cln::cl_RA)-1));
 		ASSERT_TRUE(represents(roots.back(), (cln::cl_RA)1));
-#endif
 	}
 
 	{
@@ -61,9 +55,7 @@ TEST(RootFinder, realRoots)
 		m[y] = new RealAlgebraicNumberNR<cln::cl_RA>(-1);
 		auto roots = carl::rootfinder::realRoots(p, m);
 		ASSERT_TRUE(roots.size() == 2);
-#ifdef __CLANG
 		ASSERT_TRUE(represents(roots.front(), (cln::cl_RA)-1));
 		ASSERT_TRUE(represents(roots.back(), (cln::cl_RA)1));
-#endif
 	}
 }
