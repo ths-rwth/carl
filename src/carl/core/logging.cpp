80d624e4cc98942601fcce4cc53cd501628175a1
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2014-01-15 11:43:00 +0100

    Logger carl.cad has level TRACE now.

--- src/carl/core/logging.cpp
+++ src/carl/core/logging.cpp
@@ -10,6 +10,7 @@ void setInitialLogLevel()
 	// Set minimal loglovel
     logger.setLogLevel(log4cplus::INFO_LOG_LEVEL);
 	DEVLOGGER.setLogLevel(log4cplus::ERROR_LOG_LEVEL);
+	log4cplus::Logger::getInstance(LOG4CPLUS_TEXT("carl.cad")).setLogLevel(log4cplus::TRACE_LOG_LEVEL);
 	log4cplus::Logger::getInstance(LOG4CPLUS_TEXT("carl.core")).setLogLevel(log4cplus::TRACE_LOG_LEVEL);
 	log4cplus::Logger::getInstance(LOG4CPLUS_TEXT("carl.core.hensel")).setLogLevel(log4cplus::TRACE_LOG_LEVEL);
 	log4cplus::Logger::getInstance(LOG4CPLUS_TEXT("carl.gb")).setLogLevel(log4cplus::TRACE_LOG_LEVEL);
