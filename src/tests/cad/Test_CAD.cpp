bcc436ca39be1e894978611f12b5d027c16263d4
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2014-01-22 19:57:47 +0100

    Added more examples from ginacra unit test.

--- src/tests/cad/Test_CAD.cpp
+++ src/tests/cad/Test_CAD.cpp
@@ -19,7 +19,8 @@ protected:
 
 	CADTest() : 
 		x(carl::VariablePool::getInstance().getFreshVariable("x")),
-		y(carl::VariablePool::getInstance().getFreshVariable("y"))
+		y(carl::VariablePool::getInstance().getFreshVariable("y")),
+		z(carl::VariablePool::getInstance().getFreshVariable("z"))
 	{
 		LOGMSG_INFO("carl.core", "Variables " << x << ", " << y);
 	}
@@ -32,19 +33,17 @@ protected:
 		this->p.push_back(new Polynomial({Term<cln::cl_RA>(x), -Term<cln::cl_RA>(y), Term<cln::cl_RA>(1)}));
 		// p[2] = x - y
 		this->p.push_back(new Polynomial({Term<cln::cl_RA>(x), -Term<cln::cl_RA>(y)}));
-
-		// p[0] == 0
-		this->c.push_back(Constraint(p[0]->toUnivariatePolynomial(x), Sign::ZERO, {x,y}));
-		// p[1] == 0
-		this->c.push_back(Constraint(p[1]->toUnivariatePolynomial(x), Sign::ZERO, {x,y}));
-		// p[2] == 0
-		this->c.push_back(Constraint(p[2]->toUnivariatePolynomial(x), Sign::ZERO, {x,y}));
+		// p[3] = x^2 + y^2 + z^2 - 1
+		this->p.push_back(new Polynomial({Term<cln::cl_RA>(x)*x, Term<cln::cl_RA>(y)*y, Term<cln::cl_RA>(z)*z, Term<cln::cl_RA>(-1)}));
+		// p[4] = x^2 + y^2
+		this->p.push_back(new Polynomial({Term<cln::cl_RA>(x)*x, Term<cln::cl_RA>(y)*y}));
+		// p[5] = z^3 - 1/2
+		this->p.push_back(new Polynomial({Term<cln::cl_RA>(z)*z*z, Term<cln::cl_RA>(cln::cl_RA(-1)/2)}));
 	}
 
 	virtual void TearDown() {
 		for (auto pol: this->p) delete pol;
 		this->p.clear();
-		this->c.clear();
 	}
 
 	bool hasNRValue(const carl::RealAlgebraicNumberPtr<cln::cl_RA> n, cln::cl_RA val) {
@@ -67,42 +66,112 @@ protected:
 	}
 
 	carl::CAD<cln::cl_RA> cad;
-	carl::Variable x;
-	carl::Variable y;
+	carl::Variable x, y, z;
 	std::vector<Polynomial*> p;
-	std::vector<Constraint> c;
 };
 
 TEST_F(CADTest, Check1)
 {
 	RealAlgebraicPoint<cln::cl_RA> r;
-	std::vector<Constraint> constraints;
+	std::vector<Constraint> cons;
 
 	this->cad.addPolynomial(this->p[0], {x, y});
 	this->cad.addPolynomial(this->p[1], {x, y});
-	constraints.assign({c[0], c[1]});
+	cons.assign({
+		Constraint(this->p[0]->toUnivariatePolynomial(x), Sign::ZERO, {x,y}),
+		Constraint(this->p[1]->toUnivariatePolynomial(x), Sign::ZERO, {x,y})
+	});
 	this->cad.prepareElimination();
-	ASSERT_TRUE(cad.check(constraints, r));
-	ASSERT_TRUE(c[0].satisfiedBy(r) && c[1].satisfiedBy(r));
+	ASSERT_TRUE(cad.check(cons, r));
+	for (auto c: cons) ASSERT_TRUE(c.satisfiedBy(r));
 	ASSERT_TRUE((hasNRValue(r[0], -1) && hasNRValue(r[1], 0)) || (hasNRValue(r[0], 0) && hasNRValue(r[1], 1)));
-	ASSERT_TRUE(cad.check(constraints, r));
-	ASSERT_TRUE(c[0].satisfiedBy(r) && c[1].satisfiedBy(r));
+	ASSERT_TRUE(cad.check(cons, r));
+	for (auto c: cons) ASSERT_TRUE(c.satisfiedBy(r));
 	ASSERT_TRUE((hasNRValue(r[0], -1) && hasNRValue(r[1], 0)) || (hasNRValue(r[0], 0) && hasNRValue(r[1], 1)));
 }
 
 TEST_F(CADTest, Check2)
 {
 	RealAlgebraicPoint<cln::cl_RA> r;
-	std::vector<Constraint> constraints;
+	std::vector<Constraint> cons;
 	cln::cl_RA half = cln::cl_RA(1) / 2;
 
 	this->cad.addPolynomial(this->p[0], {x, y});
 	this->cad.addPolynomial(this->p[2], {x, y});
 	this->cad.prepareElimination();
-	constraints.assign({c[0], c[2]});
-	ASSERT_TRUE(cad.check(constraints, r));
-	ASSERT_TRUE(c[0].satisfiedBy(r) && c[2].satisfiedBy(r));
+	cons.assign({
+		Constraint(this->p[0]->toUnivariatePolynomial(x), Sign::ZERO, {x,y}),
+		Constraint(this->p[2]->toUnivariatePolynomial(x), Sign::ZERO, {x,y})
+	});
+	ASSERT_TRUE(cad.check(cons, r));
+	for (auto c: cons) ASSERT_TRUE(c.satisfiedBy(r));
 	ASSERT_TRUE((hasSqrtValue(r[0], -half) && hasSqrtValue(r[1], -half)) || (hasSqrtValue(r[0], half) && hasSqrtValue(r[1], half)));
-	ASSERT_TRUE(cad.check(constraints, r));
-	ASSERT_TRUE(c[0].satisfiedBy(r) && c[2].satisfiedBy(r));
+	ASSERT_TRUE(cad.check(cons, r));
+	for (auto c: cons) ASSERT_TRUE(c.satisfiedBy(r));
+}
+
+TEST_F(CADTest, Check3)
+{
+	RealAlgebraicPoint<cln::cl_RA> r;
+	std::vector<Constraint> cons;
+
+	this->cad.addPolynomial(this->p[0], {x, y});
+	this->cad.addPolynomial(this->p[2], {x, y});
+	this->cad.prepareElimination();
+	cons.assign({
+		Constraint(this->p[0]->toUnivariatePolynomial(x), Sign::POSITIVE, {x,y}),
+		Constraint(this->p[2]->toUnivariatePolynomial(x), Sign::NEGATIVE, {x,y})
+	});
+	ASSERT_TRUE(cad.check(cons, r));
+	for (auto c: cons) ASSERT_TRUE(c.satisfiedBy(r));
+}
+
+TEST_F(CADTest, Check4)
+{
+	RealAlgebraicPoint<cln::cl_RA> r;
+	std::vector<Constraint> cons;
+
+	this->cad.addPolynomial(this->p[0], {x, y});
+	this->cad.addPolynomial(this->p[2], {x, y});
+	this->cad.prepareElimination();
+	cons.assign({
+		Constraint(this->p[0]->toUnivariatePolynomial(x), Sign::NEGATIVE, {x,y}),
+		Constraint(this->p[2]->toUnivariatePolynomial(x), Sign::POSITIVE, {x,y})
+	});
+	ASSERT_TRUE(cad.check(cons, r));
+	for (auto c: cons) ASSERT_TRUE(c.satisfiedBy(r));
+}
+
+TEST_F(CADTest, Check5)
+{
+	RealAlgebraicPoint<cln::cl_RA> r;
+	std::vector<Constraint> cons;
+
+	this->cad.addPolynomial(this->p[0], {x, y});
+	this->cad.addPolynomial(this->p[2], {x, y});
+	this->cad.prepareElimination();
+	cons.assign({
+		Constraint(this->p[0]->toUnivariatePolynomial(x), Sign::ZERO, {x,y}),
+		Constraint(this->p[2]->toUnivariatePolynomial(x), Sign::POSITIVE, {x,y})
+	});
+	ASSERT_TRUE(cad.check(cons, r));
+	for (auto c: cons) ASSERT_TRUE(c.satisfiedBy(r));
+}
+
+TEST_F(CADTest, Check6)
+{
+	RealAlgebraicPoint<cln::cl_RA> r;
+	std::vector<Constraint> cons;
+
+	this->cad.addPolynomial(this->p[3], {x, y, z});
+	this->cad.addPolynomial(this->p[4], {x, y, z});
+	this->cad.addPolynomial(this->p[5], {x, y, z});
+	this->cad.prepareElimination();
+	cons.assign({
+		Constraint(this->p[3]->toUnivariatePolynomial(x), Sign::NEGATIVE, {x,y,z}),
+		Constraint(this->p[4]->toUnivariatePolynomial(x), Sign::POSITIVE, {x,y,z}),
+		Constraint(this->p[5]->toUnivariatePolynomial(x), Sign::POSITIVE, {x,y,z})
+	});
+	ASSERT_TRUE(cad.check(cons, r));
+	for (auto c: cons) ASSERT_TRUE(c.satisfiedBy(r));
 }
