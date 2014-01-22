4a652d0e3d150ddc6423f99e7784d55a5d3b6b7e
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2014-01-22 11:57:12 +0100

    Added two test cases for CAD.
    First: x^2+y^2=1 and x+1=y
    Second: x^2+y^2=1 and x=y

--- src/tests/cad/Test_CAD.cpp
+++ src/tests/cad/Test_CAD.cpp
@@ -16,7 +16,7 @@ typedef carl::cad::Constraint<cln::cl_RA> Constraint;
 class CADTest : public ::testing::Test {
 protected:
 	typedef carl::CAD<cln::cl_RA>::MPolynomial Polynomial;
-	
+
 	CADTest() : 
 		x(carl::VariablePool::getInstance().getFreshVariable("x")),
 		y(carl::VariablePool::getInstance().getFreshVariable("y"))
@@ -24,34 +24,85 @@ protected:
 		LOGMSG_INFO("carl.core", "Variables " << x << ", " << y);
 	}
 	~CADTest() {}
-	
+
 	virtual void SetUp() {
-		this->p1 = new Polynomial({Term<cln::cl_RA>(x)*x, Term<cln::cl_RA>(y)*y, Term<cln::cl_RA>(-1)});
-		this->p2 = new Polynomial({Term<cln::cl_RA>(x), -Term<cln::cl_RA>(y)});
-		this->cad.clear();
-		this->cad.addPolynomial(this->p1, {x, y});
-		this->cad.addPolynomial(this->p2, {x, y});
-		this->cad.prepareElimination();
+		// p[0] = x^2 + y^2 - 1
+		this->p.push_back(new Polynomial({Term<cln::cl_RA>(x)*x, Term<cln::cl_RA>(y)*y, Term<cln::cl_RA>(-1)}));
+		// p[1] = x + 1 - y
+		this->p.push_back(new Polynomial({Term<cln::cl_RA>(x), -Term<cln::cl_RA>(y), Term<cln::cl_RA>(1)}));
+		// p[2] = x - y
+		this->p.push_back(new Polynomial({Term<cln::cl_RA>(x), -Term<cln::cl_RA>(y)}));
+
+		// p[0] == 0
+		this->c.push_back(Constraint(p[0]->toUnivariatePolynomial(x), Sign::ZERO, {x,y}));
+		// p[1] == 0
+		this->c.push_back(Constraint(p[1]->toUnivariatePolynomial(x), Sign::ZERO, {x,y}));
+		// p[2] == 0
+		this->c.push_back(Constraint(p[2]->toUnivariatePolynomial(x), Sign::ZERO, {x,y}));
 	}
-	
+
 	virtual void TearDown() {
-		delete this->p1;
-		delete this->p2;
+		for (auto pol: this->p) delete pol;
+		this->p.clear();
+		this->c.clear();
 	}
-	
+
+	bool hasNRValue(const carl::RealAlgebraicNumber<cln::cl_RA>* n, cln::cl_RA val) {
+		if (n->isNumeric()) return n->value() == val;
+		return false;
+	}
+	bool hasValue(const carl::RealAlgebraicNumber<cln::cl_RA>* n, cln::cl_RA val) {
+		if (n->isNumeric()) return n->value() == val;
+		else {
+			const carl::RealAlgebraicNumberIR<cln::cl_RA>* ir = static_cast<const carl::RealAlgebraicNumberIR<cln::cl_RA>*>(n);
+			return ir->getInterval().contains(val);
+		}
+	}
+	bool hasSqrtValue(const carl::RealAlgebraicNumber<cln::cl_RA>* n, cln::cl_RA val) {
+		if (n->isNumeric()) return n->value() * n->value() == val;
+		else {
+			const carl::RealAlgebraicNumberIR<cln::cl_RA>* ir = static_cast<const carl::RealAlgebraicNumberIR<cln::cl_RA>*>(n);
+			return (ir->getInterval() * ir->getInterval()).contains(val);
+		}
+	}
+
 	carl::CAD<cln::cl_RA> cad;
 	carl::Variable x;
 	carl::Variable y;
-	Polynomial* p1;
-	Polynomial* p2;
+	std::vector<Polynomial*> p;
+	std::vector<Constraint> c;
 };
 
-TEST_F(CADTest, Check)
+TEST_F(CADTest, Check1)
 {
 	RealAlgebraicPoint<cln::cl_RA> r;
 	std::vector<Constraint> constraints;
-	constraints.push_back(Constraint(p1->toUnivariatePolynomial(x), Sign::ZERO, cad.getVariables()));
-	constraints.push_back(Constraint(p2->toUnivariatePolynomial(x), Sign::ZERO, cad.getVariables()));
+
+	this->cad.addPolynomial(this->p[0], {x, y});
+	this->cad.addPolynomial(this->p[1], {x, y});
+	constraints.assign({c[0], c[1]});
+	this->cad.prepareElimination();
 	ASSERT_TRUE(cad.check(constraints, r));
+	ASSERT_TRUE(c[0].satisfiedBy(r) && c[1].satisfiedBy(r));
+	ASSERT_TRUE((hasNRValue(r[0], -1) && hasNRValue(r[1], 0)) || (hasNRValue(r[0], 0) && hasNRValue(r[1], 1)));
 	ASSERT_TRUE(cad.check(constraints, r));
+	ASSERT_TRUE(c[0].satisfiedBy(r) && c[1].satisfiedBy(r));
+	ASSERT_TRUE((hasNRValue(r[0], -1) && hasNRValue(r[1], 0)) || (hasNRValue(r[0], 0) && hasNRValue(r[1], 1)));
+}
+
+TEST_F(CADTest, Check2)
+{
+	RealAlgebraicPoint<cln::cl_RA> r;
+	std::vector<Constraint> constraints;
+	cln::cl_RA half = cln::cl_RA(1) / 2;
+
+	this->cad.addPolynomial(this->p[0], {x, y});
+	this->cad.addPolynomial(this->p[2], {x, y});
+	this->cad.prepareElimination();
+	constraints.assign({c[0], c[2]});
+	ASSERT_TRUE(cad.check(constraints, r));
+	ASSERT_TRUE(c[0].satisfiedBy(r) && c[2].satisfiedBy(r));
+	ASSERT_TRUE((hasSqrtValue(r[0], -half) && hasSqrtValue(r[1], -half)) || (hasSqrtValue(r[0], half) && hasSqrtValue(r[1], half)));
+	ASSERT_TRUE(cad.check(constraints, r));
+	ASSERT_TRUE(c[0].satisfiedBy(r) && c[2].satisfiedBy(r));
 }
