26ceb38cf837677395ddf6071f2b4dcab29a9fe2
Sebastian Junges <sebastian.junges@rwth-aachen.de>
2014-01-26 14:53:55 +0100

    moved dependencies of other libraries to own files.

---
+++ src/carl/numbers/hash_cln.h
@@ -0,0 +1,24 @@
+/** 
+ * @file   hash_cln.h
+ * @ingroup cln
+ * @author Sebastian Junges
+ * @author  Florian Corzilius
+ *
+ */
+
+#pragma once
+#include <functional>
+#include <cln/cln.h>
+namespace std
+{
+	
+template<>
+struct hash<cln::cl_RA>
+{
+	size_t operator()(const cln::cl_RA& cln_rational) const
+	{
+		return cln::equal_hashcode(cln_rational);
+	}
+};
+
+}
---
+++ src/carl/numbers/hash_gmpxx.h
@@ -0,0 +1,23 @@
+/** 
+ * @file    hash_gmpxx.h
+ * @ingroup gmpxx
+ * @author  Sebastian Junges
+ * @author  Florian Corzilius
+ *
+ */
+
+#pragma once
+#include <functional>
+#include <gmpxx.h>
+
+namespace std
+{
+template<>
+struct hash<mpq_t>
+{
+	size_t operator()(const mpq_t& gmp_rational) const
+	{
+		return mpz_get_ui(mpq_numref(gmp_rational)) ^ mpz_get_ui(mpq_denref(gmp_rational));
+	}
+};
+}
