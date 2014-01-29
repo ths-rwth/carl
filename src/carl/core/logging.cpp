a092f29950059e36130a63c0d7c816b0da1430ae
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2014-01-29 18:09:20 +0100

    Set some sub-loggers to INFO_LEVEL.

--- src/carl/core/logging.cpp
+++ src/carl/core/logging.cpp
@@ -12,6 +12,9 @@ void setInitialLogLevel()
 	DEVLOGGER.setLogLevel(log4cplus::ERROR_LOG_LEVEL);
 	log4cplus::Logger::getInstance(LOG4CPLUS_TEXT("carl.cad")).setLogLevel(log4cplus::TRACE_LOG_LEVEL);
 	log4cplus::Logger::getInstance(LOG4CPLUS_TEXT("carl.core")).setLogLevel(log4cplus::TRACE_LOG_LEVEL);
+	log4cplus::Logger::getInstance(LOG4CPLUS_TEXT("carl.core.rootfinder")).setLogLevel(log4cplus::INFO_LOG_LEVEL);
+	log4cplus::Logger::getInstance(LOG4CPLUS_TEXT("carl.core.evaluation")).setLogLevel(log4cplus::INFO_LOG_LEVEL);
+	log4cplus::Logger::getInstance(LOG4CPLUS_TEXT("carl.core.mvpolynomial")).setLogLevel(log4cplus::INFO_LOG_LEVEL);
 	log4cplus::Logger::getInstance(LOG4CPLUS_TEXT("carl.core.hensel")).setLogLevel(log4cplus::TRACE_LOG_LEVEL);
 	log4cplus::Logger::getInstance(LOG4CPLUS_TEXT("carl.gb")).setLogLevel(log4cplus::TRACE_LOG_LEVEL);
 }
