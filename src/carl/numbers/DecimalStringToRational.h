fc1e76db1f12c340c0af8f7257c78dd1d676083d
Sebastian Junges <sebastian.junges@rwth-aachen.de>
2014-04-24 12:00:40 +0200

    Method to make a rational from a decimal string

---
+++ src/carl/numbers/DecimalStringToRational.h
@@ -0,0 +1,40 @@
+/** 
+ * @file:   DecimalStringToRational.h
+ * @author: Sebastian Junges
+ *
+ * @since April 21, 2014
+ */
+
+#pragma once
+#include <vector>
+#include "boost/algorithm/string.hpp"
+
+#include "operations.h"
+
+namespace carl
+{
+template<class RationalType>
+struct DecimalStringToRational
+{
+	RationalType operator()(const std::string& inputstring) const
+	{
+		std::vector<std::string> strs;
+		boost::split(strs, inputstring, boost::is_any_of(". ,"));
+		
+		if(strs.size() > 2)
+		{
+			throw std::invalid_argument("More than one delimiter in the string.");
+		}
+		RationalType result;
+		if(!strs.front().empty())
+		{
+			result += RationalType(strs.front());
+		}
+		if(strs.size() > 1)
+		{
+			result += (RationalType(strs.back())/carl::pow(typename IntegralT<RationalType>::type(10),strs.back().size()));
+		}
+		return result;
+	}
+};
+}
