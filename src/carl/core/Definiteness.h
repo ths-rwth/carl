7e08d9fb25122b65326d9194cb87150426c95ac0
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2014-08-21 15:32:04 +0200

    Added documentation for Definiteness.

--- src/carl/core/Definiteness.h
+++ src/carl/core/Definiteness.h
@@ -9,7 +9,18 @@
 namespace carl
 {
 	/**
-	 * Regarding a polynomial as a function, its definiteness gives information about the domain. 
+	 * Regarding a polynomial \f$p\f$ as a function \f$p: X \rightarrow Y\f$, its definiteness gives information about the codomain \f$Y\f$.
 	 */
-    enum class Definiteness : unsigned { NEGATIVE = 0, NEGATIVE_SEMI = 1, NON = 2, POSITIVE_SEMI = 3, POSITIVE = 4 };	
+    enum class Definiteness : unsigned {
+		/// Indicates that \f$y < 0 \forall y \in Y\f$.
+		NEGATIVE = 0,
+		/// Indicates that \f$y \leq 0 \forall y \in Y\f$.
+		NEGATIVE_SEMI = 1,
+		/// Indicates that values may be positive and negative.
+		NON = 2,
+		/// Indicates that \f$y \geq 0 \forall y \in Y\f$.
+		POSITIVE_SEMI = 3,
+		/// Indicates that \f$y > 0 \forall y \in Y\f$.
+		POSITIVE = 4
+	};
 }
