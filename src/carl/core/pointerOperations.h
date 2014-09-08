9bb39a1e87ff5497b292ef862b3c5d13fb26dd15
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2014-09-08 16:06:34 +0200

    Added documentation for pointerOperations.

--- src/carl/core/pointerOperations.h
+++ src/carl/core/pointerOperations.h
@@ -10,8 +10,20 @@
 
 #include <functional>
 
+/**
+ * Specialization of std::equal_to for pointer types.
+ *
+ * We consider two pointers equal, if they point to the same memory location or the objects they point to are equal.
+ * Note that the memory location may also be zero.
+ */
 template<typename T>
 struct std::equal_to<T*> {
+	/**
+	 * Checks if two pointers are equal.
+	 * @param lhs First pointer.
+	 * @param rhs Second pointer.
+	 * @return If lhs and rhs are equal.
+	 */
 	bool operator()(const T* lhs, const T* rhs) const {
 		if (lhs == nullptr && rhs == nullptr) return true;
 		if (lhs == nullptr || rhs == nullptr) return false;
@@ -22,8 +34,20 @@ struct std::equal_to<T*> {
 
 namespace carl {
 
+/**
+ * An alternative specialization of std::hash for pointer types.
+ *
+ * In case the pointer is not a nullptr, we return the hash of the object it points to.
+ */
 template<typename T>
 struct ptr_hash {
+	/**
+	 * Calculates the hash of a pointer.
+	 * If the pointer is a nullptr, the result is zero.
+	 * Otherwise, the result is the hash of the referenced object.
+	 * @param t Pointer.
+	 * @return Hash of t.
+	 */
 	std::size_t operator()(const T* t) const {
 		if (t == nullptr) return 0;
 		return std::hash<T>()(*t);
