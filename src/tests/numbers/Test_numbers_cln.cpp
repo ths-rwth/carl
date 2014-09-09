9c002eb2381f154a5ae93db780c9623399fa0802
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2014-09-09 15:30:21 +0200

    Added another test case for sqrt_fast.

--- src/tests/numbers/Test_numbers_cln.cpp
+++ src/tests/numbers/Test_numbers_cln.cpp
@@ -27,8 +27,6 @@ TEST(NumbersCLN, squareroot)
 	cln::cl_RA b = cln::cl_I(2)/cln::cl_I(3);
 	std::pair<cln::cl_RA, cln::cl_RA> resultB;
 	resultB = carl::sqrt(b);
-	resultB.first.debug_print();
-	resultB.second.debug_print();
 }
 
 TEST(NumbersCLN, quotient)
@@ -76,7 +74,14 @@ TEST(NumbersCLN, quotient)
 
 TEST(NumbersCLN, sqrt_fast)
 {
-	std::pair<cln::cl_RA,cln::cl_RA> s = carl::sqrt_fast(cln::cl_I("448"));
-	EXPECT_EQ(cln::cl_RA(21), s.first);
-	EXPECT_EQ(cln::cl_RA(22), s.second);
+	{
+		std::pair<cln::cl_RA,cln::cl_RA> s = carl::sqrt_fast(cln::cl_I("64"));
+		EXPECT_EQ(cln::cl_RA(8), s.first);
+		EXPECT_EQ(cln::cl_RA(8), s.second);
+	}
+	{
+		std::pair<cln::cl_RA,cln::cl_RA> s = carl::sqrt_fast(cln::cl_I("448"));
+		EXPECT_EQ(cln::cl_RA(21), s.first);
+		EXPECT_EQ(cln::cl_RA(22), s.second);
+	}
 }
