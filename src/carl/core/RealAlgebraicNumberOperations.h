ee5c96b7d919b40cf3aa8c084aeeecfa3e39d337
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2014-09-25 14:44:12 +0200

    Added documentation.

--- src/carl/core/RealAlgebraicNumberOperations.h
+++ src/carl/core/RealAlgebraicNumberOperations.h
@@ -14,12 +14,15 @@ namespace std {
 
 /**
  * Specialization of `std::equal_to` for RealAlgebraicNumberPtr.
- * @param lhs First number.
- * @param rhs Second number.
- * @return `lhs == rhs`.
  */
 template<typename Number>
 struct equal_to<carl::RealAlgebraicNumberPtr<Number>> {
+	/**
+	 * Checks lhs and rhs for equality.
+	 * @param lhs First number.
+	 * @param rhs Second number.
+	 * @return `lhs == rhs`.
+	 */
 	bool operator()(const carl::RealAlgebraicNumberPtr<Number>& lhs, const carl::RealAlgebraicNumberPtr<Number>& rhs) const {
 		if (lhs == rhs) return true;
 		if (lhs->isNumeric()) {
@@ -46,17 +49,37 @@ struct equal_to<carl::RealAlgebraicNumberPtr<Number>> {
 	}
 };
 
+/**
+ * Specialization of `std::equal_to` for RealAlgebraicNumberIRPtr.
+ */
 template<typename Number>
 struct equal_to<carl::RealAlgebraicNumberIRPtr<Number>> {
+	/// Equality operator for RealAlgebraicNumberPtr.
 	equal_to<carl::RealAlgebraicNumberPtr<Number>> eq;
+	/**
+	 * Calls `eq(lhs, rhs)`.
+	 * @param lhs First number.
+	 * @param rhs Second number.
+	 * @return `lhs == rhs`.
+	 */
 	bool operator()(const carl::RealAlgebraicNumberIRPtr<Number>& lhs, const carl::RealAlgebraicNumberIRPtr<Number>& rhs) const {
 		return eq(lhs, rhs);
 	}
 };
 
+/**
+ * Specialization of `std::equal_to` for RealAlgebraicNumberNRPtr.
+ */
 template<typename Number>
 struct equal_to<carl::RealAlgebraicNumberNRPtr<Number>> {
+	/// Equality operator for RealAlgebraicNumberPtr.
 	equal_to<carl::RealAlgebraicNumberPtr<Number>> eq;
+	/**
+	 * Calls `eq(lhs, rhs)`.
+	 * @param lhs First number.
+	 * @param rhs Second number.
+	 * @return `lhs == rhs`.
+	 */
 	bool operator()(const carl::RealAlgebraicNumberNRPtr<Number>& lhs, const carl::RealAlgebraicNumberNRPtr<Number>& rhs) const {
 		return eq(lhs, rhs);
 	}
@@ -64,13 +87,17 @@ struct equal_to<carl::RealAlgebraicNumberNRPtr<Number>> {
 
 /**
  * Specialization of `std::not_equal_to` for RealAlgebraicNumberPtr.
- * @param lhs First number.
- * @param rhs Second number.
- * @return `lhs != rhs`.
  */
 template<typename Number>
 struct not_equal_to<carl::RealAlgebraicNumberPtr<Number>> {
+	/// Equality operator for RealAlgebraicNumberPtr.
 	std::equal_to<carl::RealAlgebraicNumberPtr<Number>> eq;
+	/**
+	 * Calls `!eq(lhs, rhs)`.
+	 * @param lhs First number.
+	 * @param rhs Second number.
+	 * @return `lhs != rhs`.
+	 */
 	bool operator()(carl::RealAlgebraicNumberPtr<Number> lhs, carl::RealAlgebraicNumberPtr<Number> rhs) const {
 		return !eq(lhs, rhs);
 	}
@@ -78,13 +105,17 @@ struct not_equal_to<carl::RealAlgebraicNumberPtr<Number>> {
 
 /**
  * Specialization of `std::less` for RealAlgebraicNumberPtr.
- * @param lhs First number.
- * @param rhs Second number.
- * @return `lhs < rhs`.
  */
 template<typename Number>
 struct less<carl::RealAlgebraicNumberPtr<Number>> {
+	/// Equality operator for RealAlgebraicNumberPtr.
 	std::equal_to<carl::RealAlgebraicNumberPtr<Number>> eq;
+	/**
+	 * Checks if one number is smaller than another.
+	 * @param lhs First number.
+	 * @param rhs Second number.
+	 * @return `lhs < rhs`.
+     */
 	bool operator()(carl::RealAlgebraicNumberPtr<Number> lhs, carl::RealAlgebraicNumberPtr<Number> rhs) const {
 		assert(lhs != nullptr);
 		assert(rhs != nullptr);
@@ -122,13 +153,17 @@ struct less<carl::RealAlgebraicNumberPtr<Number>> {
 
 /**
  * Specialization of `std::greater` for RealAlgebraicNumberPtr.
- * @param lhs First number.
- * @param rhs Second number.
- * @return `lhs > rhs`.
  */
 template<typename Number>
 struct greater<carl::RealAlgebraicNumberPtr<Number>> {
+	/// Less operator for RealAlgebraicNumberPtr.
 	std::less<carl::RealAlgebraicNumberPtr<Number>> less;
+	/**
+	 * Calls `less(rhs, lhs)`.
+	 * @param lhs First number.
+	 * @param rhs Second number.
+	 * @return `lhs > rhs`.
+	 */
 	bool operator()(carl::RealAlgebraicNumberPtr<Number> lhs, carl::RealAlgebraicNumberPtr<Number> rhs) const {
 		return less(rhs, lhs);
 	}
