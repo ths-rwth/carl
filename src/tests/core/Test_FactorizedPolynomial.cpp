a5efa517825fd516cce8eca56de86366a8a59726
MazZzinatus <mazzzinatus@gmx.de>
2014-10-08 16:39:20 +0200

    Nicer output

--- src/tests/core/Test_FactorizedPolynomial.cpp
+++ src/tests/core/Test_FactorizedPolynomial.cpp
@@ -29,43 +29,43 @@ TEST(FactorizedPolynomial, Construction)
 
     //Common divisor
     FactorizedPolynomial<P> fpC = commonDivisor( fpA, fpB );
-    std::cout << "Common divisor of " << fpA << " and " << fpB << ": " << fpC << std::endl;
+    std::cout << std::endl << "Common divisor of " << fpA << " and " << fpB << ": " << fpC << std::endl << std::endl;
     fpCache.print();
     
     //GCD
     FactorizedPolynomial<P> restA( P( 2 ), fpCache );
     FactorizedPolynomial<P> restB( P( 2 ), fpCache );
     FactorizedPolynomial<P> fpGCD = gcd( fpA, fpB, restA, restB );
-    std::cout << "GCD of " << fpA << " and " << fpB << ": " << fpGCD << " with rest " << restA << " and " << restB << std::endl;
+    std::cout << std::endl << "GCD of " << fpA << " and " << fpB << ": " << fpGCD << " with rest " << restA << " and " << restB << std::endl << std::endl;
     fpCache.print();
 
     //Common Multiple
     FactorizedPolynomial<P> fpCM = commonMultiple( fpA, fpB );
-    std::cout << "Common multiple of " << fpA << " and " << fpB << ": " << fpCM << std::endl;
+    std::cout << std::endl << "Common multiple of " << fpA << " and " << fpB << ": " << fpCM << std::endl << std::endl;
     fpCache.print();
 
     //Quotient
     FactorizedPolynomial<P> fpQuo = quotient( fpB, fpA );
-    std::cout << "Quotient: " << fpB << " / " << fpA << ": " << fpQuo << std::endl;
+    std::cout << std::endl << "Quotient: " << fpB << " / " << fpA << ": " << fpQuo << std::endl << std::endl;
     fpCache.print();
 
     //LCM
     FactorizedPolynomial<P> fpLCM = lcm( fpA, fpB );
-    std::cout << "LCM of " << fpA << " and " << fpB << ": " << fpLCM << std::endl;
+    std::cout << std::endl << "LCM of " << fpA << " and " << fpB << ": " << fpLCM << std::endl << std::endl;
     fpCache.print();
 
     //Multiplication
     FactorizedPolynomial<P> fpMul = fpA * fpB;
-    std::cout << fpA << " * " << fpB << ": " << fpMul << std::endl;
+    std::cout << std::endl << fpA << " * " << fpB << ": " << fpMul << std::endl << std::endl;
     fpCache.print();
 
     //Addition
     FactorizedPolynomial<P> fpAdd = fpA + fpB;
-    std::cout << fpA << " + " << fpB << ": " << fpAdd << std::endl;
+    std::cout << std::endl << fpA << " + " << fpB << ": " << fpAdd << std::endl << std::endl;
     fpCache.print();
 
     //Subtraction
     FactorizedPolynomial<P> fpSub = fpA - fpB;
-    std::cout << fpA << " - " << fpB << ": " << fpSub << std::endl;
+    std::cout << std::endl << fpA << " - " << fpB << ": " << fpSub << std::endl << std::endl;
     fpCache.print();
 }
