2d054a50308279ccb5b965d17f845399d650f611
Sebastian Junges <sebastian.junges@rwth-aachen.de>
2014-10-08 13:19:16 +0200

    hash constraint did not compile

--- src/carl/core/Constraint.h
+++ src/carl/core/Constraint.h
@@ -14,14 +14,48 @@ template<typename LhsType>
 class Constraint
 {
 	public:
-	Constraint(const LhsType& lhs, CompareRelation rel) : mLhs(lhs), mRelation(rel)
-	{}
+		Constraint(const LhsType& lhs, CompareRelation rel) : mLhs(lhs), mRelation(rel)
+		{}
 	
-	const LhsType& lhs() const {return mLhs;}
-	const CompareRelation& rel() const {return mRelation;}
+		const LhsType& lhs() const {return mLhs;}
+		const CompareRelation& rel() const {return mRelation;}
+		
 	private:
-	LhsType mLhs;
-	CompareRelation mRelation;
+		LhsType mLhs;
+		CompareRelation mRelation;
+};
+
+template<typename LhsT>
+bool operator==(const Constraint<LhsT>& lhs, const Constraint<LhsT>& rhs)
+{
+	return lhs.rel() == rhs.rel() && lhs.lhs() == rhs.lhs();
+}
+
+template<typename LhsT>
+bool operator!=(const Constraint<LhsT>& lhs, const Constraint<LhsT>& rhs)
+{
+	return !(lhs == rhs);
+}
+
+template<typename LhsT>
+std::ostream& operator<<(std::ostream& os, const Constraint<LhsT>& rhs)
+{
+	return os << rhs.lhs() << " " << rhs.rel() << " 0";
+}
+
+}
+
+
+namespace std {
+
+template<typename LhsType>
+struct hash<carl::Constraint<LhsType>> 
+{
+	std::size_t operator()(const carl::Constraint<LhsType>& c) {
+		std::hash<LhsType> h1;
+		std::hash<carl::CompareRelation> h2;
+		return h1(c.lhs()) << 2 | h2(c.rel());
+	}
 };
 
 }
