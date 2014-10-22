40a89b843952a07f0b32231955d75999e8aa87a7
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2014-10-22 18:26:46 +0200

    Ignore the previous commit...

--- src/carl/core/Monomial.h
+++ src/carl/core/Monomial.h
@@ -938,7 +938,7 @@ namespace carl
 				}
 				else
 				{
-					return (lhsit->first < rhsit->first ) ? CompareResult::LESS : CompareResult::GREATER;
+					return (lhsit->first < rhsit->first ) ? CompareResult::GREATER : CompareResult::LESS;
 				}
 				++lhsit;
 				++rhsit;
