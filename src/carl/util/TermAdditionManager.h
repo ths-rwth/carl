5b695724fe70d51d0dc037f607258258879de938
Florian Corzilius <flo@localhost>
2014-10-30 12:54:42 +0100

    commented the code for the manager for the addition of terms

--- src/carl/util/TermAdditionManager.h
+++ src/carl/util/TermAdditionManager.h
@@ -14,28 +14,45 @@
 namespace carl
 {
 
+/**
+ * Class to manage term addition.
+ */
 template<typename Polynomial>
 class TermAdditionManager
 {
     private:
         
+        /// A hash map of shared pointer of monomials to shared pointer of terms. 
         typedef std::unordered_map<std::shared_ptr<const Monomial>,std::shared_ptr<const Term<typename Polynomial::CoeffType>>, std::equal_to<std::shared_ptr<const Monomial>>> MapType;
     
-        ///
+        /// Id of the next free map.
         size_t mNextMapId;
-        ///
+        /** 
+         * If the ith map is used by the polynomial p, this map stores a reference to p at the ith entry of this vector. 
+         * This is only for asserting that no illegal access occurs.
+         */
         std::vector<const Polynomial*> mUsers;
-        ///
+        /// Stores the maps for the detection of equal terms.
         std::vector<MapType> mTermMaps;
-        ///
+        /** 
+         * Stores the leading term of the currently added terms for each map. It points to the end of its corresponding
+         * map, if no leading term has been found (a constant term will not be taken as a leading term).
+         */
         std::vector<typename MapType::iterator> mLeadingTerms;
-        ///
+        /** 
+         * Stores the constant term of the currently added terms for each map. It points to the end of its corresponding
+         * map, if the constant term is currently 0.
+         */
         std::vector<typename MapType::iterator> mConstantTerms;
-        ///
+        /// Mutex for allocation of the map ids.
         std::mutex mMutex;
         
     public:
             
+        /**
+         * Constructs a term addition manager.
+         * @param _numberOfMaps The number of equal-term-detection-maps this manager manages.
+         */
         TermAdditionManager( size_t _numberOfMaps = 1 ):
             mNextMapId( 0 ),
             mUsers( _numberOfMaps, nullptr ),
@@ -44,6 +61,10 @@ class TermAdditionManager
             mConstantTerms( _numberOfMaps, mTermMaps.end() )
         {}
             
+        /**
+         * Set the number of equal-term-detection-maps this manager manages to the given value.
+         * @param _newSize The value to set the number of equal-term-detection-maps this manager manages to.
+         */
         void resize( size_t _newSize )
         {
             assert( !inUse() );
@@ -53,6 +74,12 @@ class TermAdditionManager
             mConstantTerms( _newSize, mTermMaps.end() );
         }
         
+        /**
+         * Reserve a free equal-term-detection-map.
+         * @param _user The polynomial, which wants to reserve a equal-term-detection-map.
+         * @param _expectedSize An upper bound of the expected number of terms, which are going to be added.
+         * @return The id of the reserved equal-term-detection-map.
+         */
         size_t getTermMapId( const Polynomial& _user, size_t _expectedSize )
         {
             std::lock_guard<std::mutex> lock(this->freshVarMutex);
@@ -67,6 +94,12 @@ class TermAdditionManager
             return result;
         }
         
+        /**
+         * Add the given term to the equal-term-detection-map with the given id.
+         * @param _user The polynomial which uses this map.
+         * @param _id The id of the map to add the term to.
+         * @param _term The term to add.
+         */
         void addTerm( const Polynomial& _user, size_t _id, std::shared_ptr<const Term<typename Polynomial::CoeffType>> _term )
         {
             assert( mUsers.at( _id ) == &_user );
@@ -98,6 +131,15 @@ class TermAdditionManager
             }
         }
         
+        /**
+         * Copy the terms in the equal-term-detection-map to the given term vector, while putting the leading term at the end of this vector
+         * and the constant part at the beginning.
+         * @sideeffect The given vector to store the terms into will be cleared an resized before adding terms to it.
+         * @sideeffect The given id is released and the corresponding equal-term-detection-map is cleared.
+         * @param _user The polynomial which uses this map.
+         * @param _id The id of the map to add the term to.
+         * @param _terms The vector of terms containing all terms of equal-term-detection-map with the given id.
+         */
         void readTerms( const Polynomial& _user, size_t _id, std::vector<std::shared_ptr<const Term<typename Polynomial::CoeffType>>>& _terms )
         {
             assert( mUsers.at( _id ) == &_user );
@@ -131,6 +173,9 @@ class TermAdditionManager
         
      private:
          
+        /**
+         * @return true, if any equal-term-detection-map is in use.
+         */
         bool inUse() const
         {
             for( int i = 0; i < mTermMaps.size(); ++i )
