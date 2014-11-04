856563ef64d73935bb064c5adc35f3fd07af7a45
Sebastian Junges <sebastian.junges@rwth-aachen.de>
2014-11-04 22:59:18 +0000

    3 more katsura benchmarks.

--- src/carl/groebner/benchmarks/katsura.h
+++ src/carl/groebner/benchmarks/katsura.h
@@ -32,8 +32,57 @@ std::vector<MultivariatePolynomial<C, O, P>> katsura2()
 template<typename C, typename O, typename P>
 std::vector<MultivariatePolynomial<C, O, P>> katsura3()
 {
-	
+	carl::StringParser sp;
+	sp.setVariables({"x", "y", "z"});
+	std::vector<MultivariatePolynomial<C, O, P>> res;
+	//x + 2*y + 2*z - 1,
+	res.push_back(sp.parseMultivariatePolynomial<C, O, P>("x + 2*y + 2*z + -1"));	
+	//x^2 + 2*y^2 + 2*z^2 - x, 
+	res.push_back(sp.parseMultivariatePolynomial<C, O, P>("x^2 + 2*y^2 + 2*z^2 + -1*x"));
+	//2*x*y + 2*y*z - y
+	res.push_back(sp.parseMultivariatePolynomial<C, O, P>("2*x*y + 2*y*z + -1*y"));
+	return res;
 }
+
+
+template<typename C, typename O, typename P>
+std::vector<MultivariatePolynomial<C, O, P>> katsura4()
+{
+	carl::StringParser sp;
+	sp.setVariables({"x", "y", "z", "t"});
+	std::vector<MultivariatePolynomial<C, O, P>> res;
+	//x + 2*y + 2*z + 2*t - 1, 
+	res.push_back(sp.parseMultivariatePolynomial<C, O, P>("x + 2*y + 2*z + 2*t + -1"));
+	//x^2 + 2*y^2 + 2*z^2 + 2*t^2 - x, 
+	res.push_back(sp.parseMultivariatePolynomial<C, O, P>("x^2 + 2*y^2 + 2*z^2 + 2*t^2 + -1*x"));
+	//2*x*y + 2*y*z + 2*z*t - y, 
+	res.push_back(sp.parseMultivariatePolynomial<C, O, P>("2*x*y + 2*y*z + 2*z*t + -1*y"));
+	//y^2 + 2*x*z + 2*y*t - z
+	res.push_back(sp.parseMultivariatePolynomial<C, O, P>("y^2 + 2*x*z + 2*y*t + -1*z"));
+	return res;
+}
+
+
+
+template<typename C, typename O, typename P>
+std::vector<MultivariatePolynomial<C, O, P>> katsura5()
+{
+	carl::StringParser sp;
+	sp.setVariables({"x", "y", "z", "t", "u"});
+	std::vector<MultivariatePolynomial<C, O, P>> res;
+	//x + 2*y + 2*z + 2*t + 2*u - 1, 
+	res.push_back(sp.parseMultivariatePolynomial<C, O, P>("x + 2*y + 2*z + 2*t + 2*u + -1"));
+	//x^2 + 2*y^2 + 2*z^2 + 2*t^2 + 2*u^2 - x, 
+	res.push_back(sp.parseMultivariatePolynomial<C, O, P>("x^2 + 2*y^2 + 2*z^2 + 2*t^2 + 2*u^2 + -1*x"));
+	//2*x*y + 2*y*z + 2*z*t + 2*t*u - y, 
+	res.push_back(sp.parseMultivariatePolynomial<C, O, P>("2*x*y + 2*y*z + 2*z*t + 2*t*u + -1*y"));
+	//y^2 + 2*x*z + 2*y*t + 2*z*u - z, 
+	res.push_back(sp.parseMultivariatePolynomial<C, O, P>("y^2 + 2*x*z + 2*y*t + 2*z*u + -1*z"));
+	//2*y*z + 2*x*t + 2*y*u - t
+	res.push_back(sp.parseMultivariatePolynomial<C, O, P>("2*y*z + 2*x*t + 2*y*u + -1*t"));
+	return res;
+}
+
 
 
 
@@ -46,6 +95,9 @@ std::vector<MultivariatePolynomial<C, O, P>> katsura(unsigned index)
 	{
 		run_katsura_case(2);
 		run_katsura_case(3);
+		run_katsura_case(4);
+		run_katsura_case(5);
+		//run_katsura_case(6);
 		//run_katsura_case(4);
 	}
 	
