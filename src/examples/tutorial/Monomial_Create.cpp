eae209e1f664c160780587b1e5fb4de7272a9662
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2014-11-26 12:52:02 +0100

    Some work on tutorials.

---
+++ src/examples/tutorial/Monomial_Create.cpp
@@ -0,0 +1,57 @@
+#include <cassert>
+
+#include "carl/core/Monomial.h"
+#include "carl/core/MonomialPool.h"
+
+int main() {
+	carl::VariablePool& pool = carl::VariablePool::getInstance();
+
+	/*
+	 * A carl::Monomial object represents a monomial, being the product of
+	 * multiple variables. Our representation consists of a list of (unique)
+	 * carl::Variable objects, each associated with an exponent.
+	 *
+	 * Internally, a carl::Monomial stores the actual monomial as 
+	 * std::vector<std::pair<carl::Variable,unsigned>>
+	 * and, for caching purposes, the following data:
+	 * - the total degree, that is the sum of all exponents,
+	 * - the id, a unique identifier managed by the carl::MonomialPool and
+	 * - the hash of the monomial.
+	 *
+	 * A Monomial can either be constructed from a carl::Variable and an
+	 * exponent, or from an appropriate std::vector<> or std::initializer_list<>.
+	 */
+	carl::Variable x = pool.getFreshVariable();
+	carl::Variable y = pool.getFreshVariable();
+
+	carl::Monomial a(x, 2);
+	carl::Monomial b({std::make_pair(x,3), std::make_pair(y, 2)});
+
+	/*
+	 * However, carl::Monomial objects are managed by a carl::MonomialPool,
+	 * though the carl::MonomialPool seldomly accessed directly.
+	 * This means, that there is only a single instance of every monomial and
+	 * all objects that use this monomial have a std::shared_ptr of this 
+	 * instance. To obtain this std::shared_ptr, use carl::createMonomial
+	 * instead of the normal constructor.
+	 * The type of the std::shared_ptr is defined by carl::Monomial::Arg.
+	 */
+	
+	carl::Monomial::Arg c = carl::createMonomial(x, (carl::exponent)2);
+	carl::Monomial::Arg d = carl::createMonomial(std::initializer_list<std::pair<carl::Variable, carl::exponent>>({std::make_pair(x,(carl::exponent)3), std::make_pair(y, (carl::exponent)2)}), (carl::exponent)5);
+
+	assert(c == x*x);
+	assert(d == x*x*x * y*y);
+
+	/*
+	 * As you see, the creation of monomials with multiple variables is somewhat
+	 * clumsy, but in the above assertions a simpler way is already used.
+	 * Two variables can be multiplied, resulting in a monomial. Monomials can 
+	 * also be multiplied, resulting in a new monomial.
+	 */
+	carl::Monomial::Arg e = x*x*y;
+	carl::Monomial::Arg f = x*y;
+	carl::Monomial::Arg g = c*d;
+
+	assert(g == d);
+}
---
+++ src/examples/tutorial/Variable_Create.cpp
@@ -0,0 +1,82 @@
+#include <cassert>
+
+#include "carl/core/Variable.h"
+#include "carl/core/VariablePool.h"
+
+int main() {
+	carl::VariablePool& pool = carl::VariablePool::getInstance();
+
+	/*
+	 * A carl::Variable object represents a single variable.
+	 * A variable can have any type from carl::VariableType:
+	 * - Real (VT_REAL)
+	 * - Rational (VT_RATIONAL)
+	 * - Integer (VT_INT)
+	 * - Natural (VT_NATURAL)
+	 * - Boolean (VT_BOOL)
+	 * - Uninterpreted (VT_UNINTERPRETED)
+	 *
+	 * A variable object consists only of a single unsigned variable, which
+	 * encodes the following properties:
+	 * - Rank
+	 * - Id
+	 * - Type
+	 *
+	 * The id is a unique identifier. All variables with the same id are
+	 * considered equal. The id will directly be used for comparison.
+	 * The rank can be used to change the default ordering at runtime, that is
+	 * to impose an ordering that differs from the ordering of the ids.
+	 * The type encodes the variable type.
+	 *
+	 * Usually, variables should be created using the carl::VariablePool.
+	 * The VariablePool makes sure that a new variable gets a fresh consecutive
+	 * ID and stores variable names.
+	 */
+	carl::Variable a = pool.getFreshVariable("x", carl::VariableType::VT_REAL);
+	carl::Variable b = pool.getFreshVariable("y", carl::VariableType::VT_INT);
+
+	assert(a.getType() == carl::VariableType::VT_REAL);
+	assert(pool.getName(a) == "x");
+	assert(b.getType() == carl::VariableType::VT_INT);
+	assert(pool.getName(b) == "y");
+
+	/*
+	 * You can create anonymous variables by omitting the first argument.
+	 * The variables will be printed in any output as "x_<id>" in this case.
+	 * You can also omit the second argument, in this case the type is VT_REAL.
+	 */
+	carl::Variable c = pool.getFreshVariable(carl::VariableType::VT_INT);
+	carl::Variable d = pool.getFreshVariable("d");
+	carl::Variable e = pool.getFreshVariable();
+
+	assert(c.getType() == carl::VariableType::VT_INT);
+	assert(pool.getName(c) == "x_3");
+	assert(pool.getName(d) == "d");
+	assert(e.getType() == carl::VariableType::VT_REAL);
+	assert(pool.getName(e) == "x_5");
+
+	/*
+	 * In short test cases, it may be easier to simply assign ids manually.
+	 * To do this, you can use the carl::Variable constructors directly.
+	 * The default constructor produces a variable of id zero, which is
+	 * considered invalid. To check for such a case, you can compare with the
+	 * staticly stored variable Variable::NO_VARIABLE.
+	 */
+	carl::Variable f;
+	carl::Variable g(1);
+	carl::Variable h(7);
+
+	assert(f == carl::Variable::NO_VARIABLE);
+	assert(g == a);
+	assert(h.getId() == 7);
+
+	/*
+	 * As a variable objects technically consists of a single unsigned, most
+	 * compilers should optimize and avoid the overhead of actually constructing
+	 * objects. In this case, it may make a difference if you have
+	 * - carl::Variable
+	 * - const carl::Variable&
+	 * as a function argument. Use Variable::Arg for function arguments and the
+	 * preprocessor define VARIABLE_PASS_BY_VALUE to change the behavior.
+	 */
+}
