edc9a4d089656ffd3161994a017aaf6d117aafce
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2014-12-17 14:36:36 +0100

    Added interface for diophantine equation solver.

---
+++ src/carl/core/DiophantineEquation.h
@@ -0,0 +1,21 @@
+/**
+ * @file DiophantineEquations.h
+ * @author Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
+ */
+
+#pragma once
+
+#include "../numbers/numbers.h"
+#include "MultivariatePolynomial.h"
+
+namespace carl {
+
+template<typename T>
+std::vector<T> solveDiophantine(const MultivariatePolynomial<T>& p) {
+	static_assert(carl::is_integer<T>::value, "Diophantine equations are build from integral coefficients only!");
+	std::vector<T> res;
+	// Solve here
+	return res;
+}
+
+}
