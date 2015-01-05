d32e328fbe107e62fcc5471ebb139fb8e322e4e0
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2015-01-05 12:32:05 +0100

    Added assertion.

--- src/carl/core/carlLogging.h
+++ src/carl/core/carlLogging.h
@@ -318,6 +318,7 @@ public:
      * @param sink Sink.
      */
 	void configure(const std::string& id, std::shared_ptr<Sink> sink) {
+		assert(this->data.find(id) == this->data.end());
 		this->data[id] = std::make_tuple(sink, Filter(), std::make_shared<Formatter>());
 	}
 	/**
