344330f2e8117c52d64f2830710a5425b29822f6
Tobias Winkler <tobias.winkler1@rwth-aachen.de>
2015-01-13 22:10:36 +0100

    Started implementing algorithm for linear equations

--- src/carl/core/DiophantineEquation.h
+++ src/carl/core/DiophantineEquation.h
@@ -10,16 +10,54 @@
 
 namespace carl {
 
+
+    
 template<typename T>
-std::vector<T> solveDiophantine(const MultivariatePolynomial<T>& p) {
+T gcdOfLinearCoefficients(MultivariatePolynomial<T>& p) {
+    std::vector<carl::Term<T>> terms = p.getTerms();
+    T res = 0;
+    T& currGcd = res;
+    // the first element in terms is the constant part, if there is any
+    int i = p.hasConstantTerm() ? 1 : 0;
+    for(; i < terms.size(); i++) {
+        currGcd = gcd_assign(currGcd, terms[i].coeff()); // warum funktioniert das?
+    }
+    return res;
+} 
+
+/**
+ * Diophantine Equations solver
+ */
+template<typename T>
+std::vector<T> solveDiophantine(MultivariatePolynomial<T>& p) {
 	static_assert(carl::is_integer<T>::value, "Diophantine equations are build from integral coefficients only!");
 	std::vector<T> res;
-	// Solve here
+	
+        // use an algorithm for linear equations only
+        if(p.isLinear()) {
+            if(carl::mod(p.constantPart(), carl::gcdOfLinearCoefficients(p)) != 0) {
+                // in this case no solutions exist
+                // todo: what is the return value if there are no solutions?
+            }
+            else {
+                // find the smalltest coeff
+                int coeffBeginPos = p.hasConstantTerm() ? 1 : 0;
+                std::vector<carl::Term<T>>& terms = p.getTerms();
+                T min = carl::abs(terms[coeffBeginPos].coeff());
+                for(int i = coeffBeginPos + 1; i < terms.size(); i++) {
+                    if(carl::abs(terms[i].coeff()) < min) {
+                        min = terms[i].coeff();
+                    }
+                }
+                
+                
+            }
+        }
+            
 	return res;
 }
 
-cln::cl_I gcdOfLinearCoefficients(MultivariatePolynomial<cln::cl_I>& p) {
-    
-}
+
+
 
 }
