abff24d529dfe8d40d7c5704a4504fff93cfd619
Andreas Krüger <andreas.krueger@rwth-aachen.de>
2015-01-22 10:43:20 +0100

    changed code formatting rules

--- src/carl/formula/bitvector/BVCompareRelation.h
+++ src/carl/formula/bitvector/BVCompareRelation.h
@@ -6,87 +6,94 @@
 #pragma once
 #include <iostream>
 
-namespace carl {
+namespace carl
+{
 
-enum class BVCompareRelation: unsigned
-{
-	EQ,  // =
-	NEQ, // <>
-	ULT, // unsigned <
-	ULE, // unsigned <=
-	UGT, // unsigned >
-	UGE, // unsigned >=
-	SLT, // signed <
-	SLE, // signed <=
-	SGT, // signed >
-	SGE  // signed >=
-};
-	
-inline std::string toString(BVCompareRelation r)
-{
-	switch (r) {
-		case BVCompareRelation::EQ:     return "=";
-		case BVCompareRelation::NEQ:    return "<>";
-		case BVCompareRelation::ULT:    return "bvult";
-		case BVCompareRelation::ULE:    return "bvule";
-		case BVCompareRelation::UGT:    return "bvugt";
-		case BVCompareRelation::UGE:    return "bvuge";
-		case BVCompareRelation::SLT:    return "bvslt";
-		case BVCompareRelation::SLE:    return "bvsle";
-		case BVCompareRelation::SGT:    return "bvsgt";
-		case BVCompareRelation::SGE:    return "bvsge";
+	enum class BVCompareRelation : unsigned
+	{
+		EQ, // =
+		NEQ, // <>
+		ULT, // unsigned <
+		ULE, // unsigned <=
+		UGT, // unsigned >
+		UGE, // unsigned >=
+		SLT, // signed <
+		SLE, // signed <=
+		SGT, // signed >
+		SGE // signed >=
+	};
+
+	inline std::string toString(BVCompareRelation r)
+	{
+		switch(r) {
+		case BVCompareRelation::EQ: return "=";
+		case BVCompareRelation::NEQ: return "<>";
+		case BVCompareRelation::ULT: return "bvult";
+		case BVCompareRelation::ULE: return "bvule";
+		case BVCompareRelation::UGT: return "bvugt";
+		case BVCompareRelation::UGE: return "bvuge";
+		case BVCompareRelation::SLT: return "bvslt";
+		case BVCompareRelation::SLE: return "bvsle";
+		case BVCompareRelation::SGT: return "bvsgt";
+		case BVCompareRelation::SGE: return "bvsge";
+		}
+		assert(false);
+		return "";
 	}
-	assert(false);
-	return "";
-}
 
-inline std::ostream& operator<<(std::ostream& os, const BVCompareRelation& r)
-{
-	return (os << toString(r));
-}
-
-inline BVCompareRelation inverse(BVCompareRelation c)
-{
-	switch (c) {
-		case BVCompareRelation::EQ:     return BVCompareRelation::NEQ;
-		case BVCompareRelation::NEQ:    return BVCompareRelation::EQ;
-		case BVCompareRelation::ULT:    return BVCompareRelation::UGE;
-		case BVCompareRelation::ULE:    return BVCompareRelation::UGT;
-		case BVCompareRelation::UGT:    return BVCompareRelation::ULE;
-		case BVCompareRelation::UGE:    return BVCompareRelation::ULT;
-		case BVCompareRelation::SLT:    return BVCompareRelation::SGE;
-		case BVCompareRelation::SLE:    return BVCompareRelation::SGT;
-		case BVCompareRelation::SGT:    return BVCompareRelation::SLE;
-		case BVCompareRelation::SGE:    return BVCompareRelation::SLT;
+	inline std::ostream& operator<<(std::ostream& os, const BVCompareRelation& r)
+	{
+		return(os << toString(r));
 	}
-	assert(false);
-	return BVCompareRelation::EQ;
-}
 
-inline bool relationIsStrict(BVCompareRelation r) {
-	return r == BVCompareRelation::ULT ||
-	       r == BVCompareRelation::UGT ||
-	       r == BVCompareRelation::SLT ||
-	       r == BVCompareRelation::UGT ||
-	       r == BVCompareRelation::NEQ;
-}
+	inline BVCompareRelation inverse(BVCompareRelation c)
+	{
+		switch(c) {
+		case BVCompareRelation::EQ: return BVCompareRelation::NEQ;
+		case BVCompareRelation::NEQ: return BVCompareRelation::EQ;
+		case BVCompareRelation::ULT: return BVCompareRelation::UGE;
+		case BVCompareRelation::ULE: return BVCompareRelation::UGT;
+		case BVCompareRelation::UGT: return BVCompareRelation::ULE;
+		case BVCompareRelation::UGE: return BVCompareRelation::ULT;
+		case BVCompareRelation::SLT: return BVCompareRelation::SGE;
+		case BVCompareRelation::SLE: return BVCompareRelation::SGT;
+		case BVCompareRelation::SGT: return BVCompareRelation::SLE;
+		case BVCompareRelation::SGE: return BVCompareRelation::SLT;
+		}
+		assert(false);
+		return BVCompareRelation::EQ;
+	}
 
-inline bool relationIsSigned(BVCompareRelation r) {
-	return r == BVCompareRelation::SLT ||
-	       r == BVCompareRelation::SLE ||
-	       r == BVCompareRelation::SGT ||
-	       r == BVCompareRelation::SGE;
-}
-	
+	inline bool relationIsStrict(BVCompareRelation r)
+	{
+		return r == BVCompareRelation::ULT ||
+			r == BVCompareRelation::UGT ||
+			r == BVCompareRelation::SLT ||
+			r == BVCompareRelation::UGT ||
+			r == BVCompareRelation::NEQ;
+	}
+
+	inline bool relationIsSigned(BVCompareRelation r)
+	{
+		return r == BVCompareRelation::SLT ||
+			r == BVCompareRelation::SLE ||
+			r == BVCompareRelation::SGT ||
+			r == BVCompareRelation::SGE;
+	}
+
 } // end namespace carl
 
-namespace std {
+namespace std
+{
 
-template<>
-struct hash<carl::BVCompareRelation> {
-	std::size_t operator()(const carl::BVCompareRelation& rel) const {
-		return std::size_t(rel);
-	}
-};
+	template<>
+	struct hash<carl::BVCompareRelation>
+	{
+
+		std::size_t operator()(const carl::BVCompareRelation& rel) const
+		{
+			return std::size_t(rel);
+		}
+	};
 
 } // end namespace std
--- src/carl/formula/bitvector/BVVariable.h
+++ src/carl/formula/bitvector/BVVariable.h
@@ -10,45 +10,51 @@
 
 namespace carl
 {
-    class BVVariable
-    {
-        private:
-            Variable mVar;
-            size_t mWidth;
-            
-        public:
-            BVVariable(): mVar( Variable::NO_VARIABLE ) {}
 
-            BVVariable( Variable::Arg _variable, size_t _width = 1 ):
-                mVar( _variable ), mWidth( _width ) {}
+	class BVVariable
+	{
+	private:
+		Variable mVar;
+		size_t mWidth;
 
-            Variable operator()() const
-            {
-                return mVar;
-            }
-            
-            size_t width() const
-            {
-                return mWidth;
-            }
+	public:
 
-            /**
-             * @return The string representation of this bit vector variable.
-             */
-            std::string toString( bool _friendlyNames ) const
-            {
-                return VariablePool::getInstance().getName(mVar, _friendlyNames);
-            }
+		BVVariable() : mVar(Variable::NO_VARIABLE)
+		{
+		}
 
-            /**
-             * Prints the given bit vector variable on the given output stream.
-             * @param _os The output stream to print on.
-             * @param _bvvar The bit vector variable to print.
-             * @return The output stream after printing the given bit vector variable on it.
-             */
-            friend std::ostream& operator<<( std::ostream& _os, const BVVariable& _bvvar )
-            {
-                return (_os << _bvvar());
-            }
-    };
+		BVVariable(Variable::Arg _variable, size_t _width = 1) :
+		mVar(_variable), mWidth(_width)
+		{
+		}
+
+		Variable operator()() const
+		{
+			return mVar;
+		}
+
+		size_t width() const
+		{
+			return mWidth;
+		}
+
+		/**
+		 * @return The string representation of this bit vector variable.
+		 */
+		std::string toString(bool _friendlyNames) const
+		{
+			return VariablePool::getInstance().getName(mVar, _friendlyNames);
+		}
+
+		/**
+		 * Prints the given bit vector variable on the given output stream.
+		 * @param _os The output stream to print on.
+		 * @param _bvvar The bit vector variable to print.
+		 * @return The output stream after printing the given bit vector variable on it.
+		 */
+		friend std::ostream& operator<<(std::ostream& _os, const BVVariable& _bvvar)
+		{
+			return(_os << _bvvar());
+		}
+	};
 } // end namespace carl
