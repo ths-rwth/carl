9c7a6e504d9f196ecfbcb0c05e1171f5f0310f3c
Tobias Winkler <tobias.winkler1@rwth-aachen.de>
2015-01-25 19:48:51 +0100

    for testing only

--- src/tests/core/Test_DiophantineEquation.cpp
+++ src/tests/core/Test_DiophantineEquation.cpp
@@ -10,6 +10,7 @@
 #include <list>
 #include "carl/converter/OldGinacConverter.h"
 #include "carl/util/stringparser.h"
+#include "carl/core/DiophantineEquation.h"
 
 
 using namespace carl;
@@ -31,6 +32,59 @@ TEST(DiophantineEquation, test) {
     std::cout<< p.isLinear() << std::endl;
     
     std::cout << p.getTerms() << std::endl;
+    
+    cln::cl_I gcd = gcdOfLinearCoefficients(p);
+    
+    std::cout << gcd << std::endl;
+}
+
+
+TEST(DiophantineEquation, solve) {
+    VariablePool& vpool = VariablePool::getInstance();
+    Variable x = vpool.getFreshVariable();
+    Variable y = vpool.getFreshVariable();
+    Variable z = vpool.getFreshVariable();
+    
+    MultivariatePolynomial<cln::cl_I> p({(cln::cl_I)-4*x, (cln::cl_I)4*y, (cln::cl_I)12*z, (Term<cln::cl_I>)((cln::cl_I)8)});
+    
+}
+
+
+TEST(DiophantineEquation, extended_gcd) {
+    cln::cl_I s;
+    cln::cl_I t;
+    
+    cln::cl_I a = 125400000465466474;
+    cln::cl_I b = -288842288409876994;
+    
+    cln::cl_I gcd;
+    gcd = extended_gcd(a,b,s,t);
+    
+    std::cout << "s = " << s << ", t = " << t << " , gcd = " << gcd << std::endl;
+    
+    EXPECT_EQ(s*a + t*b, gcd);
+}
+
+TEST(DiophantineEquation, solve_linear) {
+    VariablePool& vpool = VariablePool::getInstance();
+    vpool.clear();
+    Variable x = vpool.getFreshVariable();
+    Variable y = vpool.getFreshVariable();
+    Variable z = vpool.getFreshVariable();
+    
+    MultivariatePolynomial<cln::cl_I> p1({(cln::cl_I)6*x, (cln::cl_I)4*y, (cln::cl_I)6*z, (Term<cln::cl_I>)((cln::cl_I)8)});
+    MultivariatePolynomial<cln::cl_I> p2({(cln::cl_I)4*x, (cln::cl_I)2*y, (cln::cl_I)3*z, (Term<cln::cl_I>)((cln::cl_I)7)});
+    
+    std::vector<cln::cl_I> res;
+    res = solveLinearDiophantine(p1);
+    
+    std::cout << p1 << std::endl;
+    std::cout << res << std::endl;
+    
+    res = solveLinearDiophantine(p2);
+    std::cout << p2 << std::endl;
+    std::cout << res << std::endl;
+    
 }
 
 
