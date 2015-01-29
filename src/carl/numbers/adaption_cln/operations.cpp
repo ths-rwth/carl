b885aa462eb825317d21955fa2c520119ace2ab7
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2015-01-29 20:18:36 +0100

    Implemented sqrt_fast in a much faster way that avoids cln::rationalize.

--- src/carl/numbers/adaption_cln/operations.cpp
+++ src/carl/numbers/adaption_cln/operations.cpp
@@ -105,16 +105,15 @@ namespace carl
     
     std::pair<cln::cl_RA, cln::cl_RA> sqrt_fast(const cln::cl_RA& a)
     {
-	assert(a >= 0);
-	cln::cl_R tmp = cln::sqrt(toLF(a));
-	cln::cl_RA root = cln::rationalize(tmp);
-	if(root == tmp) {
-            // root is a cln::cl_RA
-            return std::make_pair(root, root);
-	} else {
-            // root is a cln::cl_LF. In this case, it is not integer and we can assume that the surrounding integers contain the actual root.
-            cln::cl_I lower = carl::floor(root);
-            cln::cl_I upper = carl::ceil(root);
+		assert(a >= 0);
+		cln::cl_RA exact_root;
+		if (cln::sqrtp(a, &exact_root)) {
+			// root can be computed exactly.
+			return std::make_pair(exact_root, exact_root);
+		} else {
+			// compute an approximation with sqrt(). we can assume that the surrounding integers contain the actual root.
+			cln::cl_I lower = cln::floor1(cln::sqrt(toLF(a)));
+            cln::cl_I upper = lower + 1;
             assert(cln::expt_pos(lower,2) < a);
             assert(cln::expt_pos(upper,2) > a);
             return std::make_pair(lower, upper);
