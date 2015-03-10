a4ecd38d7f7c442effc8ee7244726a10cd53d46f
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2015-03-10 15:37:31 +0100

    Removed crap.

--- src/carl/converter/OldGinacConverter.cpp
+++ src/carl/converter/OldGinacConverter.cpp
@@ -27,30 +27,4 @@ bool similar(GiNaC::ex a, GiNaC::ex b) {
     return true;
 }
 
-	bool similar(GiNaC::ex a, GiNaC::ex b) {
-		if (b == 0) return a == 0;
-		GiNaC::ex x = a, y = b;
-		while ((!GiNaC::is_exactly_a<GiNaC::numeric>(b)) && GiNaC::divide(x, b, x));
-		while ((!GiNaC::is_exactly_a<GiNaC::numeric>(a)) && GiNaC::divide(y, a, y));
-		while ((x != 1) && (y != 1)) {
-			if (x == y) return true;
-			if (x == -y) return true;
-			if (GiNaC::is_exactly_a<GiNaC::numeric>(x) && GiNaC::is_exactly_a<GiNaC::numeric>(y)) return true;
-			GiNaC::ex g = GiNaC::gcd(x, y);
-			if (g != 1) {
-				assert(GiNaC::divide(x, g, x));
-				assert(GiNaC::divide(y, g, y));
-				continue;
-			} else {
-				GiNaC::ex gx = GiNaC::gcd(x, b);
-				GiNaC::ex gy = GiNaC::gcd(y, a);
-				if ((gx == 1) && (gy == 1)) return false;
-				assert(GiNaC::divide(x, gx, x));
-				assert(GiNaC::divide(y, gy, y));
-			}
-		}
-		return true;
-	}
-} // end namespace carl
-
 #endif
