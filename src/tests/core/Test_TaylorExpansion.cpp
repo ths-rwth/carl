d7ad9d4a5a242c3051fd96ad847c611f90b02265
Tobias Winkler <tobias.winkler1@rwth-aachen.de>
2015-04-01 14:16:48 +0200

    updated test for taylor expansion

--- src/tests/core/Test_TaylorExpansion.cpp
+++ src/tests/core/Test_TaylorExpansion.cpp
@@ -17,13 +17,13 @@ TEST(TaylorExpansion, ideal_adic_coefficient) {
     GFNumber<cln::cl_I> a3(3, gf5);
     GFNumber<cln::cl_I> a4(4, gf5);
     
+
     VariablePool& vpool = VariablePool::getInstance();
     Variable x_1 = vpool.getFreshVariable("x_1");
     Variable x_2 = vpool.getFreshVariable("x_2");
     
     MultivariatePolynomial<GFNumber<cln::cl_I>> p({a1 * x_1, a2 * x_2 * x_2, a1 * x_1 * x_2});
-    
-    // 
-    std::cout << ideal_adic_coeff(p, x_2, a2, 2);
+     
+    std::cout << TaylorExpansion<cln::cl_I>::ideal_adic_coeff(p, x_2, a1, 2) << std::endl;
     
 }
