41212fc40d6fc339ddcdcdadf9457d63199acbd5
Matthias Volk <Matthias.Volk@rwth-aachen.de>
2015-04-14 15:42:46 +0200

    Use mpir instead of gmp in tests

--- src/tests/interval/Test_Contraction.cpp
+++ src/tests/interval/Test_Contraction.cpp
@@ -4,11 +4,16 @@
 #include "carl/core/MultivariatePolynomial.h"
 #include "carl/interval/IntervalEvaluation.h"
 #include "carl/interval/Contraction.h"
+#include "carl/util/platform.h"
 
 #ifdef USE_CLN_NUMBERS
 #include <cln/cln.h>
 typedef cln::cl_RA Rational;
 typedef cln::cl_I Integer;
+#elif defined(__VS)
+#include <mpirxx.h>
+typedef mpq_class Rational;
+typedef mpz_class Integer;
 #else
 #include <gmpxx.h>
 typedef mpq_class Rational;
