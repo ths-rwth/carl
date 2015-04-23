753c4ec47bd614c742382b090eb9370c9057026e
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2015-04-23 09:51:46 +0200

    Fixed data races.

--- src/carl/util/TermAdditionManager.h
+++ src/carl/util/TermAdditionManager.h
@@ -179,7 +179,7 @@ public:
             }
 		}
         #endif
-        
+		std::lock_guard<std::mutex> lock(mMutex);
 		std::get<2>(data) = false;
 	}
 
@@ -191,9 +191,9 @@ public:
 		for (auto i = t.begin(); i != t.end(); i++) {
 			if ((*i).monomial()) termIDs[(*i).monomial()->id()] = 0;
 		}
+		std::lock_guard<std::mutex> lock(mMutex);
 		std::get<2>(data) = false;
 	}
 };
 
 }
-
