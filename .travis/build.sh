1695d05af66ced858538ada558241b43e44d3d8b
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2015-05-08 15:32:07 +0200

    Working on automatic doxygen deployment.

--- .travis/build.sh
+++ .travis/build.sh
@@ -3,8 +3,19 @@
 mkdir build || return 1
 cd build/ || return 1
 cmake -D DEVELOPER=ON ../ || return 1
-make resources || return 1
-make -j4 lib_carl || return 1
-make -j4 || return 1
-make CTEST_OUTPUT_ON_FAILURE=1 test || return 1
+
+if [[ ${USE} == "doxygen" ]]; then
+	make doc
+	
+	git config --global user.email "gereon.kremer@cs.rwth-aachen.de"
+	git config --global user.name "Travis doxygen daemon"
+	
+	git clone git@github.com:smtrat/smtrat.github.io.git
+elif
+	make resources || return 1
+	make -j4 lib_carl || return 1
+	make -j4 || return 1
+	make CTEST_OUTPUT_ON_FAILURE=1 test || return 1
+fi
+
 cd ../
