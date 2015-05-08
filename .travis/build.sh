a1036cc5af86ef4743663f87f01fb9bd01c8f759
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2015-05-08 15:37:18 +0200

    First attempt to deploy documentation automatically.

--- .travis/build.sh
+++ .travis/build.sh
@@ -10,7 +10,16 @@ if [[ ${USE} == "doxygen" ]]; then
 	git config --global user.email "gereon.kremer@cs.rwth-aachen.de"
 	git config --global user.name "Travis doxygen daemon"
 	
-	git clone git@github.com:smtrat/smtrat.github.io.git
+	git clone 219fc41efb80a7a8f102f5ca9147baf58514d734@github.com:smtrat/smtrat.github.io.git
+	cd smtrat.github.io.git/
+	
+	git rm -rf carl/
+	mkdir -p carl/
+	cp doc/html/* carl/
+	git add carl/
+	git commit -m "Updated documentation for carl"
+	git push origin master
+
 elif
 	make resources || return 1
 	make -j4 lib_carl || return 1
