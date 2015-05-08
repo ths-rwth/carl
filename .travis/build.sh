6727d1d4721dd740da575c5e892cb300c0afdc4a
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2015-05-08 15:13:55 +0200

    Working on travis.

--- .travis/build.sh
+++ .travis/build.sh
@@ -5,4 +5,6 @@ cd build/ || return 1
 cmake -D DEVELOPER=ON ../ || return 1
 make resources || return 1
 make -j4 lib_carl || return 1
+make -j4 || return 1
+make CTEST_OUTPUT_ON_FAILURE=1 test || return 1
 cd ../
