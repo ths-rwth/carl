1ed45f213c06f14a1957cb89360cac16f8ba82f3
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2015-05-08 16:03:43 +0200

    Changed to https to make github token work.

--- .travis/build.sh
+++ .travis/build.sh
@@ -10,7 +10,7 @@ if [[ ${USE} == "doxygen" ]]; then
 	git config --global user.email "gereon.kremer@cs.rwth-aachen.de"
 	git config --global user.name "Travis doxygen daemon"
 	
-	git clone 219fc41efb80a7a8f102f5ca9147baf58514d734@github.com:smtrat/smtrat.github.io.git
+	git clone https://219fc41efb80a7a8f102f5ca9147baf58514d734@github.com/smtrat/smtrat.github.io.git
 	cd smtrat.github.io.git/
 	
 	git rm -rf carl/
