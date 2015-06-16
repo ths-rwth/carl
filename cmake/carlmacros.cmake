9c691f247f3b607766a0cfee8e54e9c40ca0a3d8
Igor Nicolai Bongartz <ib797487@rwth-aachen.de>
2015-06-16 10:37:47 +0200

    fixed collect_files

--- cmake/carlmacros.cmake
+++ cmake/carlmacros.cmake
@@ -41,101 +41,38 @@ MACRO(LIST_APPEND_UNIQUE var value)
      ENDIF(NOT LIST_ADD_UNIQUE_FLAG)
 ENDMACRO(LIST_APPEND_UNIQUE)
 
-#MACRO from Stackoverflow (http://stackoverflow.com/questions/7787823/cmake-how-to-get-the-name-of-all-subdirectories-of-a-directory) 10.6.15
-MACRO(ListSubDirs result curdir)
-  FILE(GLOB children RELATIVE ${curdir} ${curdir}/*)
-  SET(dirlist "")
-  FOREACH(child ${children})
-    IF(IS_DIRECTORY ${curdir}/${child})
-        LIST(APPEND dirlist ${child})
-    ENDIF()
-  ENDFOREACH()
-  SET(${result} ${dirlist})
-ENDMACRO()
+#FUNCTION collect_files - notice that carl uses ${prefix}_${name}_sources (SOURCES)
+function(collect_files prefix name)
+  file(GLOB_RECURSE subfiles RELATIVE ${CMAKE_CURRENT_SOURCE_DIR}/${name} ${name}/*)
 
-#FUNCTION collect_files FOR RECURSION - input "no_subdir"
-function(collect_files prefix dir_name dir_path subdir)
-	#determine if the are in the dir or subdir
-	if(NOT ${subdir} STREQUAL no_subdir)
-		set(path ${dir_path}/${subdir})
-		set(name ${dir_name}_${subdir})
-	else()
-		set(path ${dir_path})
-		set(name ${dir_name})
-	endif()
+  foreach(subfile ${subfiles})
+    if(${subfile} MATCHES ".*([.]in)")
+      get_filename_component(subfile_name ${subfile} NAME_WE)
+      if(subfile_name STREQUAL "config")
+        configure_file(${CMAKE_SOURCE_DIR}/src/${prefix}/${name}/${subfile} ${CMAKE_SOURCE_DIR}/src/${prefix}/${name}/${subfile_name}.h)
+      endif()
 
-	#Recursion for subdirectories
-  ListSubDirs(subdir ${CMAKE_CURRENT_SOURCE_DIR}/${path})
+    elseif((${subfile} MATCHES ".*([.]h)") OR (${subfile} MATCHES ".*([.]tpp)"))
+      get_filename_component(subdir ${subfile} DIRECTORY)
+      if(NOT ${subdir} STREQUAL "")
+        LIST_APPEND_UNIQUE(${prefix}_${name}_subdir ${subdir})
+        list(APPEND ${prefix}_${name}_${subdir}_headers ${subfile})
+      endif()
+        list(APPEND ${prefix}_${name}_headers ${subfile})
 
-	foreach(subname ${subdir})
+    elseif(${subfile} MATCHES ".*([.]cpp)")
+      list(APPEND ${prefix}_${name}_sources ${name}/${subfile})
+    endif()
+  endforeach()
 
-		collect_files(${prefix} ${name} ${path} ${subname})
-
-		#Update upper prefix_name_...
-		list(APPEND ${prefix}_${name}_headers ${${prefix}_${name}_${subname}_headers})
-		list(APPEND ${prefix}_${name}_sources ${${prefix}_${name}_${subname}_sources})
-	endforeach()
-
-	#Headers
-	file(GLOB headers  LIST_DIRECTORIES true ${path}/*.h ${path}/*.tpp)
-	foreach(header ${headers})
-		get_filename_component(reduced_header ${header} NAME)
-		list(APPEND ${prefix}_${name}_headers ${path}/${reduced_header})
-	endforeach()
-
-	#Sources
-	file(GLOB sources ${path}/*.cpp LIST_DIRECTORIES true)
-	foreach(source ${sources})
-		get_filename_component(reduced_source ${source} NAME)
-		list(APPEND ${prefix}_${name}_sources ${path}/${reduced_source})
-	endforeach()
-
-	#Configure only if it exists
-	if(EXISTS ${CMAKE_SOURCE_DIR}/src/${prefix}/${path}/config.h.in)
-		configure_file(${CMAKE_SOURCE_DIR}/src/${prefix}/${path}/config.h.in ${CMAKE_SOURCE_DIR}/src/${prefix}/${path}/config.h)
-	endif()
-
-  #ALTERNATE VERSION
-  if(FALSE)
-      # Alternate version, take all documents even recusiv and do everything in one loop
-      file(GLOB_RECURSE subfiles RELATIVE ${CMAKE_CURRENT_SOURCE_DIR}/${path} ${path}/*)
-
-      foreach(subfile ${subfiles})
-        #message(STATUS "think about ${subfile}")
-        if(${subfile} MATCHES ".*([.]in)")
-          #message(STATUS "noticed config ${subfile}")
-          get_filename_component(subfile_name ${subfile} NAME_WE)
-          configure_file(${CMAKE_SOURCE_DIR}/src/${prefix}/${path}/${subfile} ${CMAKE_SOURCE_DIR}/src/${prefix}/${path}/${subfile_name}.h)
-
-        elseif((${subfile} MATCHES ".*([.]h)") OR (${subfile} MATCHES ".*([.]tpp)"))
-          get_filename_component(subdir ${subfile} DIRECTORY)
-          if(NOT ${subdir} STREQUAL "")
-            #message(STATUS "noticed dir for headerfiles ${subdir}")
-            LIST_APPEND_UNIQUE(${prefix}_${name}_subdir ${subdir})
-            list(APPEND ${prefix}_${name}_${subdir}_headers ${subfile})
-          endif()
-            list(APPEND ${prefix}_${name}_headers ${subfile})
-
-        elseif(${subfile} MATCHES ".*([.]cpp)")
-          message(STATUS "noticed source ${subfile}")
-          list(APPEND ${prefix}_${name}_sources ${subfile})
-        endif()
-      endforeach()
-
-      message(STATUS "display headers ${${prefix}_${name}_headers}")
-      message(STATUS "display src ${${prefix}_${name}_sources}")
-
-
-
-      foreach(subdir ${${prefix}_${name}_subdir})
-        install(FILES			${${prefix}_${name}_${subdir}_headers}
-          DESTINATION		include/${prefix}/${subdir}/${path})
-      endforeach()
-  endif()
+  foreach(subdir ${${prefix}_${name}_subdir})
+    install(FILES			${${prefix}_${name}_${subdir}_headers}
+      DESTINATION		include/${prefix}/${subdir})
+  endforeach()
 
 	#Install
 	install(FILES			${${prefix}_${name}_headers}
-			DESTINATION		include/${prefix}/${path})
+			DESTINATION		include/${prefix}/${name})
 
 	#SET the scope
 	set(${prefix}_${name}_headers ${${prefix}_${name}_headers} PARENT_SCOPE)
