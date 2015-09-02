f82c3f223201182caa3ad80303365379b6292f9f
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2015-09-02 14:54:10 +0200

    Forcing cmake update.

--- .travis/setup_ubuntu1204.sh
+++ .travis/setup_ubuntu1204.sh
@@ -15,6 +15,7 @@ sudo add-apt-repository -y "deb http://llvm.org/apt/precise/ llvm-toolchain-prec
 sudo add-apt-repository -y "deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.5 main" 
 sudo apt-get -qq update
 
+sudo apt-get purge cmake
 install cmake doxygen libboost1.48-all-dev libcln-dev libeigen3-dev libgtest-dev libstdc++-4.9-dev
 
 if [[ ${USE} == "g++-4.8" ]]; then
