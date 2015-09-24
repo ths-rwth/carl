91601ed2143a9ccdd65b26f6af66a46ccaaab360
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2015-09-24 13:52:29 +0200

    Installing doxygen only if needed.

--- .travis/setup_ubuntu1204.sh
+++ .travis/setup_ubuntu1204.sh
@@ -16,7 +16,12 @@ sudo add-apt-repository -y "deb http://llvm.org/apt/precise/ llvm-toolchain-prec
 sudo apt-get -qq update
 
 sudo apt-get purge cmake
-install cmake doxygen libboost1.48-all-dev libcln-dev libeigen3-dev libgtest-dev libstdc++-4.9-dev
+install cmake libboost1.48-all-dev libcln-dev libeigen3-dev libgtest-dev libstdc++-4.9-dev
+
+if [[ ${TASK} == "doxygen" ]]; then
+	install doxygen
+fi
+
 
 if [[ ${USE} == "g++-4.8" ]]; then
 	install gcc-4.8 g++-4.8
