ace8618defab1798a88a3eba202315e37d598b1e
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2015-09-29 15:02:19 +0200

    Reducing accesses to llvm ppa.

--- .travis/setup_ubuntu1204.sh
+++ .travis/setup_ubuntu1204.sh
@@ -11,10 +11,6 @@ if [[ ${TASK} == "doxygen" ]]; then
 fi
 sudo add-apt-repository -y ppa:smspillaz/cmake-2.8.12
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-sudo add-apt-repository -y "deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.4 main" 
-sudo add-apt-repository -y "deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.5 main" 
-sudo add-apt-repository -y "deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.6 main" 
-sudo add-apt-repository -y "deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.7 main" 
 sudo apt-get -qq update
 
 sudo apt-get purge cmake
@@ -35,15 +31,23 @@ elif [[ ${USE} == "g++-5.1" ]]; then
 	install gcc-5 g++-5
 	defCXX gcc-5 g++-5
 elif [[ ${USE} == "clang++-3.4" ]]; then
+	sudo add-apt-repository -y "deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.4 main"
+	sudo apt-get -qq update
 	install clang-3.4
 	defCXX clang-3.4 clang++-3.4
 elif [[ ${USE} == "clang++-3.5" ]]; then
+	sudo add-apt-repository -y "deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.5 main"
+	sudo apt-get -qq update
 	install clang-3.5
 	defCXX clang-3.5 clang++-3.5
 elif [[ ${USE} == "clang++-3.6" ]]; then
+	sudo add-apt-repository -y "deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.6 main"
+	sudo apt-get -qq update
 	install clang-3.6
 	defCXX clang-3.6 clang++-3.6
 elif [[ ${USE} == "clang++-3.7" ]]; then
+	sudo add-apt-repository -y "deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.7 main"
+	sudo apt-get -qq update
 	install clang-3.7
 	defCXX clang-3.7 clang++-3.7
 fi
