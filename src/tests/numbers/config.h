a354c543e02532253955dc7d3edc5c6da05f2fe7
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2015-10-13 13:10:18 +0200

    Fixed type lists for typed tests.

--- src/tests/numbers/config.h
+++ src/tests/numbers/config.h
@@ -5,20 +5,41 @@
 #include "../../carl/interval/Interval.h"
 #include "../../carl/numbers/numbers.h"
 
-typedef testing::Types<int, mpz_class> IntegerTypes;
-typedef testing::Types<mpq_class> RationalTypes;
-typedef testing::Types<int, mpz_class, mpq_class> NumberTypes;
+typedef testing::Types<
+	mpz_class,
+	#ifdef USE_CLN_NUMBERS
+	cln::cl_I,
+	#endif
+	int
+> IntegerTypes;
 
 typedef testing::Types<
-	carl::Interval<mpq_class>,
+	mpq_class,
+	#ifdef USE_CLN_NUMBERS
+	cln::cl_RA
+	#endif
+> RationalTypes;
+
+typedef testing::Types<
+	mpz_class,
+	mpq_class,
+	#ifdef USE_CLN_NUMBERS
+	cln::cl_I,
+	cln::cl_RA,
+	#endif
+	int
+> NumberTypes;
+
+typedef testing::Types<
 	carl::Interval<mpz_class>,
+	carl::Interval<mpq_class>,
 	#ifdef USE_MPFR_FLOAT
 	carl::Interval<carl::FLOAT_T<mpfr_t> >,
 	#endif
 	#ifdef USE_CLN_NUMBERS
-	carl::Interval<cln::cl_RA>,
 	carl::Interval<cln::cl_I>,
+	carl::Interval<cln::cl_RA>,
 	#endif
-	carl::Interval<double>,
-	carl::Interval<int>
+	carl::Interval<int>,
+	carl::Interval<double>
 > IntervalTypes;
