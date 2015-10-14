7380a25d3d7ca87818417e410b89d1c2a6aba83b
sschupp <stefan.schupp@cs.rwth-aachen.de>
2015-10-14 18:02:22 +0200

    minor fix in predeclaration

--- src/carl/numbers/conversion/conversion.h
+++ src/carl/numbers/conversion/conversion.h
@@ -1,10 +1,10 @@
 #pragma once
 
 namespace carl {
-	template<typename From, typename To, carl::DisableIf< std::is_same<From,To> > = dummy >
+	template<typename From, typename To, carl::DisableIf< std::is_same<From,To> > >
 	inline To convert(const From&);
 
-	template<typename From, typename To, carl::EnableIf< std::is_same<From,To> > = dummy >
+	template<typename From, typename To, carl::EnableIf< std::is_same<From,To> > >
 	inline To convert(const From& n) {
 		return n;
 	}
