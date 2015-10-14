08a835d2dd7afec32002c635ab35be5ac1b6767d
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2015-10-14 16:44:57 +0200

    A few little fixes.

--- src/carl/numbers/Number.h
+++ src/carl/numbers/Number.h
@@ -7,9 +7,20 @@ namespace carl {
 	private:
 		T mData;
 	public:
-		Number(): mData(carl::constant_one<T>::get()) {}
+		Number(): mData(carl::constant_zero<T>::get()) {}
 		Number(const T& t): mData(t) {}
 		Number(T&& t): mData(std::move(t)) {}
+		Number(const Number<T>& n): mData(n.mData) {}
+		Number(Number<T>&& n): mData(std::move(n.mData)) {}
+		
+		Number<T>& operator=(const Number<T>& n) {
+			mData = n.mData;
+			return *this;
+		}
+		Number<T>& operator=(Number<T>&& n) {
+			mData = std::move(n.mData);
+			return *this;
+		}
 		
 		operator T&() {
 			return mData;
