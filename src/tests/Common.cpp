d71d10cefa7f1ada9b2a2fdde696918c3f6977cb
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2015-10-15 12:55:49 +0200

    Fixed common.

--- src/tests/Common.cpp
+++ src/tests/Common.cpp
@@ -5,9 +5,3 @@ using carl::GFNumber;
 using carl::Term;
 using carl::MultivariatePolynomial;
 using carl::MultivariateGCD;
-
-template class carl::GaloisField<Integer>;
-template class carl::GFNumber<Integer>;
-template class carl::Term<Rational>;
-template class carl::MultivariatePolynomial<Rational>;
-template class carl::MultivariateGCD<carl::PrimitiveEuclidean, Rational>;
