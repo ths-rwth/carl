2f3a95429497fd1d3cffb773753166055117ed4e
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2015-10-30 14:05:43 +0100

    Added ppa.

--- .travis/setup_ubuntu1404.sh
+++ .travis/setup_ubuntu1404.sh
@@ -6,6 +6,7 @@ function install {
 	sudo apt-get -qq install --force-yes $*
 }
 
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get -qq update
 
 install cmake doxygen libboost1.55-all-dev libcln-dev libeigen3-dev libgtest-dev
@@ -16,6 +17,9 @@ if [[ ${USE} == "g++-4.8" ]]; then
 elif [[ ${USE} == "g++-4.9" ]]; then
 	install gcc-4.9 g++-4.9
 	defCXX gcc-4.9 g++-4.9
+elif [[ ${USE} == "g++-5.1" ]]; then
+	install gcc-5 g++-5
+	defCXX gcc-5 g++-5
 elif [[ ${USE} == "clang++-3.4" ]]; then
 	install clang-3.4
 	defCXX clang-3.4 clang++-3.4
