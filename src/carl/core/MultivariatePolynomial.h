96b9c3690236a4ed009e65b077577c8f4fcfaded
Florian Corzilius <flo@localhost>
2015-12-07 15:52:22 +0100

    minor fix in method isVariable

--- src/carl/core/MultivariatePolynomial.h
+++ src/carl/core/MultivariatePolynomial.h
@@ -367,7 +367,7 @@ public:
      */
     bool isVariable() const
     {
-        return totalDegree() == 1 && nrTerms() == 1 && lterm() == constant_one<CoeffType>::get();
+        return totalDegree() == 1 && nrTerms() == 1 && lcoeff() == constant_one<CoeffType>::get();
     }
     
     /**
@@ -376,7 +376,7 @@ public:
      */
     Variable::Arg getSingleVariable() const
     {
-        assert( !isVariable() );
+        assert( !isConstant() );
         return lterm().getSingleVariable();
     }
     
