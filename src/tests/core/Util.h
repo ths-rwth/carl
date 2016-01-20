b956296fa0c279ffda26b0b273b439cbefbfbba2
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2016-01-20 12:35:13 +0100

    Added more order checking for test.

--- src/tests/core/Util.h
+++ src/tests/core/Util.h
@@ -26,9 +26,36 @@ struct ComparisonOperator {
 	}
 };
 
+template<typename Iterator, typename Less>
+struct SingleComparisonOperator {
+	typedef void result_type;
+	Iterator it1, it2;
+	const Less& less;
+	SingleComparisonOperator(const Less& l): less(l) {}
+	void set(const Iterator& i1, const Iterator& i2) {
+		it1 = i1;
+		it2 = i2;
+	}
+	template<typename T1, typename T2>
+	void operator()(const T1& t1, const T2& t2) {
+		EXPECT_EQ(it1 < it2, less(t1, t2)) << t1 << " < " << t2;
+	}
+};
+
 template<typename List>
 void expectRightOrder(const List& list) {
 	ComparisonOperator<typename List::const_iterator> c;
+	for (auto it1 = list.begin(); it1 != list.end(); it1++) {
+		for (auto it2 = list.begin(); it2 != list.end(); it2++) {
+			c.set(it1, it2);
+			boost::apply_visitor(c, *it1, *it2);
+		}
+	}
+}
+
+template<typename List, typename Less>
+void expectRightOrder(const List& list, const Less& less) {
+	SingleComparisonOperator<typename List::const_iterator, Less> c(less);
 	for (auto it1 = list.begin(); it1 != list.end(); it1++) {
 		for (auto it2 = list.begin(); it2 != list.end(); it2++) {
 			c.set(it1, it2);
