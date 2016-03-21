1e312d070bd9b1b28998370e829eb96a13b2b1e5
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2016-03-21 16:37:34 +0100

    Added a test that ensures that the usual int types have some values.

--- src/tests/numbers/Test_numbers_native.cpp
+++ src/tests/numbers/Test_numbers_native.cpp
@@ -2,7 +2,21 @@
 
 #include "../../carl/numbers/numbers.h"
 
+#include <cstdint>
+
 using namespace carl;
+
+TEST(NumbersNative, NumberWidth)
+{
+	static_assert(sizeof(short int) == 2,"");
+	static_assert(sizeof(unsigned short int) == 2,"");
+	static_assert(sizeof(int) == 4,"");
+	static_assert(sizeof(unsigned int) == 4,"");
+	static_assert(sizeof(long int) == 8,"");
+	static_assert(sizeof(unsigned long int) == 8,"");
+	static_assert(sizeof(long long int) == 8,"");
+	static_assert(sizeof(unsigned long long int) == 8,"");
+}
 
 TEST(NumbersNative, highestPower)
 {
