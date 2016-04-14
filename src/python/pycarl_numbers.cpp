f4629d1efc692707c1dda051a66a467c4ae71b4c
hbruintjes <h.bruintjes@cs.rwth-aachen.de>
2016-04-14 13:53:02 +0200

    Use pybind11 for pycarl.numbers

--- src/python/pycarl_numbers.cpp
+++ src/python/pycarl_numbers.cpp
@@ -1,5 +1,5 @@
 #include <Python.h>
-#include <boost/python.hpp>
+#include "pybind11/pybind11.h"
 
 #include "definitions.h"
 #include "helpers.h"
@@ -10,30 +10,38 @@
 double ratToDouble(const Rational& r) { return carl::toDouble(r); }
 std::string ratToString(const Rational& r ) { return carl::toString(r, true); }
 
-BOOST_PYTHON_MODULE(_numbers) {
+namespace py = pybind11;
 
-    using namespace boost::python;
-    class_<Rational, Rational*>("Rational", "Class wrapping rational numbers")
-            .def(init<int>())
-            .def("__float__", &ratToDouble)
-            .def("__str__", &ratToString)
-            .def(self + self)
-            .def(self - self)
-            .def(self * self)
-            .def(self / self)
-            .def(self == self)
-            .def(self != self)
-            .def(-self)
-            .def(self < self)
-            .def(self > self)
-            .def(self >= self)
-            .def(self <= self)
-            .def(self > int())
-            .def(self < int())
-            .def(self == int())
-            .def(self != int())
-            .def(self >= int())
-            .def(self <= int())
-    ;
-    def("rationalize", static_cast<Rational (*)(double)>(&carl::rationalize), "Construct a rational number from a double");
+PYBIND11_PLUGIN(_numbers) {
+        py::module m("numbers", "Number plugin for pycarl");
+
+        py::class_<Rational, Rational*>("Rational", "Class wrapping rational numbers")
+        .def(py::init<int>())
+        .def("__float__", &ratToDouble)
+        .def("__str__", &ratToString)
+        .def(py::self + py::self)
+        .def(py::self - py::self)
+        .def(py::self * py::self)
+        .def(py::self / py::self)
+        .def(py::self == py::self)
+        .def(py::self != py::self)
+        .def(-py::self)
+        .def(py::self < py::self)
+        .def(py::self > py::self)
+        .def(py::self >= py::self)
+        .def(py::self <= py::self)
+        .def(py::self > int())
+        .def(py::self < int())
+        .def(py::self == int())
+        .def(py::self != int())
+        .def(py::self >= int())
+        .def(py::self <= int())
+        ;
+
+        m.def("rationalize", static_cast<Rational (*)(double)>(&carl::rationalize), "Construct a rational number from a double");
+
+        return m.ptr();
+}
+
+
 }
