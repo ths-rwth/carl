204c06c590dcbd08dae17d705c16c62d28ff7d67
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2016-04-25 16:25:30 +0200

    setting stdlib

--- .travis/setup_ubuntu1404.sh
+++ .travis/setup_ubuntu1404.sh
@@ -11,8 +11,6 @@ sudo apt-get -qq update
 
 install cmake doxygen libboost1.55-all-dev libcln-dev libeigen3-dev libgtest-dev
 
-dpkg -s libgmp-dev
-
 if [[ ${USE} == "g++-4.8" ]]; then
 	install gcc-4.8 g++-4.8
 	defCXX gcc-4.8 g++-4.8
@@ -25,9 +23,11 @@ elif [[ ${USE} == "g++-5" ]]; then
 elif [[ ${USE} == "clang++-3.4" ]]; then
 	install clang-3.4 libc++-dev
 	defCXX clang clang++
+	export CXXFLAGS="-stdlib=libc++"
 elif [[ ${USE} == "clang++-3.5" ]]; then
 	install clang-3.5 libc++-dev
 	defCXX clang-3.5 clang++-3.5
+	export CXXFLAGS="-stdlib=libc++"
 elif [[ ${USE} == "clang++-3.6" ]]; then
 	sudo add-apt-repository -y "deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.6 main"
 	sudo apt-get -qq update
