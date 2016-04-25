122504d592f0913abbfaedf5823f9f35a119ccea
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2016-04-25 13:35:35 +0200

    fix.

--- .travis/setup_ubuntu1404.sh
+++ .travis/setup_ubuntu1404.sh
@@ -6,44 +6,47 @@ function install {
 	sudo apt-get -qq install --force-yes $*
 }
 
-#sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-#sudo apt-get -qq update
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo apt-get -qq update
 
-#install cmake doxygen libboost1.55-all-dev libcln-dev libeigen3-dev libgtest-dev
+install cmake doxygen libboost1.55-all-dev libcln-dev libeigen3-dev libgtest-dev
 
 dpkg -s libgmp-dev
 
 if [[ ${USE} == "g++-4.8" ]]; then
-	#install gcc-4.8 g++-4.8
+	install gcc-4.8 g++-4.8
 	defCXX gcc-4.8 g++-4.8
 elif [[ ${USE} == "g++-4.9" ]]; then
-	#install gcc-4.9 g++-4.9
+	install gcc-4.9 g++-4.9
 	defCXX gcc-4.9 g++-4.9
 elif [[ ${USE} == "g++-5" ]]; then
-	#install gcc-5 g++-5
+	install gcc-5 g++-5
 	defCXX gcc-5 g++-5
 elif [[ ${USE} == "clang++-3.4" ]]; then
-	#install clang-3.4
+	install clang-3.4
 	defCXX clang clang++
 elif [[ ${USE} == "clang++-3.5" ]]; then
-	#install clang-3.5
+	install clang-3.5
 	defCXX clang-3.5 clang++-3.5
 elif [[ ${USE} == "clang++-3.6" ]]; then
-	#sudo add-apt-repository -y "deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.6 main"
-	#sudo apt-get -qq update
-	#install clang-3.6
+	sudo add-apt-repository -y "deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.6 main"
+	sudo apt-get -qq update
+	install clang-3.6
 	defCXX clang-3.6 clang++-3.6
 elif [[ ${USE} == "clang++-3.7" ]]; then
-	#sudo add-apt-repository -y "deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.7 main"
-	#sudo apt-get -qq update
-	#install clang-3.7
+	sudo add-apt-repository -y "deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.7 main"
+	sudo apt-get -qq update
+	install clang-3.7
 	defCXX clang-3.7 clang++-3.7
 elif [[ ${USE} == "clang++-3.8" ]]; then
+	sudo add-apt-repository -y "deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.8 main"
+	sudo apt-get -qq update
+	install clang-3.8
 	defCXX clang-3.8 clang++-3.8
 fi
 
-#sudo service postgresql stop
-#sudo service mysql stop
-#sudo service cron stop
+sudo service postgresql stop
+sudo service mysql stop
+sudo service cron stop
 
-#ps aux
+ps aux
