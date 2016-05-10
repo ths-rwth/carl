c8dfc1d9021ac2de5cc0d6208b9a16094f8dde39
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2016-05-10 15:53:31 +0200

    fixing eigen paths.

--- resources/eigen3.cmake
+++ resources/eigen3.cmake
@@ -1,13 +1,14 @@
-set(Eigen3_VERSION "3.2.8")
+set(eigen3_VERSION "3.2.8")
 
 ExternalProject_Add(
-    Eigen3
-    URL "https://bitbucket.org/eigen/eigen/get/${Eigen3_VERSION}.zip"
+    eigen3
+    URL "https://bitbucket.org/eigen/eigen/get/${eigen3_VERSION}.zip"
 	URL_MD5 e368e3dd35e9a5f46a118029cabc494d
+	SOURCE_DIR "src/eigen3/eigen3"
 	INSTALL_COMMAND ""
 )
 
-ExternalProject_Get_Property(Eigen3 source_dir)
-set(Eigen3_INCLUDE_DIR ${source_dir})
+ExternalProject_Get_Property(eigen3 source_dir)
+set(eigen3_INCLUDE_DIR ${source_dir})
 
-add_dependencies(resources Eigen3)
+add_dependencies(resources eigen3)
