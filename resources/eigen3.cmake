b7ce553ea4f36307dedc466de5f784a46f2688ab
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2016-05-10 16:11:19 +0200

    fixing eigen3 paths again-

--- resources/eigen3.cmake
+++ resources/eigen3.cmake
@@ -4,11 +4,12 @@ ExternalProject_Add(
     eigen3
     URL "https://bitbucket.org/eigen/eigen/get/${eigen3_VERSION}.zip"
 	URL_MD5 e368e3dd35e9a5f46a118029cabc494d
-	SOURCE_DIR "src/eigen3/eigen3"
-	INSTALL_COMMAND ""
+	CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=.
+	BUILD_COMMAND ""
 )
 
-ExternalProject_Get_Property(eigen3 source_dir)
-set(eigen3_INCLUDE_DIR ${source_dir})
+ExternalProject_Get_Property(eigen3 binary_dir)
+set(eigen3_INCLUDE_DIR ${binary_dir}/include)
+message(STATUS "eigen3 include ${eigen3_INCLUDE_DIR}")
 
 add_dependencies(resources eigen3)
