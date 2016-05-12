2e50d5a19539a674f51764b48fccd1db9e546115
hbruintjes <h.bruintjes@cs.rwth-aachen.de>
2016-05-12 09:41:55 +0200

    Use setup.cfg istead of custom build command

--- pycarl/setup.py
+++ pycarl/setup.py
@@ -14,35 +14,21 @@ formula_sources = glob(os.path.join('src', 'formula', '*.cpp'))
 parse_sources = glob(os.path.join('src', 'parse', '*.cpp'))
 
 # Configuration shared between external modules follows
-
-# To help along, if carl and/or pybind is not system installed, retrieve from carl distribution
 include_dirs = ['.', 'src', 'resources/pybind11/include']
-local_carl_path = os.path.join(PROJECT_DIR, '..', 'src')
-if os.path.exists(local_carl_path):
-    include_dirs.append(local_carl_path)
-
-# Like includes, also add local path for library, assuming made in 'build'
-library_dirs = []
-local_carl_lib_path = os.path.join(PROJECT_DIR, '..', 'build')
-if os.path.exists(local_carl_lib_path):
-    library_dirs.append(local_carl_lib_path)
-
 libraries = ['carl']
 extra_compile_args = ['-std=c++11']
-define_macros = []
 
+# TODO: Hardcoded rpath for Darwin
 extra_link_args = []
 if platform.system() == 'Darwin':
-    extra_link_args.append('-Wl,-rpath,'+library_dirs[0])
+    extra_link_args.append('-Wl,-rpath,' + os.path.join(PROJECT_DIR, '..', 'build'))
 
 ext_core = Extension(
     name='core',
     sources=['src/mod_core.cpp'] + core_sources,
     include_dirs=include_dirs,
     libraries=libraries,
-    library_dirs=library_dirs,
     extra_compile_args=extra_compile_args,
-    define_macros=define_macros,
     extra_link_args=extra_link_args
 )
 
@@ -51,9 +37,7 @@ ext_formula = Extension(
     sources=['src/mod_formula.cpp'] + formula_sources,
     include_dirs=include_dirs,
     libraries=libraries,
-    library_dirs=library_dirs,
     extra_compile_args=extra_compile_args,
-    define_macros=define_macros,
     extra_link_args=extra_link_args
 )
 
@@ -62,46 +46,9 @@ ext_parse = Extension(
     sources=['src/mod_parse.cpp'] + parse_sources,
     include_dirs=include_dirs,
     libraries=libraries,
-    library_dirs=library_dirs,
     extra_compile_args=extra_compile_args,
-    define_macros=define_macros,
     extra_link_args=extra_link_args
 )
-
-class pycarl_build_ext(build_ext):
-    """Extend build_ext to provide CLN toggle option
-    """
-    user_options = build_ext.user_options + [
-        ('use-cln', None,
-         "use cln numbers instead of gmpxx")
-        ]
-
-    def __init__(self, *args, **kwargs):
-        build_ext.__init__(self, *args, **kwargs)
-
-    def initialize_options (self):
-        build_ext.initialize_options(self)
-        self.use_cln = None
-
-    def finalize_options(self):
-        build_ext.finalize_options(self)
-
-        if self.use_cln:
-            self.libraries += ['cln']
-            if not self.define:
-                self.define = []
-            else:
-                self.define = list(self.define)
-            self.define += [('PYCARL_USE_CLN', 1)]
-        else:
-            self.libraries += ['gmp', 'gmpxx']
-            if not self.undef:
-                self.undef = []
-            self.undef += ['PYCARL_USE_CLN']
-
-        if library_dirs:
-            # Makes local carl library lookup that much easier
-            self.rpath += library_dirs
 
 setup(name="pycarl",
       version="1.1",
@@ -114,9 +61,5 @@ setup(name="pycarl",
       packages=['pycarl', 'pycarl.formula', 'pycarl.parse'],
       package_dir={'':'lib'},
       ext_package='pycarl',
-      ext_modules=[ext_core, ext_formula, ext_parse
-                   ],
-      cmdclass={
-        'build_ext': pycarl_build_ext,
-      }
+      ext_modules=[ext_core, ext_formula, ext_parse],
 )
