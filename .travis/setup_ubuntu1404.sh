24fb7bd806705da603211fc3b98250df672add0a
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2016-06-06 10:29:26 +0200

    Installing g++5 with clang to use new stdlib.

--- .travis/setup_ubuntu1404.sh
+++ .travis/setup_ubuntu1404.sh
@@ -31,7 +31,7 @@ elif [[ ${USE} == "clang++-3.4" ]]; then
 	defCXX clang clang++
 	export CXXFLAGS="-stdlib=libc++"
 elif [[ ${USE} == "clang++-3.5" ]]; then
-	install clang-3.5 libc++-dev
+	install clang-3.5 g++-5 libc++-dev
 	defCXX clang-3.5 clang++-3.5
 	export CXXFLAGS="-stdlib=libc++"
 elif [[ ${USE} == "clang++-3.6" ]]; then
