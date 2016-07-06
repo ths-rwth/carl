49eee069352f2d59647368260e03ccee3fe35867
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2016-07-06 11:55:56 +0200

    fixing a bug?

--- resources/eigen3.cmake
+++ resources/eigen3.cmake
@@ -1,11 +1,11 @@
 ExternalProject_Add(
-    EIGEN3
+    EIGEN3_REPO
     URL "https://bitbucket.org/eigen/eigen/get/${EIGEN3_VERSION}.zip"
 	URL_MD5 e368e3dd35e9a5f46a118029cabc494d
 	CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
 )
 
-ExternalProject_Get_Property(EIGEN3 INSTALL_DIR)
+ExternalProject_Get_Property(EIGEN3_REPO INSTALL_DIR)
 
 add_imported_library(EIGEN3 SHARED "" "${INSTALL_DIR}/include")
 
