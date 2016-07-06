7b5205a31df9b304c9dbb6f006bbbf7661ca7188
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2016-07-06 10:51:11 +0200

    Moving install to separate file.

---
+++ cmake/install.cmake
@@ -0,0 +1,22 @@
+
+install(
+	DIRECTORY ${CMAKE_SOURCE_DIR}/src/carl/
+	DESTINATION include/carl
+	FILES_MATCHING REGEX ".*\.(h|tpp)$"
+)
+
+install(
+	TARGETS lib_carl
+	RUNTIME DESTINATION bin
+	LIBRARY DESTINATION lib
+	ARCHIVE DESTINATION lib
+)
+
+if(BUILD_STATIC)
+	install(
+		TARGETS lib_carl_static
+		RUNTIME DESTINATION bin
+		LIBRARY DESTINATION lib
+		ARCHIVE DESTINATION lib
+	)
+endif()
