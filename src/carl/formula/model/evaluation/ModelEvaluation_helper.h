fc46de3e3886389385d1be78eb2fef1723e35359
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2016-08-04 11:22:54 +0200

    Fixing collectRANIR()

--- src/carl/formula/model/evaluation/ModelEvaluation_helper.h
+++ src/carl/formula/model/evaluation/ModelEvaluation_helper.h
@@ -9,6 +9,7 @@ template<typename Rational, typename Poly>
 RealAlgebraicNumberEvaluation::RANMap<Rational> collectRANIR(const std::set<Variable>& vars, const Model<Rational,Poly>& model) {
 	RealAlgebraicNumberEvaluation::RANMap<Rational> map;
 	for (auto var: vars) {
+		if (model.find(var) == model.end()) continue;
 		const ModelValue<Rational,Poly>& mv = model.evaluated(var);
 		assert(!mv.isSubstitution());
 		if (mv.isRAN()) {
