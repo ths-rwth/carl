742ebd6c35a54659a79fde382fc64d9bf226bf14
Tobias Winkler <tobias.winkler1@rwth-aachen.de>
2016-09-22 11:16:18 +0200

    fixed compareDifferentLevels

--- src/carl/thom2/ThomEncoding.h
+++ src/carl/thom2/ThomEncoding.h
@@ -381,10 +381,13 @@ private:
         static ThomComparisonResult compareDifferentLevels(const ThomEncoding<Number>& lhs, const ThomEncoding<Number>& rhs) {
                 if(lhs.polynomial() < rhs.polynomial()) return LESS;
                 if(lhs.polynomial() > rhs.polynomial()) return GREATER;
-                CARL_LOG_ASSERT("carl.thom.compare", !(lhs.mPoint == nullptr && rhs.mPoint == nullptr), "i think this should not happen");
                 if(lhs.mPoint == nullptr && rhs.mPoint != nullptr) return LESS;
                 if(lhs.mPoint != nullptr && rhs.mPoint == nullptr) return GREATER;
-                return compareDifferentLevels(lhs.point(), rhs.point());
+                if(lhs.mPoint != nullptr && rhs.mPoint != nullptr) return compareDifferentLevels(lhs.point(), rhs.point());
+                // same polynomials, mPoint both nullptr
+                CARL_LOG_ASSERT("carl.thom.compare", lhs.mPoint == nullptr && rhs.mPoint == nullptr, "i think this should not happen");
+                return lhs.relevantSignCondition().compare(rhs.relevantSignCondition());
+                
         }
         
         static ThomComparisonResult compareRational(const ThomEncoding<Number>& lhs, const Number& rhs) {
