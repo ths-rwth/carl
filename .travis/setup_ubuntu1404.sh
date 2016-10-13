aad456c20c8912a5f5a8cb454e9797bbc07d888c
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2016-10-13 16:23:06 +0200

    cleaning up.

--- .travis/setup_ubuntu1404.sh
+++ .travis/setup_ubuntu1404.sh
@@ -12,12 +12,8 @@ elif [[ ${USE} == "g++-5" ]]; then
 	defCXX gcc-5 g++-5
 elif [[ ${USE} == "g++-6" ]]; then
 	defCXX gcc-6 g++-6
-elif [[ ${USE} == "clang++-3.4" ]]; then
-	defCXX clang clang++
-	#export CXXFLAGS="-stdlib=libc++"
 elif [[ ${USE} == "clang++-3.5" ]]; then
 	defCXX clang-3.5 clang++-3.5
-	#export CXXFLAGS="-stdlib=libc++"
 elif [[ ${USE} == "clang++-3.6" ]]; then
 	defCXX clang-3.6 clang++-3.6
 elif [[ ${USE} == "clang++-3.7" ]]; then
