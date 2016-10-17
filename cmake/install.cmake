d1cd88fb9c97f30ca5822416ba69679afa538103
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2016-10-17 15:15:42 +0200

    Reducing cmake required version to 3.0, disabling install if version is 3.0.

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -1,22 +1,26 @@
 
-install(
-	DIRECTORY ${CMAKE_SOURCE_DIR}/src/carl/
-	DESTINATION include/carl
-	FILES_MATCHING REGEX ".*\.(h|tpp)$"
-)
-
-install(
-	TARGETS lib_carl
-	RUNTIME DESTINATION bin
-	LIBRARY DESTINATION lib
-	ARCHIVE DESTINATION lib
-)
-
-if(BUILD_STATIC)
+if(${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION} VERSION_GREATER 3.0)
 	install(
-		TARGETS lib_carl_static
+		DIRECTORY ${CMAKE_SOURCE_DIR}/src/carl/
+		DESTINATION include/carl
+		FILES_MATCHING REGEX ".*\.(h|tpp)$"
+	)
+
+	install(
+		TARGETS lib_carl
 		RUNTIME DESTINATION bin
 		LIBRARY DESTINATION lib
 		ARCHIVE DESTINATION lib
 	)
+
+	if(BUILD_STATIC)
+		install(
+			TARGETS lib_carl_static
+			RUNTIME DESTINATION bin
+			LIBRARY DESTINATION lib
+			ARCHIVE DESTINATION lib
+		)
+	endif()
+else()
+	message(STATUS "Disabled install target due to cmake version less than 3.1")
 endif()
