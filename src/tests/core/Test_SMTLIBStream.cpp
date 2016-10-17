5d9960ff821e41aebe3032438ce9908f69152291
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2016-10-17 17:01:38 +0200

    Adding tests for SMTLIBStream.

---
+++ src/tests/core/Test_SMTLIBStream.cpp
@@ -0,0 +1,27 @@
+#include "gtest/gtest.h"
+
+#include "carl/core/VariablePool.h"
+#include "carl/io/SMTLIBStream.h"
+
+#include "../Common.h"
+
+
+TEST(SMTLIBStream, Base)
+{
+
+	carl::Variable x = carl::freshRealVariable("x");
+	carl::SMTLIBStream sls(std::cout);
+	//sls << x;
+	
+	auto mon = x*x*x;
+	
+	Rational r = 4;
+	
+	carl::UnivariatePolynomial<Rational> up(x, {r*r*r, r*r, r});
+	carl::MultivariatePolynomial<Rational> mp = Rational(r*r)*x*x + r*x + r;
+
+	sls << mp << std::endl;;
+	std::cout << mp << std::endl;
+
+
+}
