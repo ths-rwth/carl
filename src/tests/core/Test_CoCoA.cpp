2ee48c2027a68d7b3f63e38fef436a38a5ca71f5
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2016-11-17 14:01:23 +0100

    Added test case for CoCoAAdaptor::factorize()

--- src/tests/core/Test_CoCoA.cpp
+++ src/tests/core/Test_CoCoA.cpp
@@ -29,19 +29,51 @@ TEST(CoCoA, Basic)
 	EXPECT_EQ(x[0]+1, g);
 }
 
-TEST(CoCoA, Adaptor)
+TEST(CoCoA, GCD)
 {
 	using Poly = MultivariatePolynomial<mpq_class>;
+	CoCoA::GlobalManager CoCoAFoundations;
 	Variable x = freshRealVariable("x");
+	
 	Poly p1 = x*x - mpq_class(1);
 	Poly p2 = (x+mpq_class(1)) * (x-mpq_class(2));
 	Poly p3 = x+mpq_class(1);
-	
-	CoCoA::GlobalManager CoCoAFoundations;
 	
 	CoCoAAdaptor<Poly> c({p1, p2});
 	Poly q = c.gcd(p1, p2);
 	EXPECT_EQ(p3, q);
 }
 
+TEST(CoCoA, Factorize)
+{
+	using Poly = MultivariatePolynomial<mpq_class>;
+	CoCoA::GlobalManager CoCoAFoundations;
+	Variable x = freshRealVariable("x");
+	
+	Poly p1 = x*x - mpq_class(1);
+	Poly p2 = (x+mpq_class(1)) * (x-mpq_class(2));
+	Poly q1 = x+mpq_class(1);
+	Poly q2 = x-mpq_class(1);
+	Poly q3 = x-mpq_class(2);
+	
+	CoCoAAdaptor<Poly> c({p1, p2});
+	{
+		auto res = c.factorize(p1);
+		auto it1 = res.find(q1);
+		EXPECT_FALSE(it1 == res.end());
+		EXPECT_EQ(it1->second, 1);
+		auto it2 = res.find(q2);
+		EXPECT_FALSE(it2 == res.end());
+		EXPECT_EQ(it2->second, 1);
+	}
+	{
+		auto res = c.factorize(p2);
+		auto it1 = res.find(q1);
+		EXPECT_FALSE(it1 == res.end());
+		EXPECT_EQ(it1->second, 1);
+		auto it2 = res.find(q3);
+		EXPECT_FALSE(it2 == res.end());
+		EXPECT_EQ(it2->second, 1);
+	}
+}
 #endif
