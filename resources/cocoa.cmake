ac2c907af071deb5c5ed28d7205027623a1e46f8
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2016-11-18 13:48:39 +0100

    Added proper dependencies for shipped resources, passing gmp.a to cocoa.

--- resources/cocoa.cmake
+++ resources/cocoa.cmake
@@ -1,9 +1,11 @@
+get_target_property(GMP_LIB GMP_STATIC IMPORTED_LOCATION)
+
 ExternalProject_Add(
     CoCoALib
 	URL "http://cocoa.dima.unige.it/cocoalib/tgz/CoCoALib-${COCOA_VERSION}.tgz"
 	URL_MD5 19be2e8a20b1cc274dbfd69be7807a95
 	BUILD_IN_SOURCE YES
-	CONFIGURE_COMMAND ./configure --no-readline --threadsafe-hack --prefix=<INSTALL_DIR>
+	CONFIGURE_COMMAND ./configure --no-readline --threadsafe-hack --prefix=<INSTALL_DIR> --with-libgmp=${GMP_LIB}
 #	PATCH_COMMAND patch configure ${CMAKE_SOURCE_DIR}/resources/cocoa/configure_simple.patch
 	PATCH_COMMAND patch --forward configure ${CMAKE_SOURCE_DIR}/resources/cocoa/configure.patch
 		COMMAND patch --forward configuration/gmp-check-cxxflags.sh ${CMAKE_SOURCE_DIR}/resources/cocoa/configuration/gmp-check-cxxflags.sh.patch
@@ -21,4 +23,7 @@ ExternalProject_Get_Property(CoCoALib source_dir)
 add_imported_library(COCOA SHARED "${source_dir}/lib/libcocoa.a" "${source_dir}/include")
 add_imported_library(COCOA STATIC "${source_dir}/lib/libcocoa.a" "${source_dir}/include")
 
-add_dependencies(resources CoCoALib)
+add_dependencies(CoCoALib GMP_STATIC)
+add_dependencies(COCOA_SHARED CoCoALib)
+add_dependencies(COCOA_STATIC CoCoALib)
+add_dependencies(resources COCOA_SHARED COCOA_STATIC)
