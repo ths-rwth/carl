1ad149f9dcb173dbb3687872b6ced6ba6c413f3e
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2016-11-21 17:39:31 +0100

    Added test for Timer.

---
+++ src/tests/util/Test_Timer.cpp
@@ -0,0 +1,20 @@
+#include "gtest/gtest.h"
+
+#include "carl/util/Timer.h"
+
+#include <chrono>
+#include <thread>
+
+using namespace carl;
+
+TEST(Timer, Basics)
+{
+	carl::Timer t;
+	std::this_thread::sleep_for(std::chrono::milliseconds(50));
+	EXPECT_TRUE(t.passed() >= 50);
+	EXPECT_TRUE(t.passed() < 75);
+	t.reset();
+	std::this_thread::sleep_for(std::chrono::milliseconds(50));
+	EXPECT_TRUE(t.passed() >= 50);
+	EXPECT_TRUE(t.passed() < 75);
+}
