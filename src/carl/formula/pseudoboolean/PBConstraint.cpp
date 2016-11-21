8ba36b71e81c1f74c04bea16d4f23db55f061584
mg632769 <marta.grobelna@rwth-aachen.de>
2016-11-21 13:56:24 +0100

    Update PBConstraint.cpp

--- src/carl/formula/pseudoboolean/PBConstraint.cpp
+++ src/carl/formula/pseudoboolean/PBConstraint.cpp
@@ -1,53 +1,29 @@
+
 #include "PBConstraint.h"
 
+namespace carl {
 
-PBConstraint::PBConstraint( std::list< std::pair<bool, int> >  ls, carl::Relation rel, int rs){
- this->lhs = ls;
- this->relation = rel;
- this->rhs = rs;
- }
+    PBConstraint::PBConstraint(const std::vector< std::pair<Variable, int> >&  ls, carl::Relation rel, int rs):
+     lhs(ls),
+     relation(rel),
+     rhs(rs)
+    {}
 
-std::list<bool> PBConstraint::getVariables(){
-    std::list<bool> varList;
-    for(auto i = this->lhs.begin(); i != this->lhs.end(); ++i){
-        varList.push_back(i->first);
-    }
-    return varList;
-}
-
-PBConstraint PBConstraint::negateConst(){
-    carl::Relation nRel = inverse(this->relation);
-    PBConstraint negConst(this->lhs, nRel, this->rhs);
-    return negConst;
-}
-
-template<typename Rational, typename Poly>
-void PBConstraint::substituteIn(PBConstraint& c, const carl::Model<Rational,Poly>& m){
-    int newRHS = c.rhs;
-    std::list< std::pair<bool, int> > constLHS = c.lhs;
-    
-    for(auto i = m.begin(); i != m.end(); ++i){
-        if(i->second.asBool()){
-            //Look for the variable i->first in the lhs.
-            for(auto j = constLHS.begin(); j != constLHS.end(); ++j){
-                if(j->first == i->first){
-                    newRHS -= j->second;
-                    constLHS.erase(j);
-                }
-            }
+    std::vector<Variable> PBConstraint::getVariables(){
+        std::vector<Variable> varVector;
+        for(auto i = this->lhs.begin(); i != this->lhs.end(); ++i){
+            varVector.push_back(i->first);
         }
-        
+        return varVector;
     }
-    
-}
+
+    PBConstraint PBConstraint::negateConst(){
+        carl::Relation nRel = inverse(this->relation);
+        PBConstraint negConst(this->lhs, nRel, this->rhs);
+        return negConst;
+    }
 
 
-template<typename Rational, typename Poly>
-void PBConstraint::evaluate(carl::ModelValue<Rational,Poly>& res, PBConstraint& c, const
-                            carl::Model<Rational,Poly>& m){
-    
-//TODO
-    
-    
 }
+
 
