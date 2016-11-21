39e53e97543b2bb00b4cbfc6198b8c28772c85c5
mg632769 <marta.grobelna@rwth-aachen.de>
2016-11-21 20:26:59 +0100

    Removed some bugs.

--- src/carl/formula/pseudoboolean/PBConstraint.h
+++ src/carl/formula/pseudoboolean/PBConstraint.h
@@ -6,51 +6,77 @@
 
 namespace carl {
    
-
     class PBConstraint{
-        
-        
-    public:
-        PBConstraint(const std::vector< std::pair<Variable, int> >& , carl::Relation, int);
-        std::vector<Variable> getVariables();
-        PBConstraint negate();
-
-        template<typename Rational, typename Poly>
-        void substituteIn(PBConstraint& c, const carl::Model<Rational,Poly>& m);
-      
-        template<typename Rational, typename Poly, typename Number>
-        void evaluate(carl::ModelValue<Rational,Poly>& res, PBConstraint& c, const
-                      carl::Model<Rational,Poly>& m);
-
-        carl::Relation relation;
+    private:
+        Relation relation;
         int rhs;
         std::vector< std::pair<Variable, int> > lhs;
         
+    public:
+        PBConstraint();
+        PBConstraint(const std::vector< std::pair<Variable, int> >& , Relation, int);
+        std::vector<Variable> getVariables();
+        PBConstraint negate();
+        void setLHS(const std::vector< std::pair<Variable, int> >&);
+        void setRelation(Relation);
+        void setRHS(int);
+        std::vector< std::pair<Variable, int>> getLHS();
+        Relation getRelation();
+        int getRHS();
+        template<typename Rational, typename Poly>
+        void substituteIn(PBConstraint& c, const Model<Rational,Poly>& m);
+        template<typename Rational, typename Poly, typename Number>
+        void evaluate(ModelValue<Rational,Poly>& res, PBConstraint& c, const
+                      Model<Rational,Poly>& m);
     };
  
+    PBConstraint::PBConstraint(){}
     
-    PBConstraint::PBConstraint(const std::vector< std::pair<Variable, int> >& ls, carl::Relation rel, int rs):
+    PBConstraint::PBConstraint(const std::vector< std::pair<Variable, int> >& ls, Relation rel, int rs):
     lhs(ls),
     relation(rel),
     rhs(rs)
     {}
     
-    
     std::vector<Variable> PBConstraint::getVariables(){
         std::vector<Variable> varVector;
-        for(auto i = this->lhs.begin(); i != this->lhs.end(); ++i){
-            varVector.push_back(i->first);
+        std::vector<std::pair<Variable, int>> ls = this->lhs;
+        for(auto it = ls.begin(); it != ls.end(); ++it){
+            varVector.push_back(it->first);
         }
         return varVector;
     }
-    
+
     PBConstraint PBConstraint::negate(){
-        carl::Relation nRel = inverse(this->relation);
+        Relation nRel = inverse(this->relation);
         PBConstraint negConst(this->lhs, nRel, this->rhs);
         return negConst;
     }
-    
 
+    std::vector< std::pair<Variable, int>> PBConstraint::getLHS(){
+        return this->lhs;
+    }
+    
+    Relation PBConstraint::getRelation(){
+        return this->relation;
+    }
+    
+    int PBConstraint::getRHS(){
+        return this->rhs;
+    }
+
+    void PBConstraint::setLHS(const std::vector< std::pair<Variable, int> >& ls){
+        this->lhs = ls;
+    }
+    
+    void PBConstraint::setRelation(Relation r){
+        this->relation = r;
+    }
+    
+    void PBConstraint::setRHS(int rs){
+        this->rhs = rs;
+    }
+        
     
 }
 #include "PBConstraint.tpp"
--- src/carl/formula/pseudoboolean/PBConstraint.tpp
+++ src/carl/formula/pseudoboolean/PBConstraint.tpp
@@ -1,36 +1,33 @@
 namespace carl {
 
-  template<typename Rational, typename Poly>
+template<typename Rational, typename Poly>
 void substituteIn(PBConstraint& c, const carl::Model<Rational, Poly>& m){
-        for(auto i = c.lhs.begin(); i != c.lhs.end();){
-            auto it = m.find(i->first);
-            //Look if model contains this variable.
-            if(it != m.end()){
-                //Check if the variable is a bool.
-                if(it->second.isBool()){
-                    //If the variable is true, substract the factor from the rhs.
-                    if(it->second == true){
-                    c.rhs -= i->second;
+    std::vector<std::pair<Variable, int>> newLHS;
+    for(auto it = c.getLHS().begin(); it != c.getLHS().end(); it++){
+        auto element = m.find(it->first);
+        if(element != m.end()){
+            if(element->second.isBool()){
+                if(element->second == true){
+                    c.setRHS(c.getRHS() - it->second);
                 }
-                //Remove the monomial form the constraint.
-                i = c.lhs.erase(i);
-                }else{
-                    i++;
-                }
+                it = c.getLHS().erase(it);
             }
-
+        }else{
+            newLHS.push_back(std::make_pair(it->first, it->second));
         }
+    }
+    c.setLHS(newLHS);
 }
 
 
-    template<typename Rational, typename Poly, typename Number>
+
+template<typename Rational, typename Poly, typename Number>
 void evaluate(carl::ModelValue<Rational,Poly>& res, PBConstraint& c, const
-    carl::Model<Rational,Poly>& m){
+  /*  carl::Model<Rational,Poly>& m){
     substituteIn(c, m);
     //Call evaluate function from Constraint.h
-    if(res.isBool()){
-        res = evaluate(Number(c.rhs), c.relation);
-    }
+    evaluate(Number(c.getRHS()), c.getRelation());
+    */
 }
 
 }
