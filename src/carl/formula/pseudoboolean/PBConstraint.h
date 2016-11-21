b4c9209804f8e48b01bc9a2e99c4928bfda8a5ed
mg632769 <marta.grobelna@rwth-aachen.de>
2016-11-21 12:46:22 +0100

    Removed bugs from substituteIn.

--- src/carl/formula/pseudoboolean/PBConstraint.h
+++ src/carl/formula/pseudoboolean/PBConstraint.h
@@ -1,32 +1,56 @@
-#include <list>
+#include <vector>
 #include <utility>
 #include "../../core/Relation.h"
 #include "../model/Model.h"
+#include "../Constraint.h"
 
-class PBConstraint{
+namespace carl {
+   
+
+    class PBConstraint{
+        
+        
+    public:
+        PBConstraint(const std::vector< std::pair<Variable, int> >& , carl::Relation, int);
+        std::vector<Variable> getVariables();
+        PBConstraint negate();
+
+        template<typename Rational, typename Poly>
+        void substituteIn(PBConstraint& c, const carl::Model<Rational,Poly>& m);
+      
+        template<typename Rational, typename Poly, typename Number>
+        void evaluate(carl::ModelValue<Rational,Poly>& res, PBConstraint& c, const
+                      carl::Model<Rational,Poly>& m);
+
+        carl::Relation relation;
+        int rhs;
+        std::vector< std::pair<Variable, int> > lhs;
+        
+    };
+ 
+    
+    PBConstraint::PBConstraint(const std::vector< std::pair<Variable, int> >& ls, carl::Relation rel, int rs):
+    lhs(ls),
+    relation(rel),
+    rhs(rs)
+    {}
     
     
-public:
-    PBConstraint( std::list< std::pair<bool, int> >, carl::Relation, int);
-    std::list<bool> getVariables();
-    PBConstraint negateConst();
+    std::vector<Variable> PBConstraint::getVariables(){
+        std::vector<Variable> varVector;
+        for(auto i = this->lhs.begin(); i != this->lhs.end(); ++i){
+            varVector.push_back(i->first);
+        }
+        return varVector;
+    }
     
-    template<typename Rational, typename Poly>
-    void substituteIn(PBConstraint& c, const carl::Model<Rational,Poly>& m);
+    PBConstraint PBConstraint::negate(){
+        carl::Relation nRel = inverse(this->relation);
+        PBConstraint negConst(this->lhs, nRel, this->rhs);
+        return negConst;
+    }
     
-    template<typename Rational, typename Poly>
-    void evaluate(carl::ModelValue<Rational,Poly>& res, PBConstraint& c, const
-                  carl::Model<Rational,Poly>& m);
-    
-    
-private:
-    carl::Relation relation;
-    int rhs;
-    std::list< std::pair<bool, int> > lhs;
 
     
-
-    
-};
-
-
+}
+#include "PBConstraint.tpp"
