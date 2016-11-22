c3ca877958b5381a458d03c8cf9c23c15afd4f8c
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2016-11-22 16:39:40 +0100

    Adding a test case for PBConstraint.

--- src/tests/formula/Test_PBConstraint.cpp
+++ src/tests/formula/Test_PBConstraint.cpp
@@ -1,7 +1,34 @@
 #include "gtest/gtest.h"
 
-//#include <carl/formula/pseudoboolean/PBConstraint.h>
+#include <carl/formula/pseudoboolean/PBConstraint.h>
 
 TEST(PBConstraint, Basic)
 {
+	using Rational = mpq_class;
+	using Poly = carl::MultivariatePolynomial<Rational>;
+	auto x = carl::freshBooleanVariable("x");
+	auto y = carl::freshBooleanVariable("y");
+	auto z = carl::freshBooleanVariable("z");
+	carl::PBConstraint pbc;
+	std::vector<std::pair<carl::Variable, int>> lhs;
+	lhs.emplace_back(x, 2);
+	lhs.emplace_back(y, 5);
+	lhs.emplace_back(z, 4);
+	pbc.setLHS(lhs);
+	pbc.setRHS(6);
+	pbc.setRelation(carl::Relation::GREATER);
+	
+	carl::Model<Rational, Poly> m;
+	m.emplace(x, true);
+	m.emplace(z, false);
+	
+	std::cout << pbc << std::endl;
+	pbc.substituteIn(m);
+	std::cout << pbc << std::endl;
+	
+	m.emplace(y, true);
+	carl::ModelValue<Rational,Poly> res;
+	pbc.evaluate(res, m);
+	std::cout << pbc << std::endl;
+	std::cout << res << std::endl;
 }
