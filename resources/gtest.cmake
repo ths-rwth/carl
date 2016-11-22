509e5e45c0b7c71b3b6423d611563bf8ba1f6425
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2016-11-22 11:17:04 +0100

    Building gtest only if tests are built.

--- resources/gtest.cmake
+++ resources/gtest.cmake
@@ -1,17 +1,18 @@
 ExternalProject_Add(
-    googletest
+    GTest_EP
 	GIT_REPOSITORY https://github.com/google/googletest.git
 	GIT_TAG "release-${GTEST_VERSION}"
 	INSTALL_COMMAND ""
 	BUILD_BYPRODUCTS ${CMAKE_BINARY_DIR}/resources/src/googletest-build/${CMAKE_FIND_LIBRARY_PREFIXES}gtest.a ${CMAKE_BINARY_DIR}/resources/src/googletest-build/${CMAKE_FIND_LIBRARY_PREFIXES}gtest_main.a
 )
 
-ExternalProject_Get_Property(googletest source_dir)
-ExternalProject_Get_Property(googletest binary_dir)
+ExternalProject_Get_Property(GTest_EP source_dir)
+ExternalProject_Get_Property(GTest_EP binary_dir)
 
 add_imported_library(GTESTCORE STATIC "${binary_dir}/${CMAKE_FIND_LIBRARY_PREFIXES}gtest.a" "${source_dir}/include")
 add_imported_library(GTESTMAIN STATIC "${binary_dir}/${CMAKE_FIND_LIBRARY_PREFIXES}gtest_main.a" "${source_dir}/include")
 
 set(GTEST_LIBRARIES GTESTCORE_STATIC GTESTMAIN_STATIC pthread dl)
 
-add_dependencies(resources googletest)
+add_dependencies(GTESTCORE_STATIC GTest_EP)
+add_dependencies(GTESTMAIN_STATIC GTest_EP)
