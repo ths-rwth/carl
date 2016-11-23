b8e4aa465b1845ce45947c6796467b88ac8696ce
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2016-11-23 15:36:09 +0100

    Made coverage work for gcc... maybe?

--- cmake/coverage.cmake
+++ cmake/coverage.cmake
@@ -21,7 +21,14 @@ if (COVERAGE)
 		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fprofile-arcs -ftest-coverage --coverage -g -O0")
 		set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -lgcov")
 		
-		# -> https://github.com/codecov/example-cpp11
+		add_custom_target(coverage-collect
+			COMMAND lcov --directory . --zerocounters
+			COMMAND make test
+			COMMAND lcov --directory . --base-directory . --capture -o coverage.info
+			COMMAND lcov --remove coverage.info '/usr/*' 'build/resources/*' 'src/tests/*' --output-file coverage.info
+			COMMAND genhtml coverage.info -o "coverage/"
+			WORKING_DIRECTORY ${CMAKE_BUILD_DIR}
+		)
 	endif()
 
 endif()
