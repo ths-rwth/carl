1cec2f506f76146f772568addaeaf61cd96cb7f3
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2016-11-24 09:12:56 +0100

    Make timer test safer.

--- src/tests/util/Test_Timer.cpp
+++ src/tests/util/Test_Timer.cpp
@@ -12,9 +12,7 @@ TEST(Timer, Basics)
 	carl::Timer t;
 	std::this_thread::sleep_for(std::chrono::milliseconds(50));
 	EXPECT_TRUE(t.passed() >= 50);
-	EXPECT_TRUE(t.passed() < 75);
 	t.reset();
 	std::this_thread::sleep_for(std::chrono::milliseconds(50));
 	EXPECT_TRUE(t.passed() >= 50);
-	EXPECT_TRUE(t.passed() < 75);
 }
