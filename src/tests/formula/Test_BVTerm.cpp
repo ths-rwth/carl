584efa4e37e65d287a42e06a95f80a7cce3e1a86
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2016-11-25 14:42:56 +0100

    New tests.

--- src/tests/formula/Test_BVTerm.cpp
+++ src/tests/formula/Test_BVTerm.cpp
@@ -40,10 +40,74 @@ TEST(BVTerm, TermTypeOutput)
 	EXPECT_TRUE(getOutput(carl::BVTermType::REPEAT) == "repeat");
 }
 
+TEST(BVTerm, typeIsUnary)
+{
+	EXPECT_FALSE(carl::typeIsUnary(carl::BVTermType::CONSTANT));
+	EXPECT_FALSE(carl::typeIsUnary(carl::BVTermType::VARIABLE));
+	EXPECT_FALSE(carl::typeIsUnary(carl::BVTermType::CONCAT));
+	EXPECT_FALSE(carl::typeIsUnary(carl::BVTermType::EXTRACT));
+	EXPECT_TRUE(carl::typeIsUnary(carl::BVTermType::NOT));
+	EXPECT_TRUE(carl::typeIsUnary(carl::BVTermType::NEG));
+	EXPECT_FALSE(carl::typeIsUnary(carl::BVTermType::AND));
+	EXPECT_FALSE(carl::typeIsUnary(carl::BVTermType::OR));
+	EXPECT_FALSE(carl::typeIsUnary(carl::BVTermType::XOR));
+	EXPECT_FALSE(carl::typeIsUnary(carl::BVTermType::NAND));
+	EXPECT_FALSE(carl::typeIsUnary(carl::BVTermType::NOR));
+	EXPECT_FALSE(carl::typeIsUnary(carl::BVTermType::XNOR));
+	EXPECT_FALSE(carl::typeIsUnary(carl::BVTermType::ADD));
+	EXPECT_FALSE(carl::typeIsUnary(carl::BVTermType::SUB));
+	EXPECT_FALSE(carl::typeIsUnary(carl::BVTermType::MUL));
+	EXPECT_FALSE(carl::typeIsUnary(carl::BVTermType::DIV_U));
+	EXPECT_FALSE(carl::typeIsUnary(carl::BVTermType::DIV_S));
+	EXPECT_FALSE(carl::typeIsUnary(carl::BVTermType::MOD_U));
+	EXPECT_FALSE(carl::typeIsUnary(carl::BVTermType::MOD_S1));
+	EXPECT_FALSE(carl::typeIsUnary(carl::BVTermType::MOD_S2));
+	EXPECT_FALSE(carl::typeIsUnary(carl::BVTermType::EQ));
+	EXPECT_FALSE(carl::typeIsUnary(carl::BVTermType::LSHIFT));
+	EXPECT_FALSE(carl::typeIsUnary(carl::BVTermType::RSHIFT_LOGIC));
+	EXPECT_FALSE(carl::typeIsUnary(carl::BVTermType::RSHIFT_ARITH));
+	EXPECT_TRUE(carl::typeIsUnary(carl::BVTermType::LROTATE));
+	EXPECT_TRUE(carl::typeIsUnary(carl::BVTermType::RROTATE));
+	EXPECT_TRUE(carl::typeIsUnary(carl::BVTermType::EXT_U));
+	EXPECT_TRUE(carl::typeIsUnary(carl::BVTermType::EXT_S));
+	EXPECT_TRUE(carl::typeIsUnary(carl::BVTermType::REPEAT));
+}
+
+TEST(BVTerm, typeIsBinary)
+{
+	EXPECT_FALSE(carl::typeIsBinary(carl::BVTermType::CONSTANT));
+	EXPECT_FALSE(carl::typeIsBinary(carl::BVTermType::VARIABLE));
+	EXPECT_TRUE(carl::typeIsBinary(carl::BVTermType::CONCAT));
+	EXPECT_FALSE(carl::typeIsBinary(carl::BVTermType::EXTRACT));
+	EXPECT_FALSE(carl::typeIsBinary(carl::BVTermType::NOT));
+	EXPECT_FALSE(carl::typeIsBinary(carl::BVTermType::NEG));
+	EXPECT_TRUE(carl::typeIsBinary(carl::BVTermType::AND));
+	EXPECT_TRUE(carl::typeIsBinary(carl::BVTermType::OR));
+	EXPECT_TRUE(carl::typeIsBinary(carl::BVTermType::XOR));
+	EXPECT_TRUE(carl::typeIsBinary(carl::BVTermType::NAND));
+	EXPECT_TRUE(carl::typeIsBinary(carl::BVTermType::NOR));
+	EXPECT_TRUE(carl::typeIsBinary(carl::BVTermType::XNOR));
+	EXPECT_TRUE(carl::typeIsBinary(carl::BVTermType::ADD));
+	EXPECT_TRUE(carl::typeIsBinary(carl::BVTermType::SUB));
+	EXPECT_TRUE(carl::typeIsBinary(carl::BVTermType::MUL));
+	EXPECT_TRUE(carl::typeIsBinary(carl::BVTermType::DIV_U));
+	EXPECT_TRUE(carl::typeIsBinary(carl::BVTermType::DIV_S));
+	EXPECT_TRUE(carl::typeIsBinary(carl::BVTermType::MOD_U));
+	EXPECT_TRUE(carl::typeIsBinary(carl::BVTermType::MOD_S1));
+	EXPECT_TRUE(carl::typeIsBinary(carl::BVTermType::MOD_S2));
+	EXPECT_TRUE(carl::typeIsBinary(carl::BVTermType::EQ));
+	EXPECT_TRUE(carl::typeIsBinary(carl::BVTermType::LSHIFT));
+	EXPECT_TRUE(carl::typeIsBinary(carl::BVTermType::RSHIFT_LOGIC));
+	EXPECT_TRUE(carl::typeIsBinary(carl::BVTermType::RSHIFT_ARITH));
+	EXPECT_FALSE(carl::typeIsBinary(carl::BVTermType::LROTATE));
+	EXPECT_FALSE(carl::typeIsBinary(carl::BVTermType::RROTATE));
+	EXPECT_FALSE(carl::typeIsBinary(carl::BVTermType::EXT_U));
+	EXPECT_FALSE(carl::typeIsBinary(carl::BVTermType::EXT_S));
+	EXPECT_FALSE(carl::typeIsBinary(carl::BVTermType::REPEAT));
+}
+
 TEST(BVTerm, Construction)
 {
     Variable a = freshBitvectorVariable("a");
     Variable b = freshBitvectorVariable("b");
-
-    
 }
