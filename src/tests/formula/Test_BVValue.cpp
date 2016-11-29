c57c8196160e6eb1c9d7c926ba54f668dfc28db2
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2016-11-29 17:00:06 +0100

    making constants smaller.

--- src/tests/formula/Test_BVValue.cpp
+++ src/tests/formula/Test_BVValue.cpp
@@ -15,12 +15,12 @@ protected:
 	carl::BVValue bv;
 	carl::BVValue bv32_0;
 	carl::BVValue bv32_1;
-	carl::BVValue bv32_e31;
+	carl::BVValue bv32_e30;
 	
 	BVValueTest():
 		bv32_0(32, T(0)),
 		bv32_1(32, T(1)),
-		bv32_e31(32, T((1 << 31) - 1))
+		bv32_e30(32, T((1 << 30) - 1))
 	{}
 };
 
@@ -31,16 +31,16 @@ TYPED_TEST(BVValueTest, Constructor)
 {
 	BDB bdb_0(32, 0);
 	BDB bdb_1(32, 1);
-	BDB bdb_e31(32, (1 << 31) - 1);
+	BDB bdb_e30(32, (1 << 30) - 1);
 	EXPECT_EQ(bdb_0, BDB(this->bv32_0));
 	EXPECT_EQ(bdb_1, BDB(this->bv32_1));
-	EXPECT_EQ(bdb_e31, BDB(this->bv32_e31));
+	EXPECT_EQ(bdb_e30, BDB(this->bv32_e30));
 	
 	EXPECT_EQ(32, this->bv32_0.width());
 	EXPECT_EQ(32, this->bv32_1.width());
-	EXPECT_EQ(32, this->bv32_e31.width());
+	EXPECT_EQ(32, this->bv32_e30.width());
 	
 	EXPECT_TRUE(this->bv32_0.isZero());
 	EXPECT_FALSE(this->bv32_1.isZero());
-	EXPECT_FALSE(this->bv32_e31.isZero());
+	EXPECT_FALSE(this->bv32_e30.isZero());
 }
