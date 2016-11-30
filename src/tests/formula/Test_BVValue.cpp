e5518e560cd809fc9c7c88f4ac8281c4df26771f
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2016-11-30 14:50:02 +0100

    Adding tests for BVValue.

--- src/tests/formula/Test_BVValue.cpp
+++ src/tests/formula/Test_BVValue.cpp
@@ -29,18 +29,62 @@ TYPED_TEST_CASE(BVValueTest, IntegerTypes);
 
 TYPED_TEST(BVValueTest, Constructor)
 {
-	BDB bdb_0(32, 0);
-	BDB bdb_1(32, 1);
-	BDB bdb_e30(32, (1 << 30) - 1);
-	EXPECT_EQ(bdb_0, BDB(this->bv32_0));
-	EXPECT_EQ(bdb_1, BDB(this->bv32_1));
-	EXPECT_EQ(bdb_e30, BDB(this->bv32_e30));
-	
+	EXPECT_EQ(BDB(32, 0), BDB(this->bv32_0));
+	EXPECT_EQ(BDB(32, 1), BDB(this->bv32_1));
+	EXPECT_EQ(BDB(32, 1073741823), BDB(this->bv32_e30));
+}
+
+TYPED_TEST(BVValueTest, width)
+{
 	EXPECT_EQ(32, this->bv32_0.width());
 	EXPECT_EQ(32, this->bv32_1.width());
-	EXPECT_EQ(32, this->bv32_e30.width());
-	
+	EXPECT_EQ(32, this->bv32_e30.width());	
+}
+
+TYPED_TEST(BVValueTest, toString)
+{
+	EXPECT_EQ("#b00000000000000000000000000000000", this->bv32_0.toString());
+	EXPECT_EQ("#b00000000000000000000000000000001", this->bv32_1.toString());
+	EXPECT_EQ("#b00111111111111111111111111111111", this->bv32_e30.toString());
+}
+
+TYPED_TEST(BVValueTest, isZero)
+{
 	EXPECT_TRUE(this->bv32_0.isZero());
 	EXPECT_FALSE(this->bv32_1.isZero());
 	EXPECT_FALSE(this->bv32_e30.isZero());
+}
+
+TYPED_TEST(BVValueTest, UnaryMinus)
+{
+	EXPECT_EQ(carl::BVValue(32, 0), -this->bv32_0);
+	EXPECT_EQ(carl::BVValue(32, -1), -this->bv32_1);
+	EXPECT_EQ(carl::BVValue(32, -1073741823), -this->bv32_e30);
+}
+
+TYPED_TEST(BVValueTest, BinaryInversion)
+{
+	EXPECT_EQ(carl::BVValue(32, 4294967295), ~this->bv32_0);
+	EXPECT_EQ(carl::BVValue(32, 4294967294), ~this->bv32_1);
+	EXPECT_EQ(carl::BVValue(32, 3221225472), ~this->bv32_e30);
+}
+
+TYPED_TEST(BVValueTest, rotateLeft)
+{
+	EXPECT_EQ(carl::BVValue(32, 0), this->bv32_0.rotateLeft(1));
+	EXPECT_EQ(carl::BVValue(32, 0), this->bv32_0.rotateLeft(5));
+	EXPECT_EQ(carl::BVValue(32, 2), this->bv32_1.rotateLeft(1));
+	EXPECT_EQ(carl::BVValue(32, 32), this->bv32_1.rotateLeft(5));
+	EXPECT_EQ(carl::BVValue(32, 2147483646), this->bv32_e30.rotateLeft(1));
+	EXPECT_EQ(carl::BVValue(32, 4294967271), this->bv32_e30.rotateLeft(5));
+}
+
+TYPED_TEST(BVValueTest, rotateRight)
+{
+	EXPECT_EQ(carl::BVValue(32, 0), this->bv32_0.rotateRight(1));
+	EXPECT_EQ(carl::BVValue(32, 0), this->bv32_0.rotateRight(5));
+	EXPECT_EQ(carl::BVValue(32, 2147483648), this->bv32_1.rotateRight(1));
+	EXPECT_EQ(carl::BVValue(32, 134217728), this->bv32_1.rotateRight(5));
+	EXPECT_EQ(carl::BVValue(32, 2684354559), this->bv32_e30.rotateRight(1));
+	EXPECT_EQ(carl::BVValue(32, 4194303999), this->bv32_e30.rotateRight(5));
 }
