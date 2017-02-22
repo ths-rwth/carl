91f5b8c6984b0c11d42496afb131f8a2638347db
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2017-02-22 19:06:50 +0100

    Make parsing work for more number formats, including 123.0 for integers.

--- src/carl/numbers/parser/parser.h
+++ src/carl/numbers/parser/parser.h
@@ -9,6 +9,8 @@
 #include <boost/spirit/include/qi.hpp>
 #include <boost/spirit/include/qi_parse.hpp>
 
+#include "../typetraits.h"
+
 namespace carl {
 namespace parser {
 
@@ -16,6 +18,21 @@ namespace parser {
 	namespace px = boost::phoenix;
 
 	using Skipper = qi::space_type;
+	
+	template<bool is_int> struct isDivisible {};
+	template<> struct isDivisible<true> {
+		template<typename Attr>
+		bool operator()(const Attr& n, std::size_t exp) {
+			std::cout << "Checking mod(" << n << ", " << Attr(carl::pow(Attr(10), exp)) << ")" << std::endl;
+			return isZero(carl::mod(n, Attr(carl::pow(Attr(10), exp))));
+		}
+	};
+	template<> struct isDivisible<false> {
+		template<typename Attr>
+		bool operator()(const Attr&, std::size_t) {
+			return true;
+		}
+	};
 
 	/**
 	 * Specialization of qi::real_policies for our rational types.
@@ -23,10 +40,48 @@ namespace parser {
 	 */
 	template<typename T>
 	struct RationalPolicies : qi::real_policies<T> {
+		static constexpr bool T_is_int = carl::is_subset_of_integers<T>::value;
+		static constexpr bool allow_leading_dot = true;
+		static constexpr bool allow_trailing_dot = true;
+		static constexpr bool expect_dot = false;
+		
+		template <typename It>
+	    static bool parse_dot(It& first, const It& last) {
+			return qi::real_policies<T>::parse_dot(first, last);
+		}
+		template <typename It, typename Attr>
+		static bool parse_frac_n(It& first, const It& last, Attr& attr, int& frac_digits) {
+			It save = first;
+			if (T_is_int) {
+				Attr local_attr;
+				int local_frac_digits;
+				bool res = qi::real_policies<T>::parse_frac_n(first, last, local_attr, local_frac_digits);
+				if (!res) return false;
+				assert(frac_digits >= 0);
+				if (!isZero(local_attr)) {
+					first = save;
+					return false;
+				}
+				frac_digits = 0;
+				return true;
+			}
+			return qi::real_policies<T>::parse_frac_n(first, last, attr, frac_digits);
+		}
 	    template <typename It, typename Attr>
-	    static bool parse_nan(It&, It const&, Attr&) { return false; }
+		static bool parse_exp_n(It& first, const It& last, Attr& attr_) {
+			bool res = qi::real_policies<T>::parse_exp_n(first, last, attr_);
+			if (!res) return false;
+			if (T_is_int) return attr_ >= 0;
+			return true;
+		}
+		template <typename It, typename Attr>
+	    static bool parse_nan(It&, const It&, Attr&) {
+			return false;
+		}
 	    template <typename It, typename Attr>
-	    static bool parse_inf(It&, It const&, Attr&) { return false; }
+	    static bool parse_inf(It&, const It&, Attr&) {
+			return false;
+		}
 	};
 
 	/// Parses (signed) integers
@@ -51,23 +106,34 @@ namespace parser {
 		}
 	};
 	
-	template<typename T>
-	bool parseInteger(const std::string& input, T& output) {
-		return parse(input.begin(), input.end(), IntegerParser<T>(), output);
-	}
-	template<typename T>
-	bool parseDecimal(const std::string& input, T& output) {
-		return parse(input.begin(), input.end(), DecimalParser<T>(), output);
-	}
-	template<typename T>
-	bool parseRational(const std::string& input, T& output) {
-		Skipper s;
+	template<typename Parser, typename T>
+	bool parse_impl(const std::string& input, T& output) {
 		auto it = input.begin();
-		bool res = phrase_parse(it, input.end(), RationalParser<T>(), s, output);
-		if (res) {
+		if (parse(it, input.end(), Parser(), output)) {
 			return it == input.end();
 		}
 		return false;
+	}
+	template<typename Parser, typename T, typename S>
+	bool parse_impl(const std::string& input, T& output, const S& skipper) {
+		auto it = input.begin();
+		if (phrase_parse(it, input.end(), Parser(), skipper, output)) {
+			return it == input.end();
+		}
+		return false;
+	}
+	
+	template<typename T>
+	bool parseInteger(const std::string& input, T& output) {
+		return parse_impl<DecimalParser<T>>(input, output);
+	}
+	template<typename T>
+	bool parseDecimal(const std::string& input, T& output) {
+		return parse_impl<DecimalParser<T>>(input, output);
+	}
+	template<typename T>
+	bool parseRational(const std::string& input, T& output) {
+		return parse_impl<RationalParser<T>>(input, output, Skipper());
 	}
 }
 }
