f25af68ca3e05b32a3718cc9a239f91778258ca9
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2017-02-22 14:35:21 +0100

    Adding a unit test for SMTLIBStream.

--- src/tests/core/Test_SMTLIBStream.cpp
+++ src/tests/core/Test_SMTLIBStream.cpp
@@ -1,28 +1,19 @@
 #include "gtest/gtest.h"
 
 #include "carl/core/VariablePool.h"
+#include "carl/formula/Formula.h"
 #include "carl/io/SMTLIBStream.h"
 
 #include "../Common.h"
 
+using FormulaT = carl::Formula<carl::MultivariatePolynomial<Rational>>;
 
 TEST(SMTLIBStream, Base)
 {
-
 	carl::Variable x = carl::freshRealVariable("x");
-	carl::SMTLIBStream sls;
-	//sls << x;
-	
-	auto mon = x*x*x;
-	
 	Rational r = 4;
-	
-	carl::UnivariatePolynomial<Rational> up(x, {r*r*r, r*r, r});
 	carl::MultivariatePolynomial<Rational> mp = Rational(r*r)*x*x + r*x + r;
 
-	sls << mp << std::endl;
-	std::cout << sls << std::endl;
-	std::cout << mp << std::endl;
-
-
+	FormulaT f(mp, carl::Relation::GEQ);
+	std::cout << outputSMTLIB(carl::Logic::QF_NRA, {f}) << std::endl;
 }
