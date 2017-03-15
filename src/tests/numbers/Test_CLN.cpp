8c5f6e43552246be808d49e585a3114a03cf7298
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2017-03-15 10:54:51 +0100

    Removing parse tests from CLN-specific test case.

--- src/tests/numbers/Test_CLN.cpp
+++ src/tests/numbers/Test_CLN.cpp
@@ -7,9 +7,6 @@ TEST(CLN, Rationalize)
 {
     EXPECT_TRUE( carl::rationalize<cln::cl_RA>(carl::toDouble(cln::cl_RA(1)/cln::cl_RA(3))) != cln::cl_RA(1)/cln::cl_RA(3) );
     EXPECT_TRUE( carl::rationalize<cln::cl_RA>(carl::toDouble(cln::cl_RA(1)/cln::cl_RA(20))) != cln::cl_RA(1)/cln::cl_RA(20) );
-    
-    EXPECT_TRUE(carl::parse<cln::cl_RA>("123") == cln::cl_RA(123));
-    EXPECT_TRUE(carl::parse<cln::cl_RA>("1e3") == cln::cl_RA(1000));
 }
 #endif
 #endif
