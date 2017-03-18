83775c9d5ef984fe257d97cdecdcdb4903737b38
Gereon Kremer <gereon.kremer@rwth-aachen.de>
2017-03-18 12:27:37 +0100

    Added invalid_value<>() to generate an invalid enum value.

--- src/tests/Common.h
+++ src/tests/Common.h
@@ -6,6 +6,8 @@
 #include <carl/numbers/numbers.h>
 
 #include <gtest/gtest.h>
+#include <limits>
+#include <type_traits>
 
 using Rational = mpq_class;
 //using Integer = mpz_class;
@@ -61,4 +63,9 @@ template<typename T, typename TT>
 void require_type(const TT& tt) {
 	T t = tt;
 	(void)(t);
+}
+
+template<typename T>
+constexpr T invalid_value() {
+	return T(std::numeric_limits<typename std::underlying_type<T>::type>::max());
 }
