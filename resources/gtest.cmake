3ad8f13172496acb6685bfa35778261bb8439124
Gereon Kremer <gereon.kremer@rwth-aachen.de>
2017-03-18 16:07:22 +0100

    Migrating to gtest 1.8

--- resources/gtest.cmake
+++ resources/gtest.cmake
@@ -3,6 +3,7 @@ ExternalProject_Add(
 	GIT_REPOSITORY https://github.com/google/googletest.git
 	GIT_TAG "release-${GTEST_VERSION}"
 	UPDATE_COMMAND ""
+	BUILD_COMMAND make gtest gtest_maing
 	INSTALL_COMMAND ""
 	BUILD_BYPRODUCTS ${CMAKE_BINARY_DIR}/resources/src/GTest_EP-build/${CMAKE_FIND_LIBRARY_PREFIXES}gtest.a ${CMAKE_BINARY_DIR}/resources/src/GTest_EP-build/${CMAKE_FIND_LIBRARY_PREFIXES}gtest_main.a
 )
@@ -10,8 +11,8 @@ ExternalProject_Add(
 ExternalProject_Get_Property(GTest_EP source_dir)
 ExternalProject_Get_Property(GTest_EP binary_dir)
 
-add_imported_library(GTESTCORE STATIC "${binary_dir}/${CMAKE_FIND_LIBRARY_PREFIXES}gtest${STATIC_EXT}" "${source_dir}/include")
-add_imported_library(GTESTMAIN STATIC "${binary_dir}/${CMAKE_FIND_LIBRARY_PREFIXES}gtest_main${STATIC_EXT}" "${source_dir}/include")
+add_imported_library(GTESTCORE STATIC "${binary_dir}/googlemock/gtest/${CMAKE_FIND_LIBRARY_PREFIXES}gtest${STATIC_EXT}" "${source_dir}/googletest/include")
+add_imported_library(GTESTMAIN STATIC "${binary_dir}/googlemock/gtest/${CMAKE_FIND_LIBRARY_PREFIXES}gtest_main${STATIC_EXT}" "${source_dir}/googletest/include")
 
 set(GTEST_LIBRARIES GTESTCORE_STATIC GTESTMAIN_STATIC pthread dl)
 
