41d575d2f2c3f80db5a61eb8f2d43a2bd605975f
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2017-04-12 09:05:15 +0200

    Enforce installation of gcc5.

--- .travis/setup_osx.sh
+++ .travis/setup_osx.sh
@@ -15,7 +15,7 @@ if [[ ${USE} == "g++-4.8" ]]; then
 	#install gcc-4.8 g++-4.8
 	#defCXX gcc-4.8 g++-4.8
 elif [[ ${USE} == "g++-5" ]]; then
-	install gcc@5
+	install --overwrite gcc@5
 	defCXX gcc-5 g++-5
 elif [[ ${USE} == "clang++-3.4" ]]; then
 	echo "clang++-3.4 is not supported"
