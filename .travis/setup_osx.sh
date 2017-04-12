29d203535a264b90a8edd18f8f4e88cd7687a036
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2017-04-12 15:48:26 +0200

    Adding setup for gcc6 on travis.

--- .travis/setup_osx.sh
+++ .travis/setup_osx.sh
@@ -18,6 +18,10 @@ elif [[ ${USE} == "g++-5" ]]; then
 	install gcc@5
 	brew link --overwrite gcc@5
 	defCXX gcc-5 g++-5
+elif [[ ${USE} == "g++-6" ]]; then
+	install gcc@6
+	brew link --overwrite gcc@6
+	defCXX gcc-6 g++-6
 elif [[ ${USE} == "clang++-3.4" ]]; then
 	echo "clang++-3.4 is not supported"
 	#install clang-3.4
