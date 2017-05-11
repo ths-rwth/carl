2c1daf2380f94f6faa987b0162d78d537ce36cbe
Sebastian Junges <sebastian.junges@rwth-aachen.de>
2017-05-11 16:54:35 +0200

    VariableType from and to string

--- src/carl/core/Variable.cpp
+++ src/carl/core/Variable.cpp
@@ -1,11 +1,30 @@
 #include "Variable.h"
 #include "VariablePool.h"
 
+#include <stdexcept>
+
 namespace carl
 {
 	const Variable Variable::NO_VARIABLE = Variable();
 	
 	std::string Variable::getName() const {
 		return carl::VariablePool::getInstance().getName(*this, true);
+	}
+
+	VariableType variableTypeFromString(const std::string& in) {
+		if(in == "Real") {
+			return VariableType::VT_REAL;
+		} else if(in == "Int") {
+			return VariableType::VT_INT;
+		} else if(in == "Bool") {
+			return VariableType::VT_BOOL;
+		} else if(in == "Bitvector") {
+			return VariableType ::VT_BITVECTOR;
+		} else if(in == "Uninterpreted") {
+			return VariableType::VT_UNINTERPRETED;
+		} else {
+			throw std::invalid_argument("Input string does not encode an input string");
+		}
+
 	}
 }
