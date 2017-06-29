4bbdaeb1a35ab116215bc27616945897a977db3e
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2017-06-29 12:54:42 +0200

    Make CodeWriter use debug routines instead of providing its own.

--- src/carl/io/CodeWriter.h
+++ src/carl/io/CodeWriter.h
@@ -5,32 +5,16 @@
 
 #pragma once
 
-#include <cxxabi.h>
 #include <iostream>
 #include <set>
-#include <typeinfo>
 #include <utility>
 #include <vector>
 
 
 #include "../core/Monomial.h"
+#include "../util/debug.h"
 
 namespace carl {
-
-inline std::string demangle(const char* name) {
-	int status = -4; // some arbitrary value to eliminate the compiler warning
-	// enable c++11 by passing the flag -std=c++11 to g++
-	std::unique_ptr<char, void(*)(void*)> res {
-		abi::__cxa_demangle(name, NULL, NULL, &status),
-		std::free
-	};
-	return (status == 0) ? res.get() : name ;
-}
-template<typename T>
-std::string typeString() {
-	const char* name = typeid(T).name();
-	return demangle(name);
-}
 
 struct ConstructorPrinter {
 	void operator()(std::ostream& os, const mpq_class& n) {
