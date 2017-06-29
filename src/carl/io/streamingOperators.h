c812678824e45c1535dbb3f2e42431ba15a067e4
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2017-06-29 12:30:19 +0200

    Made streaming operators inline.

--- src/carl/io/streamingOperators.h
+++ src/carl/io/streamingOperators.h
@@ -22,25 +22,25 @@ namespace carl {
 
 /*
 template<typename T>
-std::ostream& operator<<(std::ostream& os, const std::forward_list<T>& l);
+inline std::ostream& operator<<(std::ostream& os, const std::forward_list<T>& l);
 template<typename T>
-std::ostream& operator<<(std::ostream& os, const std::list<T>& l);
+inline std::ostream& operator<<(std::ostream& os, const std::list<T>& l);
 template<typename Key, typename Value, typename Comparator>
-std::ostream& operator<<(std::ostream& os, const std::map<Key, Value, Comparator>& m);
+inline std::ostream& operator<<(std::ostream& os, const std::map<Key, Value, Comparator>& m);
 template<typename U, typename V>
-std::ostream& operator<<(std::ostream& os, const std::pair<U, V>& p);
+inline std::ostream& operator<<(std::ostream& os, const std::pair<U, V>& p);
 template<typename T, typename C>
-std::ostream& operator<<(std::ostream& os, const std::set<T, C>& s);
+inline std::ostream& operator<<(std::ostream& os, const std::set<T, C>& s);
 template<std::size_t I, typename... T, typename std::enable_if<I == sizeof...(T), void>::type*>
-std::ostream& operator<<(std::ostream& os, const std::tuple<T...>& t);
+inline std::ostream& operator<<(std::ostream& os, const std::tuple<T...>& t);
 template<std::size_t I, typename... T, typename std::enable_if<I < sizeof...(T), void>::type*>
-std::ostream& operator<<(std::ostream& os, const std::tuple<T...>& t);
+inline std::ostream& operator<<(std::ostream& os, const std::tuple<T...>& t);
 template<typename Key, typename Value, typename H, typename E, typename A>
-std::ostream& operator<<(std::ostream& os, const std::unordered_map<Key, Value, H, E, A>& m);
+inline std::ostream& operator<<(std::ostream& os, const std::unordered_map<Key, Value, H, E, A>& m);
 template<typename T, typename H, typename K, typename A>
-std::ostream& operator<<(std::ostream& os, const std::unordered_set<T, H, K, A>& s);
+inline std::ostream& operator<<(std::ostream& os, const std::unordered_set<T, H, K, A>& s);
 template<typename T>
-std::ostream& operator<<(std::ostream& os, const std::vector<T>& v);
+inline std::ostream& operator<<(std::ostream& os, const std::vector<T>& v);
 */
 
 /**
@@ -51,7 +51,7 @@ std::ostream& operator<<(std::ostream& os, const std::vector<T>& v);
  * @return Output stream.
  */
 template<typename T>
-std::ostream& operator<<(std::ostream& os, const std::forward_list<T>& l) {
+inline std::ostream& operator<<(std::ostream& os, const std::forward_list<T>& l) {
 	os << "[";
 	bool first = true;
 	for (const auto& it: l) {
@@ -70,7 +70,7 @@ std::ostream& operator<<(std::ostream& os, const std::forward_list<T>& l) {
  * @return Output stream.
  */
 template<typename T>
-std::ostream& operator<<(std::ostream& os, const std::list<T>& l) {
+inline std::ostream& operator<<(std::ostream& os, const std::list<T>& l) {
 	os << "[" << l.size() << ": ";
 	bool first = true;
 	for (const auto& it: l) {
@@ -89,7 +89,7 @@ std::ostream& operator<<(std::ostream& os, const std::list<T>& l) {
  * @return Output stream.
  */
 template<typename Key, typename Value, typename Comparator>
-std::ostream& operator<<(std::ostream& os, const std::map<Key, Value, Comparator>& m) {
+inline std::ostream& operator<<(std::ostream& os, const std::map<Key, Value, Comparator>& m) {
 	os << "{";
 	bool first = true;
 	for (const auto& it: m) {
@@ -108,7 +108,7 @@ std::ostream& operator<<(std::ostream& os, const std::map<Key, Value, Comparator
  * @return Output stream.
  */
 template<typename U, typename V>
-std::ostream& operator<<(std::ostream& os, const std::pair<U, V>& p) {
+inline std::ostream& operator<<(std::ostream& os, const std::pair<U, V>& p) {
 	return os << "(" << p.first << ", " << p.second << ")";
 }
 
@@ -120,7 +120,7 @@ std::ostream& operator<<(std::ostream& os, const std::pair<U, V>& p) {
  * @return Output stream.
  */
 template<typename T, typename C>
-std::ostream& operator<<(std::ostream& os, const std::set<T, C>& s) {
+inline std::ostream& operator<<(std::ostream& os, const std::set<T, C>& s) {
 	os << "{" << s.size() << ": ";
 	bool first = true;
 	for (const auto& it: s) {
@@ -138,7 +138,7 @@ std::ostream& operator<<(std::ostream& os, const std::set<T, C>& s) {
  * @return Output stream.
  */
 template<std::size_t I = 0, typename... T, typename std::enable_if<I == sizeof...(T), void>::type* = nullptr>
-std::ostream& operator<<(std::ostream& os, const std::tuple<T...>& /*unused*/) {
+inline std::ostream& operator<<(std::ostream& os, const std::tuple<T...>& /*unused*/) {
 	return os << ")";
 }
 
@@ -164,7 +164,7 @@ std::ostream& operator<<(std::ostream& os, const std::tuple<T...>& t) {
  * @return Output stream.
  */
 template<typename Key, typename Value, typename H, typename E, typename A>
-std::ostream& operator<<(std::ostream& os, const std::unordered_map<Key, Value, H, E, A>& m) {
+inline std::ostream& operator<<(std::ostream& os, const std::unordered_map<Key, Value, H, E, A>& m) {
 	os << "{";
 	bool first = true;
 	for (const auto& it: m) {
@@ -183,7 +183,7 @@ std::ostream& operator<<(std::ostream& os, const std::unordered_map<Key, Value, 
  * @return Output stream.
  */
 template<typename T, typename H, typename K, typename A>
-std::ostream& operator<<(std::ostream& os, const std::unordered_set<T, H, K, A>& s) {
+inline std::ostream& operator<<(std::ostream& os, const std::unordered_set<T, H, K, A>& s) {
 	os << "{" << s.size() << ": ";
 	bool first = true;
 	for (const auto& it: s) {
@@ -202,7 +202,7 @@ std::ostream& operator<<(std::ostream& os, const std::unordered_set<T, H, K, A>&
  * @return Output stream.
  */
 template<typename T>
-std::ostream& operator<<(std::ostream& os, const std::vector<T>& v) {
+inline std::ostream& operator<<(std::ostream& os, const std::vector<T>& v) {
 	os << "[" << v.size() << ": ";
 	bool first = true;
 	for (const auto& it: v) {
