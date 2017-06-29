95019dae005f7985ef1922fb3dfb04b75e746ef1
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2017-06-29 12:43:16 +0200

    Make gtest EP windows-compatible.

--- resources/gtest.cmake
+++ resources/gtest.cmake
@@ -3,7 +3,8 @@ ExternalProject_Add(
 	GIT_REPOSITORY https://github.com/google/googletest.git
 	GIT_TAG "release-${GTEST_VERSION}"
 	UPDATE_COMMAND ""
-	BUILD_COMMAND ${CMAKE_MAKE_PROGRAM} gtest gtest_main
+	BUILD_COMMAND cmake --build . --config RELEASE --target gtest
+	COMMAND cmake --build . --config RELEASE --target gtest_main
 	INSTALL_COMMAND ""
 	BUILD_BYPRODUCTS ${CMAKE_BINARY_DIR}/resources/src/GTest_EP-build/googlemock/gtest/${CMAKE_FIND_LIBRARY_PREFIXES}gtest${STATIC_EXT} ${CMAKE_BINARY_DIR}/resources/src/GTest_EP-build/googlemock/gtest/${CMAKE_FIND_LIBRARY_PREFIXES}gtest_main${STATIC_EXT}
 )
@@ -11,10 +12,17 @@ ExternalProject_Add(
 ExternalProject_Get_Property(GTest_EP source_dir)
 ExternalProject_Get_Property(GTest_EP binary_dir)
 
-add_imported_library(GTESTCORE STATIC "${binary_dir}/googlemock/gtest/${CMAKE_FIND_LIBRARY_PREFIXES}gtest${STATIC_EXT}" "${source_dir}/googletest/include")
-add_imported_library(GTESTMAIN STATIC "${binary_dir}/googlemock/gtest/${CMAKE_FIND_LIBRARY_PREFIXES}gtest_main${STATIC_EXT}" "${source_dir}/googletest/include")
+if(WIN32)
+	add_imported_library(GTESTCORE STATIC "${binary_dir}/googlemock/gtest/Release/${CMAKE_FIND_LIBRARY_PREFIXES}gtest${STATIC_EXT}" "${source_dir}/googletest/include")
+	add_imported_library(GTESTMAIN STATIC "${binary_dir}/googlemock/gtest/Release/${CMAKE_FIND_LIBRARY_PREFIXES}gtest_main${STATIC_EXT}" "${source_dir}/googletest/include")
 
-set(GTEST_LIBRARIES GTESTCORE_STATIC GTESTMAIN_STATIC pthread dl)
+	set(GTEST_LIBRARIES GTESTCORE_STATIC GTESTMAIN_STATIC)
+else()
+	add_imported_library(GTESTCORE STATIC "${binary_dir}/googlemock/gtest/${CMAKE_FIND_LIBRARY_PREFIXES}gtest${STATIC_EXT}" "${source_dir}/googletest/include")
+	add_imported_library(GTESTMAIN STATIC "${binary_dir}/googlemock/gtest/${CMAKE_FIND_LIBRARY_PREFIXES}gtest_main${STATIC_EXT}" "${source_dir}/googletest/include")
+
+	set(GTEST_LIBRARIES GTESTCORE_STATIC GTESTMAIN_STATIC pthread dl)
+endif()
 
 add_dependencies(GTESTCORE_STATIC GTest_EP)
 add_dependencies(GTESTMAIN_STATIC GTest_EP)
