1ea4edafc360f306fd9bc024dc393eb65f4b1665
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2017-07-05 16:12:16 +0200

    Building in debug mode on appveyor.

--- .ci/setup_appveyor.cmd
+++ .ci/setup_appveyor.cmd
@@ -9,18 +9,26 @@ IF NOT EXIST mpir-3.0.0 (
 	cd mpir-3.0.0\build.vc15
 	
 	msbuild.bat gc dll x64 RELEASE > nul
+	msbuild.bat gc dll x64 DEBUG > nul
 	msbuild.bat gc lib x64 RELEASE > nul
+	msbuild.bat gc lib x64 DEBUG > nul
 	
 	cd c:\Libraries\mpir-3.0.0\dll\x64\Release
 	cp mpir.dll gmp.dll
 	cp mpir.dll gmpxx.dll
+	cd c:\Libraries\mpir-3.0.0\dll\x64\Debug
+	cp mpir.dll gmp.dll
+	cp mpir.dll gmpxx.dll
 	cd c:\Libraries\mpir-3.0.0\lib\x64\Release
+	cp mpirxx.lib gmpxx.lib
+	cp mpir.lib gmp.lib
+	cd c:\Libraries\mpir-3.0.0\lib\x64\Debug
 	cp mpir.lib gmp.lib
 	cp mpirxx.lib gmpxx.lib
 )
 
-set PATH=%PATH%;c:\Libraries\mpir-3.0.0\dll\x64\Release
-set PATH=%PATH%;c:\Libraries\mpir-3.0.0\lib\x64\Release
+set PATH=%PATH%;c:\Libraries\mpir-3.0.0\dll\x64\Debug
+set PATH=%PATH%;c:\Libraries\mpir-3.0.0\lib\x64\Debug
 
 echo Looking for preinstalled Boost
 set PATH=%PATH%;C:\Libraries\boost_1_64_0
@@ -30,8 +38,8 @@ cd c:\projects\carl
 md build
 cd build
 
-cmake -G "Visual Studio 14 2015 Win64" -DBUILD_STATIC="ON" -DBOOST_ROOT="%BOOST_ROOT%" -DBOOST_LIBRARYDIR="%BOOST_LIBRARYDIR%" -DBUILD_TYPE=RELEASE -DBoost_COMPILER="-vc141" ..
-cmake --build . --config Release --target lib_carl
-cmake --build . --config Release --target lib_carl_static
-cmake --build . --config Release --target all-tests
-cmake --build . --config Release --target RUN_TESTS
+cmake -G "Visual Studio 14 2015 Win64" -DBUILD_STATIC="ON" -DBOOST_ROOT="%BOOST_ROOT%" -DBOOST_LIBRARYDIR="%BOOST_LIBRARYDIR%" -DBUILD_TYPE=DEBUG -DBoost_COMPILER="-vc141" ..
+cmake --build . --config Debug --target lib_carl
+cmake --build . --config Debug --target lib_carl_static
+cmake --build . --config Debug --target all-tests
+cmake --build . --config Debug --target RUN_TESTS
