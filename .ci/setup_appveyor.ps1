f0c33ba413891e99b9e300f96873f93273d2ab22
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2017-07-12 09:13:45 +0200

    Fixing boost in appveyor.

--- .ci/setup_appveyor.ps1
+++ .ci/setup_appveyor.ps1
@@ -33,14 +33,14 @@ $env:PATH += ";c:\Libraries\mpir-3.0.0\dll\x64\Debug"
 $env:PATH += ";c:\Libraries\mpir-3.0.0\lib\x64\Debug"
 
 echo "Looking for preinstalled Boost"
-$env:PATH += ";%BOOST_ROOT%"
-$env:PATH += ";%BOOST_LIBRARYDIR%"
+$env:PATH += ";$BOOST_ROOT"
+$env:PATH += ";$BOOST_LIBRARYDIR"
 
 cd c:\projects\carl
 md build
 cd build
 
-cmake -G "Visual Studio 14 2015 Win64" -DBUILD_STATIC="ON" -DBOOST_ROOT="%BOOST_ROOT%" -DBOOST_LIBRARYDIR="%BOOST_LIBRARYDIR%" -DCMAKE_BUILD_TYPE=DEBUG -DBoost_COMPILER="-vc141" -DLOGGING="ON" ..
+cmake -G "Visual Studio 14 2015 Win64" -DBUILD_STATIC="ON" -DBOOST_ROOT="$BOOST_ROOT" -DBOOST_LIBRARYDIR="$BOOST_LIBRARYDIR" -DCMAKE_BUILD_TYPE=DEBUG -DBoost_COMPILER="-vc141" -DLOGGING="ON" ..
 cmake --build . --config Debug --target lib_carl
 cmake --build . --config Debug --target lib_carl_static
 cmake --build . --config Debug --target all-tests
