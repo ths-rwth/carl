15150057f89dc36331f277dc3c03bffa1b340e6e
Gereon Kremer <gereon.kremer@rwth-aachen.de>
2017-07-21 14:50:02 +0200

    Converted custom command to custom EP step.

--- resources/addons/pycarl.cmake
+++ resources/addons/pycarl.cmake
@@ -4,16 +4,17 @@ ExternalProject_Add(
     GIT_REPOSITORY https://github.com/moves-rwth/pycarl.git
     BUILD_IN_SOURCE YES
     CONFIGURE_COMMAND ""
-    BUILD_COMMAND . pycarl-env/bin/activate && python setup.py build_ext -j 1 develop
+    BUILD_COMMAND . pycarl-venv/bin/activate && python setup.py build_ext -j 1 develop
     INSTALL_COMMAND ""
 )
 
 ExternalProject_Get_Property(pycarl SOURCE_DIR)
 
-add_custom_command(
-    OUTPUT ${SOURCE_DIR}/pycarl-env/bin/activate
-    COMMAND virtualenv -p python3 pycarl-env
+ExternalProject_Add_Step(pycarl create-venv
+    COMMAND virtualenv -p python3 pycarl-venv
     WORKING_DIRECTORY ${SOURCE_DIR}
+    DEPENDERS build
+    DEPENDEES download
 )
 
 add_dependencies(pycarl carl-parser)
