88c9a2a7d4ecdc800ff4ce9707a055c2726d6f79
sjunges <sebastian.junges@gmail.com>
2017-08-22 20:17:57 +0200

    remove files that were wrongfully commited earlier

--- src/tests/io/serialization/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/sjunges/i2/carl/src/tests/io/serialization
-# Build directory: /Users/sjunges/i2/carl/src/tests/io/serialization
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(parser "/Users/sjunges/i2/carl/bin/runParserTests")
