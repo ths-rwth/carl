1997dbfeba6f27af6ef8b632d35702957526fb10
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2017-08-25 10:03:54 +0200

    Replaced local variable by persistent static variable in MultivariateRoot.

--- src/carl/formula/model/mvroot/MultivariateRoot.h
+++ src/carl/formula/model/mvroot/MultivariateRoot.h
@@ -11,9 +11,13 @@
 
 #include <algorithm>
 #include <iostream>
+#include <tuple>
 
 namespace carl {
 
+/**
+ * This class represents a root of a multivariate polynomial with respect to some assignment for the other variables.
+ */
 template<typename Poly>
 class MultivariateRoot {
 public:
@@ -21,43 +25,42 @@ public:
 	using RAN = RealAlgebraicNumber<Number>;
 	using EvalMap = RealAlgebraicNumberEvaluation::RANMap<Number>;
 private:
+	static const Variable sVar;
 	Poly mPoly;
 	std::size_t mK;
-	Variable mVar;
 public:
-	MultivariateRoot(const Poly& p, std::size_t k, Variable v):
-		mPoly(p), mK(k), mVar(v)
+	MultivariateRoot(const Poly& p, std::size_t k): mPoly(p), mK(k)
 	{
 		assert(mK > 0);
 	}
 	
+	std::size_t k() const noexcept {
+		return mK;
+	}
 	const Poly& poly() const noexcept {
 		return mPoly;
 	}
 	Poly poly(Variable var) const {
-		return mPoly.substitute(mVar, Poly(var));
+		return mPoly.substitute(sVar, Poly(var));
 	}
-	std::size_t k() const noexcept {
-		return mK;
-	}
-	Variable var() const noexcept {
-		return mVar;
+	static Variable var() noexcept {
+		return sVar;
 	}
 	bool isUnivariate() const {
 		return mPoly.isUnivariate();
 	}
 	std::set<Variable> gatherVariables() const {
 		Variables var = mPoly.gatherVariables();
-		var.erase(mVar);
+		var.erase(sVar);
 		return var;
 	}
-	void substituteIn(Variable::Arg var, const Poly& poly) {
+	void substituteIn(Variable var, const Poly& poly) {
 		mPoly.substituteIn(var, poly);
 	}
 	
 	boost::optional<RAN> evaluate(const EvalMap& m) const {
 		CARL_LOG_DEBUG("carl.multivariateroot", "Evaluating " << *this << " against " << m);
-		auto poly = mPoly.toUnivariatePolynomial(mVar);
+		auto poly = mPoly.toUnivariatePolynomial(sVar);
 		std::map<Variable, Interval<Number>> intervalMap;
 		auto unipoly = RealAlgebraicNumberEvaluation::evaluateCoefficients(poly, m, intervalMap);
 		CARL_LOG_TRACE("carl.multivariateroot", "Intermediate polynomial is " << unipoly);
@@ -72,13 +75,21 @@ public:
 	}
 	
 	bool operator==(const MultivariateRoot& mr) const {
-		return mK == mr.mK && mVar == mr.mVar && mPoly == mr.mPoly;
+		return std::tie(mK, mPoly) == std::tie(mr.mK, mr.mPoly);
+	}
+	bool operator<(const MultivariateRoot& mr) const {
+		return std::tie(mK, mPoly) < std::tie(mr.mK, mr.mPoly);
 	}
 };
 
+template<typename Poly>
+const Variable MultivariateRoot<Poly>::sVar = carl::VariablePool::getInstance().getFreshPersistentVariable("__z");
+
 template<typename P>
 std::ostream& operator<<(std::ostream& os, const MultivariateRoot<P>& mr) {
-	return os << "root(" << mr.poly() << ", " << mr.k() << ", " << mr.var() << ")";
+	return os << "root(" << mr.poly() << ", " << mr.k() << ", " << MultivariateRoot<P>::var() << " = " << mr.var().getId() << ")";
+}
+
 }
 
 }
