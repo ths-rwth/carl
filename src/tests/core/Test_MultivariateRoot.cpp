b826673636a260595962c6b3226ac22c34c368dc
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2017-09-14 10:24:02 +0200

    Another test for MVRoots.

--- src/tests/core/Test_MultivariateRoot.cpp
+++ src/tests/core/Test_MultivariateRoot.cpp
@@ -1,6 +1,9 @@
 #include "gtest/gtest.h"
 #include "carl/core/MultivariatePolynomial.h"
 #include "carl/formula/model/mvroot/MultivariateRoot.h"
+#include "carl/formula/Formula.h"
+#include "carl/formula/model/Model.h"
+#include "carl/formula/model/evaluation/ModelEvaluation.h"
 
 #include "../Common.h"
 
@@ -47,4 +50,32 @@ TYPED_TEST(MultivariateRootTest, Evaluate)
 	m.emplace(x, RealAlgebraicNumber<TypeParam>(2));
 	EXPECT_EQ(RealAlgebraicNumber<TypeParam>(-1), *mr1.evaluate(m));
 	EXPECT_EQ(RealAlgebraicNumber<TypeParam>(0), *mr2.evaluate(m));
+}
+
+TYPED_TEST(MultivariateRootTest, Evaluate2)
+{
+	// (skoSM !> root((-1)*__z*skoSP+__z^2+2*__z+(-1)*skoSP+1, 2, __z = 1)) on 
+	// {skoSP = (IR ]-5793/4096, -181/128[, __r^2 + -2 R), skoSM = (NR -1)}
+	using Poly = MultivariatePolynomial<TypeParam>;
+	using MultiRoot = MultivariateRoot<Poly>;
+	using IntervalT = Interval<TypeParam>;
+	using RANT = RealAlgebraicNumber<TypeParam>;
+	using ModelT = Model<TypeParam,Poly>;
+	
+	Variable x = freshRealVariable("x");
+	Variable y = freshRealVariable("y");
+	Variable z = MultivariateRoot<Poly>::var();
+	Poly p = -Poly(z)*x+Poly(z)*z+Poly(2)*z-x+Poly(1);
+	
+	VariableComparison<Poly> vc(y, MultiRoot(p, 2), Relation::GREATER, true);
+	Formula<Poly> f(vc);
+	
+	ModelT m;
+	IntervalT rani(Rational(-5793)/4096, BoundType::STRICT, Rational(-181)/128, BoundType::STRICT);
+	UnivariatePolynomial<Rational> ranp(x, {Rational(-2), Rational(0), Rational(1)});
+	m.assign(x, RANT(ranp, rani));
+	m.assign(y, RANT(-1));
+	
+	auto res = model::evaluate(f, m);
+	EXPECT_TRUE(res.isBool() && res.asBool());
 }
