e905baf4921a67b9c5d8c075dbe4407f7a349e1f
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2017-09-15 10:09:11 +0200

    Disabled a clang-tidy check to avoid a segfault.

--- cmake/clang-tidy.cmake
+++ cmake/clang-tidy.cmake
@@ -3,9 +3,18 @@ if(NOT CLANG_TIDY)
 	message(STATUS "Did not find clang-tidy, target tidy is disabled.")
 else()
 	message(STATUS "Found clang-tidy, use \"make tidy\" to run it.")
+	if ("${CMAKE_CXX_COMPILER_ID}" MATCHES "Clang") # Matches "Clang" and "AppleClang"
+		if (CMAKE_CXX_COMPILER_VERSION VERSION_GREATER 5)
+			message(STATUS "Please enable readability-redundant-member-init (disabled due to #32966)")
+		endif()
+	endif()
 
 	set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 	set(CLANG_TIDY_CHECKS "*")
+	
+	# Avoid https://bugs.llvm.org/show_bug.cgi?id=32966
+	set(CLANG_TIDY_CHECKS "${CLANG_TIDY_CHECKS},-readability-redundant-member-init")
+	
 	set(CLANG_TIDY_CHECKS "${CLANG_TIDY_CHECKS},-llvm-header-guard")
 	set(CLANG_TIDY_CHECKS "${CLANG_TIDY_CHECKS},-llvm-namespace-comment")
 	set(CLANG_TIDY_CHECKS "${CLANG_TIDY_CHECKS},-readability-else-after-return")
