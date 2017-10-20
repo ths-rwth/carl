f4299abd4b4cf4d025872190be61f48cceb653e7
Matthias Volk <matthias.volk@cs.rwth-aachen.de>
2017-10-20 17:09:53 +0200

    Added test case for problem in Constraint

--- src/tests/formula/Test_Formula.cpp
+++ src/tests/formula/Test_Formula.cpp
@@ -48,6 +48,8 @@ TEST(Formula, Construction)
     EXPECT_EQ( Constr( px+Rational(1), Relation::LESS ), Constr( x, Relation::LESS, -Rational(1) ) );
     EXPECT_EQ( Constr( px+Rational(1), Relation::GEQ ), Constr( x, Relation::GEQ, -Rational(1) ) );
     EXPECT_EQ( Constr( px+Rational(1), Relation::GREATER ), Constr( x, Relation::GREATER, -Rational(1) ) );
+    // Test should be fixed
+    // EXPECT_EQ( Constr( px+Rational(1), Relation::EQ ), Constr( x, Relation::EQ, -Rational(1) ) );
 
     // Uninterpreted functions are
 
