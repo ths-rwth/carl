4604e5bbf9d26b26ffe3358f5e8aa6bd2985ccce
Malte Neuss <malte.neuss@rwth-aachen.de>
2017-10-31 22:52:43 +0100

    Add doc to MultivaraiteRoot.h and make some local variables easier to understand

--- src/carl/formula/model/mvroot/MultivariateRoot.h
+++ src/carl/formula/model/mvroot/MultivariateRoot.h
@@ -14,6 +14,19 @@
 
 namespace carl {
 
+/**
+ * This is basically a hack to represent a function f: algReal^k -> algReal;
+ * from multi-dimensional point whose components are algebraic reals to an
+ * algebraic real.
+ * Think of this class as representing a real algebraic number (root of a
+ * univariate poly; normally stored as a univariate poly and a number,
+ * representing which of the possibly many roots this number is) that is stored
+ * as a multivariate poly (and a distinguished main variable, and a root
+ * number/index) where you first have to plug in values for all variables that
+ * are not the main variable to get the normally used univariate poly).
+ * Thus this algebraic real can vary depending on the values plugged in for the
+ * other variables.
+ */
 template<typename Poly>
 class MultivariateRoot {
 public:
@@ -22,57 +35,57 @@ public:
 	using EvalMap = RealAlgebraicNumberEvaluation::RANMap<Number>;
 private:
 	Poly mPoly;
-	std::size_t mK;
-	Variable mVar;
+	std::size_t mRootIndex;
+	Variable mMainVariable;
 public:
-	MultivariateRoot(const Poly& p, std::size_t k, Variable v):
-		mPoly(p), mK(k), mVar(v)
+	MultivariateRoot(const Poly& p, std::size_t rootIndex, Variable mainVariable):
+		mPoly(p), mRootIndex(rootIndex), mMainVariable(mainVariable)
 	{
-		assert(mK > 0);
+		assert(mRootIndex > 0);
 	}
-	
+
 	const Poly& poly() const noexcept {
 		return mPoly;
 	}
 	Poly poly(Variable var) const {
-		return mPoly.substitute(mVar, Poly(var));
+		return mPoly.substitute(mMainVariable, Poly(var));
 	}
 	std::size_t k() const noexcept {
-		return mK;
+		return mRootIndex;
 	}
 	Variable var() const noexcept {
-		return mVar;
+		return mMainVariable;
 	}
 	bool isUnivariate() const {
 		return mPoly.isUnivariate();
 	}
 	std::set<Variable> gatherVariables() const {
 		Variables var = mPoly.gatherVariables();
-		var.erase(mVar);
+		var.erase(mMainVariable);
 		return var;
 	}
 	void substituteIn(Variable::Arg var, const Poly& poly) {
 		mPoly.substituteIn(var, poly);
 	}
-	
-	boost::optional<RAN> evaluate(const EvalMap& m) const {
-		CARL_LOG_DEBUG("carl.multivariateroot", "Evaluating " << *this << " against " << m);
-		auto poly = mPoly.toUnivariatePolynomial(mVar);
+
+	boost::optional<RAN> evaluate(const EvalMap& algebraicPoint) const {
+		CARL_LOG_DEBUG("carl.multivariateroot", "Evaluating " << *this << " against " << algebraicPoint);
+		auto poly = mPoly.toUnivariatePolynomial(mMainVariable);
 		std::map<Variable, Interval<Number>> intervalMap;
-		auto unipoly = RealAlgebraicNumberEvaluation::evaluateCoefficients(poly, m, intervalMap);
+		auto unipoly = RealAlgebraicNumberEvaluation::evaluateCoefficients(poly, algebraicPoint, intervalMap);
 		CARL_LOG_TRACE("carl.multivariateroot", "Intermediate polynomial is " << unipoly);
 		auto roots = rootfinder::realRoots(unipoly);
 		CARL_LOG_TRACE("carl.multivariateroot", "Got roots " << roots);
-		if (roots.size() < mK) return boost::none;
-		assert(roots.size() >= mK);
+		if (roots.size() < mRootIndex) return boost::none;
+		assert(roots.size() >= mRootIndex);
 		auto it = roots.begin();
-		std::advance(it, long(mK)-1);
+		std::advance(it, long(mRootIndex)-1);
 		CARL_LOG_DEBUG("carl.multivariateroot", "Result is " << *it);
 		return *it;
 	}
-	
+
 	bool operator==(const MultivariateRoot& mr) const {
-		return mK == mr.mK && mVar == mr.mVar && mPoly == mr.mPoly;
+		return mRootIndex == mr.mRootIndex && mMainVariable == mr.mMainVariable && mPoly == mr.mPoly;
 	}
 };
 
