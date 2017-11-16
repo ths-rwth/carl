2b2e5d5117073e484c4ea58433b1ad2c085062b2
Gereon Kremer <gereon.kremer@rwth-aachen.de>
2017-11-16 16:12:21 +0100

    Refactoring CoCoA tests.

--- src/tests/core/Test_CoCoA.cpp
+++ src/tests/core/Test_CoCoA.cpp
@@ -11,50 +11,45 @@
 
 #include "CoCoA/library.H"
 
-using namespace carl;
-
-TEST(CoCoA, Basic)
-{
+TEST(CoCoA, Basic) {
 	auto xv = CoCoA::symbol("x");
 	auto yv = CoCoA::symbol("y");
 	CoCoA::ring Q = CoCoA::RingQQ();
-	
-	CoCoA::SparsePolyRing polyRing = CoCoA::NewPolyRing(Q, {xv,yv}, CoCoA::StdDegLex);
+
+	CoCoA::SparsePolyRing polyRing = CoCoA::NewPolyRing(Q, {xv, yv}, CoCoA::StdDegLex);
 	auto x = CoCoA::indets(polyRing);
-	
-	auto p = x[0]*x[0] - 1;
+
+	auto p = (x[0] * x[0]) - 1;
 	auto q = (x[0] + 1) * (x[0] - 2);
-	
+
 	auto g = CoCoA::gcd(p, q);
-	EXPECT_EQ(x[0]+1, g);
+	EXPECT_EQ(x[0] + 1, g);
 }
 
-TEST(CoCoA, GCD)
-{
-	using Poly = MultivariatePolynomial<mpq_class>;
-	Variable x = freshRealVariable("x");
-	
-	Poly p1 = x*x - mpq_class(1);
-	Poly p2 = (x+mpq_class(1)) * (x-mpq_class(2));
-	Poly p3 = x+mpq_class(1);
-	
-	CoCoAAdaptor<Poly> c({p1, p2});
+TEST(CoCoA, GCD) {
+	using Poly = carl::MultivariatePolynomial<mpq_class>;
+	carl::Variable x = carl::freshRealVariable("x");
+
+	Poly p1 = (x * x) - mpq_class(1);
+	Poly p2 = (x + mpq_class(1)) * (x - mpq_class(2));
+	Poly p3 = x + mpq_class(1);
+
+	carl::CoCoAAdaptor<Poly> c({p1, p2});
 	Poly q = c.gcd(p1, p2);
 	EXPECT_EQ(p3, q);
 }
 
-TEST(CoCoA, Factorize)
-{
-	using Poly = MultivariatePolynomial<mpq_class>;
-	Variable x = freshRealVariable("x");
-	
-	Poly p1 = x*x - mpq_class(1);
-	Poly p2 = (x+mpq_class(1)) * (x-mpq_class(2));
-	Poly q1 = x+mpq_class(1);
-	Poly q2 = x-mpq_class(1);
-	Poly q3 = x-mpq_class(2);
-	
-	CoCoAAdaptor<Poly> c({p1, p2});
+TEST(CoCoA, Factorize) {
+	using Poly = carl::MultivariatePolynomial<mpq_class>;
+	carl::Variable x = carl::freshRealVariable("x");
+
+	Poly p1 = (x * x) - mpq_class(1);
+	Poly p2 = (x + mpq_class(1)) * (x - mpq_class(2));
+	Poly q1 = x + mpq_class(1);
+	Poly q2 = x - mpq_class(1);
+	Poly q3 = x - mpq_class(2);
+
+	carl::CoCoAAdaptor<Poly> c({p1, p2});
 	{
 		auto res = c.factorize(p1);
 		auto it1 = res.find(q1);
@@ -75,33 +70,32 @@ TEST(CoCoA, Factorize)
 	}
 }
 
-TEST(CoCoA, SquareFreePart)
-{
-	using Poly = MultivariatePolynomial<mpq_class>;
-	Variable x = freshRealVariable("x");
-	Variable y = freshRealVariable("y");
-	
-	CoCoAAdaptor<Poly> c({Poly(x*y)});
+TEST(CoCoA, SquareFreePart) {
+	using Poly = carl::MultivariatePolynomial<mpq_class>;
+	carl::Variable x = carl::freshRealVariable("x");
+	carl::Variable y = carl::freshRealVariable("y");
+
+	carl::CoCoAAdaptor<Poly> c({Poly(x * y)});
 	{
-		Poly p = (x*x - mpq_class(1)) * (x*x - mpq_class(1)) * (x+mpq_class(1)) * (x-mpq_class(2));
-		Poly res = Poly(x*x*x) - mpq_class(2)*x*x - x + mpq_class(2);
+		Poly p = (x * x - mpq_class(1)) * (x * x - mpq_class(1)) * (x + mpq_class(1)) * (x - mpq_class(2));
+		Poly res = Poly(x * x * x) - mpq_class(2) * x * x - x + mpq_class(2);
 		EXPECT_EQ(c.squareFreePart(p), res);
 	}
 	{
-		Poly p = (x*x - mpq_class(4)) * (x - mpq_class(2)) + (x*x*y - mpq_class(4)*y) * (x - mpq_class(2));
-		Poly res = Poly(x*x*y) + x*x - mpq_class(4)*y - mpq_class(4);
+		Poly p = (x * x - mpq_class(4)) * (x - mpq_class(2)) + (x * x * y - mpq_class(4) * y) * (x - mpq_class(2));
+		Poly res = Poly(x * x * y) + x * x - mpq_class(4) * y - mpq_class(4);
 		EXPECT_EQ(c.squareFreePart(p), res);
 	}
 }
 
-MultivariatePolynomial<mpq_class> randomPoly(const std::initializer_list<Variable>& vars) {
+carl::MultivariatePolynomial<mpq_class> randomPoly(const std::initializer_list<carl::Variable>& vars) {
 	static std::mt19937 rand(4);
-	MultivariatePolynomial<mpq_class> res;
+	carl::MultivariatePolynomial<mpq_class> res;
 	for (std::size_t i = 0; i < 20; i++) {
 		mpq_class coeff = rand() % 1024;
-		Monomial::Arg m = nullptr;
+		carl::Monomial::Arg m;
 		std::size_t left = i;
-		for (const auto& v: vars) {
+		for (const auto& v : vars) {
 			if (left == 0) break;
 			std::size_t e = rand() % (left + 1);
 			if (e == 0) continue;
@@ -113,25 +107,24 @@ MultivariatePolynomial<mpq_class> randomPoly(const std::initializer_list<Variabl
 	return res;
 }
 
-TEST(CoCoA, Benchmark)
-{
-	using Poly = MultivariatePolynomial<mpq_class>;
-	Variable x = freshRealVariable("x");
-	Variable y = freshRealVariable("y");
-	
+TEST(CoCoA, Benchmark) {
+	using Poly = carl::MultivariatePolynomial<mpq_class>;
+	carl::Variable x = carl::freshRealVariable("x");
+	carl::Variable y = carl::freshRealVariable("y");
+
 	std::size_t count = 10;
 	std::cout << "Generating " << count << "..." << std::endl;
-	std::vector<std::tuple<Poly,Poly,Poly>> instances;
+	std::vector<std::tuple<Poly, Poly, Poly>> instances;
 	for (std::size_t i = 0; i < count; i++) {
 		Poly a = randomPoly({x, y});
 		Poly b = randomPoly({x, y});
 		Poly c = randomPoly({x, y});
-		instances.emplace_back(a*c, b*c, c);
+		instances.emplace_back(a * c, b * c, c);
 	}
 	std::cout << "Starting..." << std::endl;
-	CoCoAAdaptor<Poly> conv({randomPoly({x, y})});
+	carl::CoCoAAdaptor<Poly> conv({randomPoly({x, y})});
 	carl::Timer timer;
-	for (const auto& i: instances) {
+	for (const auto& i : instances) {
 		auto res = conv.gcd(std::get<0>(i), std::get<1>(i));
 		EXPECT_EQ(res, std::get<2>(i));
 	}
