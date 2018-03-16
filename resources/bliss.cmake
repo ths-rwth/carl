51692b7d90acb7506732f70e73adec41650472f2
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2018-03-16 16:41:43 +0100

    Adding CMakeLists.txt for bliss instead of makefile patch.

--- resources/bliss.cmake
+++ resources/bliss.cmake
@@ -3,14 +3,11 @@ ExternalProject_Add(
     URL "http://www.tcs.hut.fi/Software/bliss/bliss-${BLISS_VERSION}.zip"
 	DOWNLOAD_NO_PROGRESS 1
 	BUILD_IN_SOURCE 1
-	PATCH_COMMAND patch Makefile ${CMAKE_SOURCE_DIR}/resources/bliss/Makefile.patch
-	CONFIGURE_COMMAND ""
-	INSTALL_COMMAND DESTDIR=<INSTALL_DIR> make install
+	PATCH_COMMAND cp ${CMAKE_SOURCE_DIR}/resources/bliss/CMakeLists.txt .
+	CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
 )
 
 ExternalProject_Get_Property(BLISS-EP INSTALL_DIR)
-
-message(STATUS "Bliss at ${INSTALL_DIR}")
 
 add_imported_library(BLISS SHARED "${INSTALL_DIR}/lib/libbliss.so" "${INSTALL_DIR}/include")
 add_imported_library(BLISS STATIC "${INSTALL_DIR}/lib/libbliss.a" "${INSTALL_DIR}/include")
