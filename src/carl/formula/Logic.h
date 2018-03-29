6f666f68bbe8e83b93f3b41c7ccb0de3741f1758
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2018-03-29 12:42:31 +0200

    Added logic QF_PB.

--- src/carl/formula/Logic.h
+++ src/carl/formula/Logic.h
@@ -5,7 +5,7 @@
 namespace carl {
 
 enum class Logic {
-	QF_BV, QF_LIA, QF_LIRA, QF_LRA, QF_NIA, QF_NIRA, QF_NRA, QF_UF, UNDEFINED
+	QF_BV, QF_LIA, QF_LIRA, QF_LRA, QF_NIA, QF_NIRA, QF_NRA, QF_PB, QF_UF, UNDEFINED
 };
 
 inline std::ostream& operator<<(std::ostream& os, const Logic& l) {
@@ -17,6 +17,7 @@ inline std::ostream& operator<<(std::ostream& os, const Logic& l) {
 		case Logic::QF_NIA:		os << "QF_NIA"; break;
 		case Logic::QF_NIRA:	os << "QF_NIRA"; break;
 		case Logic::QF_NRA:		os << "QF_NRA"; break;
+		case Logic::QF_PB:		os << "QF_PB"; break;
 		case Logic::QF_UF:		os << "QF_UF"; break;
 		case Logic::UNDEFINED:	os << "undefined"; break;
 	}
