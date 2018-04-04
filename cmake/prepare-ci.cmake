82695fd12d7c8e66d614c510bf573163d42ece0f
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2018-04-04 14:02:22 +0200

    Gracefully handle if python3 is not found.

--- cmake/prepare-ci.cmake
+++ cmake/prepare-ci.cmake
@@ -5,12 +5,14 @@ configure_file( ${CMAKE_SOURCE_DIR}/.ci/sonarcloud.properties.in ${CMAKE_BINARY_
 
 
 find_package(PythonInterp 3 REQUIRED)
-
-add_custom_target(.travis.yml)
-
-add_custom_command(
-	TARGET .travis.yml
-	BYPRODUCTS ${CMAKE_SOURCE_DIR}/.travis.yml
-	COMMAND ${PYTHON_EXECUTABLE} ${CMAKE_SOURCE_DIR}/.ci/travis_generate.py
-	WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/.ci/
-)
+if(PYTHONINTERP_FOUND)
+	add_custom_target(.travis.yml)
+	add_custom_command(
+		TARGET .travis.yml
+		BYPRODUCTS ${CMAKE_SOURCE_DIR}/.travis.yml
+		COMMAND ${PYTHON_EXECUTABLE} ${CMAKE_SOURCE_DIR}/.ci/travis_generate.py
+		WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/.ci/
+	)
+else()
+	message(STATUS "Did not find python3, target .travis.yml is not available.")
+endif()
