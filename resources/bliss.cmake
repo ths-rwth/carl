b686d3d2e61ae2cb2ce657426c5ce8894109af37
Matthias Volk <matthias.volk@cs.rwth-aachen.de>
2018-04-19 14:24:18 +0200

    Let bliss compile with ninja

--- resources/bliss.cmake
+++ resources/bliss.cmake
@@ -4,6 +4,7 @@ ExternalProject_Add(
 	DOWNLOAD_NO_PROGRESS 1
 	PATCH_COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_SOURCE_DIR}/resources/bliss/CMakeLists.txt <SOURCE_DIR>
 	CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
+	BUILD_BYPRODUCTS ${CMAKE_BINARY_DIR}/resources/lib/libbliss{STATIC_EXT} ${CMAKE_BINARY_DIR}/resources/lib/libbliss${DYNAMIC_EXT}
 )
 
 ExternalProject_Get_Property(BLISS-EP INSTALL_DIR)
