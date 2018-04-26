4de943a9e9dceddd2a31c84607d2636e18906c1c
Gereon <gereon.kremer@cs.rwth-aachen.de>
2018-04-26 14:57:02 +0200

    Added an external project for doxygen.

---
+++ resources/doxygen.cmake
@@ -0,0 +1,15 @@
+string(REPLACE "." "_" DOXYGEN_TAG ${DOXYGEN_VERSION})
+
+ExternalProject_Add(
+	Doxygen-EP
+	GIT_REPOSITORY "https://github.com/doxygen/doxygen.git"
+	GIT_TAG Release_${DOXYGEN_TAG}
+	CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
+)
+
+ExternalProject_Get_Property(Doxygen-EP INSTALL_DIR)
+
+add_executable(Doxygen::doxygen IMPORTED GLOBAL)
+set_target_properties(Doxygen::doxygen PROPERTIES IMPORTED_LOCATION "${INSTALL_DIR}/bin/doxygen")
+
+add_dependencies(Doxygen::doxygen Doxygen-EP)
