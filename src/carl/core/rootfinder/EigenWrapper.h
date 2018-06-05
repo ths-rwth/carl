7426e0946bb6542b13b219fbaca340c0d7cbf9aa
Gereon <gereon.kremer@cs.rwth-aachen.de>
2018-06-05 17:31:04 +0200

    Added a comment.

--- src/carl/core/rootfinder/EigenWrapper.h
+++ src/carl/core/rootfinder/EigenWrapper.h
@@ -7,7 +7,8 @@ namespace rootfinder {
 namespace eigen {
 
 /**
- * Compute the eigenvalues of 
+ * Compute approximations of the real roots of the univariate polynomials with the given coefficients.
+ * This method internally constructs a companion matrix and computes the eigenvalues.
  */
 std::vector<double> root_approximation(const std::vector<double>& coeffs);
 
