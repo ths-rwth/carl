1b05ccc2fd7abe25f133abbf6292529b262f7175
Gereon <gereon.kremer@cs.rwth-aachen.de>
2018-06-12 10:23:58 +0200

    Made cmake compatible version the default, add PROJECT_VERSION_FULL.

--- cmake/export.cmake
+++ cmake/export.cmake
@@ -47,7 +47,7 @@ endforeach()
 include(CMakePackageConfigHelpers)
 
 write_basic_package_version_file(${CMAKE_CURRENT_BINARY_DIR}/carlConfigVersion.cmake
-	COMPATIBILITY SameMajorVersion
+	COMPATIBILITY AnyNewerVersion
 )
 
 # ... for the build tree
