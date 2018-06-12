3cf9f8b75997e95b469c3779ef955d6a441c9bc1
Gereon <gereon.kremer@cs.rwth-aachen.de>
2018-06-12 09:59:53 +0200

    Using proper version for cmake export file.

--- cmake/export.cmake
+++ cmake/export.cmake
@@ -47,8 +47,8 @@ endforeach()
 include(CMakePackageConfigHelpers)
 
 write_basic_package_version_file(${CMAKE_CURRENT_BINARY_DIR}/carlConfigVersion.cmake
-	 VERSION 0.1.0
-	 COMPATIBILITY SameMajorVersion )
+	COMPATIBILITY SameMajorVersion
+)
 
 # ... for the build tree
 set(CONF_INCLUDE_DIRS "${PROJECT_SOURCE_DIR}/src") # TODO looks not correct, this would be hypro_INCLUDE_DIR
