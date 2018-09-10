9642dcd54c882c36b724b42a81570a85cb816a22
Gereon <gereon.kremer@cs.rwth-aachen.de>
2018-09-10 14:03:56 +0200

    Some refactoring and reformatting in UTerm.

--- src/carl/formula/uninterpreted/UTerm.cpp
+++ src/carl/formula/uninterpreted/UTerm.cpp
@@ -13,26 +13,47 @@
 
 namespace carl
 {
-    /**
-     * @return The domain of this uninterpreted term.
-     */
-    const Sort& UTerm::domain() const {
-      return std::visit(overloaded {
-              [](const UVariable& var) -> auto& { return var.domain(); },
-              [](const UFInstance& ufi) -> auto& { return ufi.uninterpretedFunction().codomain(); },
-          }, mTerm);
-    }
+	bool UTerm::isUVariable() const {
+		return std::visit(overloaded {
+			[](const UVariable& var) { return true; },
+			[](const UFInstance& ufi) { return false; },
+		}, mTerm);
+	}
+	bool UTerm::isUFInstance() const {
+		return std::visit(overloaded {
+			[](const UVariable& var) { return false; },
+			[](const UFInstance& ufi) { return true; },
+		}, mTerm);
+	}
 
-    /**
-      * Prints the given uninterpreted term on the given output stream.
-      * @param os The output stream to print on.
-      * @param ut The uninterpreted term to print.
-      * @return The output stream after printing the given uninterpreted term on it.
-      */
-    std::ostream& operator<<(std::ostream& os, const UTerm& ut) {
-        return std::visit(overloaded {
-                [&os](const UVariable& var) -> auto& { return os << var; },
-                [&os](const UFInstance& ufi) -> auto& { return os << ufi; },
-            }, ut.mTerm);
-    }
+	const Sort& UTerm::domain() const {
+		return std::visit(overloaded {
+			[](const UVariable& var) -> auto& { return var.domain(); },
+			[](const UFInstance& ufi) -> auto& { return ufi.uninterpretedFunction().codomain(); },
+		}, mTerm);
+	}
+
+	bool operator==(const UTerm& lhs, const UTerm& rhs) {
+		return std::visit(overloaded {
+			[](const UVariable& varL, const UVariable& varR) { return varL.variable().id() == varR.variable().id(); },
+			[](const UFInstance& ufiL, const UFInstance& ufiR) { return ufiL.id() == ufiR.id(); },
+			[](const auto& termL, const auto& termR) { return false; },
+		}, lhs.asVariant(), rhs.asVariant());
+	}
+
+	bool operator<(const UTerm& lhs, const UTerm& rhs) {
+		return std::visit(overloaded {
+			[](const UVariable& varL, const UVariable& varR) { return varL.variable().id() < varR.variable().id(); },
+			[](const UFInstance& ufiL, const UFInstance& ufiR) { return ufiL.id() < ufiR.id(); },
+			[](const UVariable& varL, const UFInstance& ufiR) { return true; },
+			[](const auto& termL, const auto& termR) { return false; },
+		}, lhs.asVariant(), rhs.asVariant());
+	}
+
+	std::ostream& operator<<(std::ostream& os, const UTerm& ut) {
+		return std::visit(overloaded {
+			[&os](const UVariable& var) -> auto& { return os << var; },
+			[&os](const UFInstance& ufi) -> auto& { return os << ufi; },
+		}, ut.asVariant());
+	}
 }
--- src/carl/formula/uninterpreted/UTerm.h
+++ src/carl/formula/uninterpreted/UTerm.h
@@ -15,123 +15,100 @@
 
 namespace carl
 {
-    /**
-     * Implements an uninterpreted term, that is either an uninterpreted variable or an uninterpreted function instance.
-     */
-    class UTerm {
+	/**
+	 * Implements an uninterpreted term, that is either an uninterpreted variable or an uninterpreted function instance.
+	 */
+	class UTerm {
+		using Super = std::variant<UVariable, UFInstance>;
+		Super mTerm;
 
-        using Super = std::variant<UVariable, UFInstance>;
+	public:
+		/**
+		 * Default constructor.
+		 */
+		UTerm() = default;
 
-        Super mTerm;
+		/**
+		 * Constructs an uninterpreted term.
+		 * @param term
+		 */
+		UTerm(const Super& term): mTerm(term) {}
 
-        public:
-            /**
-             * Default constructor.
-             */
-            UTerm() = default;
+		const auto& asVariant() const {
+			return mTerm;
+		}
 
-            /**
-             * Constructs an uninterpreted term.
-             * @param term
-             */
-            UTerm(Super term): mTerm(term) {}
+		/**
+		 * @return true, if the stored term is a UVariable.
+		 */
+		bool isUVariable() const;
 
-            /**
-              * @return true, if the stored term is a UVariable.
-              */
-            bool isUVariable() const {
-                return std::visit(overloaded {
-                        [](const UVariable& var) { return true; },
-                        [](const UFInstance& ufi) { return false; },
-                    }, mTerm);
-            }
+		/**
+		 * @return true, if the stored term is a UFInstance.
+		 */
+		bool isUFInstance() const;
 
-            /**
-              * @return true, if the stored term is a UFInstance.
-              */
-            bool isUFInstance() const {
-              return std::visit(overloaded {
-                      [](const UVariable& var) { return false; },
-                      [](const UFInstance& ufi) { return true; },
-                  }, mTerm);
-            }
+		/**
+		 * @return The stored term as UVariable.
+		 */
+		const UVariable& asUVariable() const {
+			assert(isUVariable());
+			return std::get<UVariable>(mTerm);
+		}
+		/**
+		 * @return The stored term as UFInstance.
+		 */
+		const UFInstance& asUFInstance() const {
+			assert(isUFInstance());
+			return std::get<UFInstance>(mTerm);
+		}
 
-            /**
-              * @return The stored term as UVariable.
-              */
-            const UVariable asUVariable() const {
-              assert(isUVariable());
-              return std::get<UVariable>(mTerm);
-            }
-            /**
-              * @return The stored term as UFInstance.
-              */
-            const UFInstance asUFInstance() const {
-              assert(isUFInstance());
-              return std::get<UFInstance>(mTerm);
-            }
+		/**
+		 * @return The domain of this uninterpreted term.
+		 */
+		const Sort& domain() const;
+	};
 
-            /**
-             * @return The domain of this uninterpreted term.
-             */
-             const Sort& domain() const;
 
-            /**
-             * @param ut The uninterpreted term to compare with.
-             * @return true, if this and the given uninterpreted term are equal.
-             */
-            bool operator==(const UTerm& ut) const
-            {
-                return std::visit(overloaded {
-                        [](const UVariable& varL, const UVariable& varR) { return varL.variable().id() == varR.variable().id(); },
-                        [](const UFInstance& ufiL, const UFInstance& ufiR) { return ufiL.id() == ufiR.id(); },
-                        [](const auto& termL, const auto& termR) { return false; },
-                    }, (*this).mTerm, ut.mTerm);
-            }
+	/**
+	 * @param lhs The uninterpreted term to the left.
+	 * @param rhs The uninterpreted term to the right.
+	 * @return true, if the given uninterpreted terms are equal.
+	 */
+	bool operator==(const UTerm& lhs, const UTerm& rhs);
 
-            /**
-             * @param ut The uninterpreted term to compare with.
-             * @return true, if this uninterpreted term is less than the given one.
-             */
-            bool operator<(const UTerm& ut) const
-            {
-              return std::visit(overloaded {
-                      [](const UVariable& varL, const UVariable& varR) { return varL.variable().id() < varR.variable().id(); },
-                      [](const UFInstance& ufiL, const UFInstance& ufiR) { return ufiL.id() < ufiR.id(); },
-                      [](const UVariable& varL, const UFInstance& ufiR) { return true; },
-                      [](const auto& termL, const auto& termR) { return false; },
-                  }, (*this).mTerm, ut.mTerm);
-            }
+	/**
+	 * @param lhs The uninterpreted term to the left.
+	 * @param rhs The uninterpreted term to the right.
+	 * @return true, if lhs is smaller than rhs.
+	 */
+	bool operator<(const UTerm& lhs, const UTerm& rhs);
 
-            /**
-             * Prints the given uninterpreted term on the given output stream.
-             * @param os The output stream to print on.
-             * @param ut The uninterpreted term to print.
-             * @return The output stream after printing the given uninterpreted term on it.
-             */
-            friend std::ostream& operator<<( std::ostream& os, const UTerm& ut);
-
-            friend struct std::hash<carl::UTerm>;
-    };
+	/**
+	 * Prints the given uninterpreted term on the given output stream.
+	 * @param os The output stream to print on.
+	 * @param ut The uninterpreted term to print.
+	 * @return The output stream after printing the given uninterpreted term on it.
+	 */
+	std::ostream& operator<<(std::ostream& os, const UTerm& ut);
 } // end namespace carl
 
 namespace std
 {
-    /**
-     * Implements std::hash for uninterpreted terms.
-     */
-    template<>
-    struct hash<carl::UTerm> {
-      public:
-          /**
-           * @param ut The uninterpreted term to get the hash for.
-           * @return The hash of the given uninterpreted term.
-           */
-          std::size_t operator()(const carl::UTerm& ut) const {
-              return std::visit(carl::overloaded {
-                [](const carl::UVariable& var) { return carl::hash_all(var); },
-                [](const carl::UFInstance& ufi) { return carl::hash_all(ufi); },
-              }, ut.mTerm);
-          }
-    };
+	/**
+	 * Implements std::hash for uninterpreted terms.
+	 */
+	template<>
+	struct hash<carl::UTerm> {
+		/**
+		 * @param ut The uninterpreted term to get the hash for.
+		 * @return The hash of the given uninterpreted term.
+		 */
+		std::size_t operator()(const carl::UTerm& ut) const {
+			return std::visit(carl::overloaded {
+				[](const carl::UVariable& var) { return carl::hash_all(var); },
+				[](const carl::UFInstance& ufi) { return carl::hash_all(ufi); },
+			}, ut.asVariant());
+		}
+	};
 }
