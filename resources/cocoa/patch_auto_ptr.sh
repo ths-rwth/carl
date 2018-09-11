a9f527b109882dc0c95d349cea0f78d81f7caaec
Gereon <gereon.kremer@cs.rwth-aachen.de>
2018-09-11 12:49:36 +0200

    Fixing bash script that patches CoCoALib.

--- resources/cocoa/patch_auto_ptr.sh
+++ resources/cocoa/patch_auto_ptr.sh
@@ -1,6 +1,6 @@
 if [ `uname` = 'Linux' ]; then
 	find . -type f | xargs sed -i "s/auto_ptr/unique_ptr/g"
-elif [ `uname` = `Darwin` ]; then
+elif [ `uname` = 'Darwin' ]; then
 	export LC_ALL=C
 	find . -type f | xargs sed -i '' "s/auto_ptr/unique_ptr/g"
 else
