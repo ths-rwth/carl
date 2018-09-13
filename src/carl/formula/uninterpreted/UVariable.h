6426f376cab731af21903b9c18cc2834431d44ab
Gereon <gereon.kremer@cs.rwth-aachen.de>
2018-09-13 12:48:44 +0200

    Refactored UVariable.

--- src/carl/formula/uninterpreted/UVariable.h
+++ src/carl/formula/uninterpreted/UVariable.h
@@ -12,107 +12,101 @@
 #include "../Sort.h"
 #include "../SortManager.h"
 
-namespace carl
-{
-    /**
-     * Implements an uninterpreted variable.
-     */
-    class UVariable {
-        private:
-            /// The according variable, hence, the actual content of this class.
-            Variable mVar;
-            /// The domain.
-            Sort mDomain;
+namespace carl {
+/**
+ * Implements an uninterpreted variable.
+ */
+class UVariable {
+private:
+	/// The according variable, hence, the actual content of this class.
+	Variable mVar;
+	/// The domain.
+	Sort mDomain;
 
-        public:
-  			/**
-  			 * Default constructor.
-  			 * The resulting object will not be a valid variable, but a dummy object.
-         */
-        UVariable():
-				      mVar(Variable::NO_VARIABLE),
-		          mDomain() {}
+public:
+	/**
+	 * Default constructor.
+	 * The resulting object will not be a valid variable, but a dummy object.
+	 */
+	UVariable()
+		: mVar(Variable::NO_VARIABLE),
+		  mDomain() {}
 
-         explicit UVariable(Variable var):
-              mVar(var),
-	            mDomain(SortManager::getInstance().getInterpreted(var.type())) {}
+	explicit UVariable(Variable var)
+		: mVar(var),
+		  mDomain(SortManager::getInstance().getInterpreted(var.type())) {}
 
-        /**
-         * Constructs an uninterpreted variable.
-         * @param _var The variable of the uninterpreted variable to construct.
-         * @param _domain The domain of the uninterpreted variable to construct.
-         */
-        UVariable(Variable var, Sort domain):
-            mVar(var),
-            mDomain(domain) {}
+	/**
+	 * Constructs an uninterpreted variable.
+	 * @param _var The variable of the uninterpreted variable to construct.
+	 * @param _domain The domain of the uninterpreted variable to construct.
+	 */
+	UVariable(Variable var, Sort domain)
+		: mVar(var),
+		  mDomain(domain) {}
 
-        /**
-         * @return The according variable, hence, the actual content of this class.
-         */
-        Variable variable() const {
-            return mVar;
-        }
+	/**
+	 * @return The according variable, hence, the actual content of this class.
+	 */
+	Variable variable() const {
+		return mVar;
+	}
 
-        /**
-         * @return The domain of this uninterpreted variable.
-         */
-        const Sort& domain() const {
-            return mDomain;
-        }
+	/**
+	 * @return The domain of this uninterpreted variable.
+	 */
+	const Sort& domain() const {
+		return mDomain;
+	}
+};
 
-        /**
-         * @param uvar The uninterpreted variable to compare with.
-         * @return true, if this and the given uninterpreted variable are equal.
-         */
-        bool operator==( const UVariable& uvar ) const {
-            return mVar == uvar.variable();
-        }
-        bool operator==(Variable::Arg var) const {
-            return mVar == var;
-        }
 
-        /**
-         * @param uvar The uninterpreted variable to compare with.
-         * @return true, if this uninterpreted variable is less than the given one.
-         */
-        bool operator<( const UVariable& uvar ) const {
-            return mVar < uvar.variable();
-        }
 
-        /**
-         * @return The string representation of this uninterpreted variable.
-         */ 
-        std::string toString(bool friendlyNames = true) const {
-	          return VariablePool::getInstance().getName(mVar, friendlyNames);
-        }
+/**
+ * Prints the given uninterpreted variable on the given output stream.
+ * @param os The output stream to print on.
+ * @param uvar The uninterpreted variable to print.
+ * @return The output stream after printing the given uninterpreted variable on it.
+ */
+inline std::ostream& operator<<(std::ostream& os, const UVariable& uvar) {
+	return os << uvar.variable();
+}
 
-        /**
-         * Prints the given uninterpreted variable on the given output stream.
-         * @param os The output stream to print on.
-         * @param uvar The uninterpreted variable to print.
-         * @return The output stream after printing the given uninterpreted variable on it.
-         */
-        friend std::ostream& operator<<( std::ostream& os, const UVariable& uvar ) {
-            return (os << uvar.variable());
-        }
-    };
+/**
+ * @param lhs The left variable.
+ * @param rhs The right variable.
+ * @return true, if the variable are equal.
+ */
+bool inline operator==(const UVariable& lhs, const UVariable& rhs) {
+	return lhs.variable() == rhs.variable();
+}
+
+/**
+ * @param lhs The left variable.
+ * @param rhs The right variable.
+ * @return true, if the left variable is smaller.
+ */
+bool inline operator<(const UVariable& lhs, const UVariable& rhs) {
+	return lhs.variable() < rhs.variable();
+}
+
+
 } // end namespace carl
 
-namespace std
-{
-    /**
-     * Implements std::hash for uninterpreted variables.
-     */
-    template<>
-    struct hash<carl::UVariable> {
-      public:
-          /**
-           * @param uvar The uninterpreted variable to get the hash for.
-           * @return The hash of the given uninterpreted variable.
-           */
-          size_t operator()( const carl::UVariable& uvar ) const
-          {
-              return carl::hash_all(uvar.variable());
-          }
-    };
-}
+namespace std {
+/**
+ * Implements std::hash for uninterpreted variables.
+ */
+template<>
+struct hash<carl::UVariable> {
+public:
+	/**
+	 * @param uvar The uninterpreted variable to get the hash for.
+	 * @return The hash of the given uninterpreted variable.
+	 */
+	std::size_t operator()(const carl::UVariable& uvar) const {
+		return carl::hash_all(uvar.variable());
+	}
+};
+
+} // namespace std
