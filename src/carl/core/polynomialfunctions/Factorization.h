90ced272a1642f3a826484e8273c6335772bb9f2
Gereon <gereon.kremer@cs.rwth-aachen.de>
2018-09-18 16:04:58 +0200

    Added carl::irreducibleFactors() that wraps the respective CoCoAAdaptor method.

--- src/carl/core/polynomialfunctions/Factorization.h
+++ src/carl/core/polynomialfunctions/Factorization.h
@@ -83,4 +83,39 @@ Factors<MultivariatePolynomial<C,O,P>> factorization(const MultivariatePolynomia
 	return factors;
 }
 
+/**
+ * Try to factorize a multivariate polynomial and return the irreducible factors (without multiplicities).
+ * Uses CoCoALib and GiNaC, if available, depending on the coefficient type of the polynomial.
+ */
+template<typename C, typename O, typename P>
+std::vector<MultivariatePolynomial<C,O,P>> irreducibleFactors(const MultivariatePolynomial<C,O,P>& p, bool includeConstants = true) {
+	if (p.totalDegree() <= 1) {
+		return {p};
+	}
+	using TypeSelector = carl::function_selector::NaryTypeSelector;
+
+	using types = carl::function_selector::wrap_types<
+		mpz_class,mpq_class
+#if defined USE_GINAC
+		,cln::cl_I,cln::cl_RA
+#endif
+	>;
+
+	auto s = carl::createFunctionSelector<TypeSelector, types>(
+	#if defined USE_COCOA
+		[includeConstants](const auto& p){ CoCoAAdaptor<MultivariatePolynomial<C,O,P>> c({p}); return c.irreducibleFactors(p, includeConstants); },
+		[includeConstants](const auto& p){ CoCoAAdaptor<MultivariatePolynomial<C,O,P>> c({p}); return c.irreducibleFactors(p, includeConstants); }
+	#else
+		[includeConstants](const auto& p){ return std::vector<MultivariatePolynomial<C,O,P>>({p}); },
+		[includeConstants](const auto& p){ return std::vector<MultivariatePolynomial<C,O,P>>({p}); }
+	#endif
+	#if defined USE_GINAC
+		,
+		[includeConstants](const auto& p){ return std::vector<MultivariatePolynomial<C,O,P>>({p}); },
+		[includeConstants](const auto& p){ return std::vector<MultivariatePolynomial<C,O,P>>({p}); }
+	#endif
+	);
+	return s(p);
+}
+
 }
