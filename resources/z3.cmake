f3e8edb2020d3dea2fd71776dfd04a9281bddfec
Gereon <gereon.kremer@cs.rwth-aachen.de>
2018-10-09 10:19:10 +0200

    Some cleanup in z3 external project.

--- resources/z3.cmake
+++ resources/z3.cmake
@@ -3,42 +3,23 @@ ExternalProject_Add(
 	GIT_REPOSITORY "https://github.com/Z3Prover/z3.git"
 	GIT_TAG z3-${Z3_VERSION}
 	UPDATE_COMMAND ""
-	CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
+	CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR> -DCMAKE_INSTALL_LIBDIR=lib
 	BUILD_COMMAND ${CMAKE_COMMAND} -DBUILD_LIBZ3_SHARED=ON <SOURCE_DIR>
 	COMMAND ${CMAKE_MAKE_PROGRAM} libz3
+	COMMAND ${CMAKE_MAKE_PROGRAM} install
 	COMMAND ${CMAKE_COMMAND} -DBUILD_LIBZ3_SHARED=OFF <SOURCE_DIR>
 	COMMAND ${CMAKE_MAKE_PROGRAM} libz3
+	COMMAND ${CMAKE_MAKE_PROGRAM} install
+	INSTALL_COMMAND ""
 )
 
 ExternalProject_Get_Property(Z3-EP INSTALL_DIR)
-ExternalProject_Get_Property(Z3-EP SOURCE_DIR)
 
-
-#file(REMOVE_RECURSE "${INSTALL_DIR}/include/z3")
-#file(COPY "${SOURCE_DIR}" DESTINATION "${INSTALL_DIR}/include")
-#file(RENAME "${INSTALL_DIR}/include/Z3-EP" "${INSTALL_DIR}/include/z3")
-
-add_imported_library(Z3 SHARED "${INSTALL_DIR}/lib/libz3${DYNAMIC_EXT}" "${SOURCE_DIR}/src") #"${INSTALL_DIR}/include")
-add_imported_library(Z3 STATIC "${INSTALL_DIR}/lib/libz3${STATIC_EXT}" "${SOURCE_DIR}/src") #"${INSTALL_DIR}/include")
+add_imported_library(Z3 SHARED "${INSTALL_DIR}/lib/libz3${DYNAMIC_EXT}" "${INSTALL_DIR}/include")
+add_imported_library(Z3 STATIC "${INSTALL_DIR}/lib/libz3${STATIC_EXT}" "${INSTALL_DIR}/include")
 
 add_dependencies(Z3_SHARED Z3-EP)
 add_dependencies(Z3_STATIC Z3-EP)
 
 add_dependencies(resources Z3_SHARED)
 add_dependencies(resources Z3_STATIC)
-
-#add_dependencies(resources Z3-EP)
-
-
-
-
-# ---
-
-#add_imported_library(Z3 SHARED "${INSTALL_DIR}/libz3${DYNAMIC_EXT}" "${INSTALL_DIR}/include")
-#add_imported_library(Z3 STATIC "${INSTALL_DIR}/libz3${STATIC_EXT}" "${INSTALL_DIR}/include")
-
-#add_dependencies(Z3_SHARED Z3-EP)
-#add_dependencies(Z3_STATIC Z3-EP)
-
-#add_dependencies(resources Z3_SHARED)
-#add_dependencies(resources Z3_STATIC)
