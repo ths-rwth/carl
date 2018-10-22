1c6c82743d7b3e2a503115a6ffa2971b19cb350c
Jasper Nalbach <derjasper@posteo.de>
2018-10-22 19:25:03 +0200

    added some assertions

--- src/carl/formula/model/ran/RealAlgebraicNumber.h
+++ src/carl/formula/model/ran/RealAlgebraicNumber.h
@@ -105,15 +105,26 @@ public:
 	explicit RealAlgebraicNumber(const Number& n, bool isRoot = true):
 		mValue(n),
 		mIsRoot(isRoot)
-	{}
+	{
+		#ifdef USE_Z3_RANS
+		assert(false);
+		#endif
+	}
 	explicit RealAlgebraicNumber(Variable var, bool isRoot = true):
 		mIsRoot(isRoot),
 		mIR(std::make_shared<IntervalContent>(Polynomial(var), Interval<Number>::zeroInterval()))
-	{}
+	{
+		#ifdef USE_Z3_RANS
+		assert(false);
+		#endif
+	}
 	explicit RealAlgebraicNumber(const Polynomial& p, const Interval<Number>& i, bool isRoot = true):
 		mIsRoot(isRoot),
 		mIR(std::make_shared<IntervalContent>(p.normalized(), i))
 	{
+		#ifdef USE_Z3_RANS
+		assert(false);
+		#endif
 		assert(!carl::isZero(mIR->polynomial) && mIR->polynomial.degree() > 0);
 		assert(i.isOpenInterval() || i.isPointInterval());
 		assert(count_real_roots(p, i) == 1);
@@ -130,6 +141,9 @@ public:
 		mIsRoot(isRoot),
 		mIR(std::make_shared<IntervalContent>(p.normalized(), i, sturmSequence))
 	{
+		#ifdef USE_Z3_RANS
+		assert(false);
+		#endif
 		assert(!carl::isZero(mIR->polynomial) && mIR->polynomial.degree() > 0);
 		assert(i.isOpenInterval() || i.isPointInterval());
 		assert(carl::count_real_roots(p, i) == 1);
@@ -146,7 +160,11 @@ public:
 	explicit RealAlgebraicNumber(const ThomEncoding<Number>& te, bool isRoot = true):
 		mIsRoot(isRoot),
 		mTE(std::make_shared<ThomEncoding<Number>>(te))
-	{}
+	{
+		#ifdef USE_Z3_RANS
+		assert(false);
+		#endif
+	}
 
 	#ifdef USE_Z3_RANS
 	explicit RealAlgebraicNumber(const Z3Ran<Number>& zr, bool isRoot = true):
