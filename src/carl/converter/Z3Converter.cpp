b60d62d2bfb559fc7fbe78962906b24662a7f7e7
Jasper Nalbach <derjasper@posteo.de>
2018-11-06 16:09:46 +0100

    fix previous commit

--- src/carl/converter/Z3Converter.cpp
+++ src/carl/converter/Z3Converter.cpp
@@ -4,7 +4,13 @@
 
 namespace carl {
 
+    // FIXME not thread safe
     Z3Converter& z3() {
+        static bool initialized = false;
+        if (!initialized) {
+            Z3Converter::initialize();
+            initialized = true;
+        }
         static Z3Converter m;
         return m;
     }
