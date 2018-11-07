990b808d25a1917dce33a5bc8a49eef707d5b351
Gereon <gereon.kremer@cs.rwth-aachen.de>
2018-11-07 10:15:03 +0100

    Make clang7 job mandatory.

--- .ci/travis_generate.py
+++ .ci/travis_generate.py
@@ -32,7 +32,7 @@ properties.update({
 jobs = [
 	job("0-clang", ["build", "linux", "clang-5.0", "build.sh"]),
 	job("0-clang", ["build", "linux", "clang-6.0", "build.sh"]),
-	job("0-clang", ["build", "linux", "clang-7.0", "build.sh", "mayfail"]),
+	job("0-clang", ["build", "linux", "clang-7.0", "build.sh"]),
 	job("1-gcc", ["dependencies", "linux", "g++-7", "build.sh"]),
 	job("1-gcc", ["build", "linux", "g++-7", "j1", "build.sh"]),
 	job("1-gcc", ["dependencies", "linux", "g++-8", "build.sh"]),
