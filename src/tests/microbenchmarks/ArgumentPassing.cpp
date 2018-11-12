fffc61d468a67ba725f256d558fcad4ae1c3c6a5
Gereon <gereon.kremer@cs.rwth-aachen.de>
2018-11-12 11:01:43 +0100

    Propert indentation.

--- src/tests/microbenchmarks/ArgumentPassing.cpp
+++ src/tests/microbenchmarks/ArgumentPassing.cpp
@@ -7,66 +7,66 @@
 
 template<typename T>
 void pass_by_ref(const T& v) {
-  benchmark::DoNotOptimize(v);
+	benchmark::DoNotOptimize(v);
 }
 template<typename T>
 void pass_by_value(T v) {
-  benchmark::DoNotOptimize(v);
+	benchmark::DoNotOptimize(v);
 }
 
 static void Variable_by_ref(benchmark::State& state) {
-  carl::Variable v;
-  for (auto _ : state)
-    pass_by_ref(v);
+	carl::Variable v = carl::freshBooleanVariable();
+	for (auto _ : state)
+		pass_by_ref(v);
 }
 BENCHMARK(Variable_by_ref);
 
 static void Variable_by_value(benchmark::State& state) {
-  carl::Variable v;
-  for (auto _ : state)
-    pass_by_value(v);
+	carl::Variable v = carl::freshBooleanVariable();
+	for (auto _ : state)
+		pass_by_value(v);
 }
 BENCHMARK(Variable_by_value);
 
 static void UFInstance_by_ref(benchmark::State& state) {
-  carl::UFInstance v;
-  for (auto _ : state)
-    pass_by_ref(v);
+	carl::UFInstance v;
+	for (auto _ : state)
+		pass_by_ref(v);
 }
 BENCHMARK(UFInstance_by_ref);
 
 static void UFInstance_by_value(benchmark::State& state) {
-  carl::UFInstance v;
-  for (auto _ : state)
-    pass_by_value(v);
+	carl::UFInstance v;
+	for (auto _ : state)
+		pass_by_value(v);
 }
 BENCHMARK(UFInstance_by_value);
 
 static void UTerm_by_ref(benchmark::State& state) {
-  carl::UTerm v;
-  for (auto _ : state)
-    pass_by_ref(v);
+	carl::UTerm v;
+	for (auto _ : state)
+		pass_by_ref(v);
 }
 BENCHMARK(UTerm_by_ref);
 
 static void UTerm_by_value(benchmark::State& state) {
-  carl::UTerm v;
-  for (auto _ : state)
-    pass_by_value(v);
+	carl::UTerm v;
+	for (auto _ : state)
+		pass_by_value(v);
 }
 BENCHMARK(UTerm_by_value);
 
 static void UVariable_by_ref(benchmark::State& state) {
-  carl::UVariable v;
-  for (auto _ : state)
-    pass_by_ref(v);
+	carl::UVariable v;
+	for (auto _ : state)
+		pass_by_ref(v);
 }
 BENCHMARK(UVariable_by_ref);
 
 static void UVariable_by_value(benchmark::State& state) {
-  carl::UVariable v;
-  for (auto _ : state)
-    pass_by_value(v);
+	carl::UVariable v;
+	for (auto _ : state)
+		pass_by_value(v);
 }
 BENCHMARK(UVariable_by_value);
 
