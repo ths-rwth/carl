30ac77e54dd8e9d1d60a0b4cf907be9652655e82
Gereon <gereon.kremer@cs.rwth-aachen.de>
2018-11-13 14:14:10 +0100

    Added exact definitions.

--- src/carl/core/polynomialfunctions/Content.h
+++ src/carl/core/polynomialfunctions/Content.h
@@ -10,7 +10,7 @@ namespace  carl {
 /**
  * The content of a polynomial is the gcd of the coefficients of the normal part of a polynomial.
  * The content of zero is zero.
- * @see @cite GCL92, page 53
+ * @see @cite GCL92, page 53, definition 2.18
  * @return The content of the polynomial.
  */
 template<typename Coeff>
--- src/carl/core/polynomialfunctions/PrimitivePart.h
+++ src/carl/core/polynomialfunctions/PrimitivePart.h
@@ -10,7 +10,7 @@ namespace  carl {
 /**
  * The primitive part of p is the normal part of p divided by the content of p.
  * The primitive part of zero is zero.
- * @see @cite GCL92, page 53
+ * @see @cite GCL92, page 53, definition 2.18
  * @return The primitive part of the polynomial.
  */
 template<typename Coeff>
