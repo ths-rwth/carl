645508c02d2e106db8522dd7deb39d7ddb2c2613
Gereon <gereon.kremer@cs.rwth-aachen.de>
2018-11-13 10:03:41 +0100

    Refactor carlVariables to also store UVariable and BVVariable.

--- src/carl/core/Variables.h
+++ src/carl/core/Variables.h
@@ -1,8 +1,11 @@
 #pragma once
 
 #include "Variable.h"
+#include "../formula/bitvector/BVVariable.h"
+#include "../formula/uninterpreted/UVariable.h"
 
 #include <algorithm>
+#include <variant>
 #include <vector>
 
 namespace carl {
@@ -10,20 +13,22 @@ namespace carl {
 class carlVariables {
 public:
 	friend bool operator==(const carlVariables& lhs, const carlVariables& rhs);
+	friend std::ostream& operator<<(std::ostream& os, const carlVariables& vars);
 private:
-	std::vector<Variable> mVariables;
+	using VarTypes = std::variant<Variable,BVVariable,UVariable>;
+	std::vector<VarTypes> mVariables;
 public:
 	carlVariables() = default;
-	explicit carlVariables(std::initializer_list<Variable> i):
+	explicit carlVariables(std::initializer_list<VarTypes> i):
 		mVariables(i)
 	{}
-	template<typename Iterable>
-	explicit carlVariables(const Iterable& container) {
-		add(container);
+	template<typename Iterator>
+	explicit carlVariables(const Iterator& b, const Iterator& e) {
+		add(b, e);
 	}
-	template<typename Iterable, typename F>
-	carlVariables(const Iterable& container, F&& f) {
-		add(container, std::forward<F>(f));
+	template<typename Iterator, typename F>
+	carlVariables(const Iterator& b, const Iterator& e, F&& f) {
+		add(b, e, std::forward<F>(f));
 	}
 
 	auto begin() const {
@@ -40,28 +45,31 @@ public:
 	}
 
 	void compact() {
+		CARL_LOG_DEBUG("carl.core", "Before: " << *this);
 		std::sort(begin(), end());
-		mVariables.erase(std::unique(begin(), end()));
+		CARL_LOG_DEBUG("carl.core", "Sorted: " << *this);
+		mVariables.erase(std::unique(begin(), end()), mVariables.end());
+		CARL_LOG_DEBUG("carl.core", "After:  " << *this);
 	}
 
-	void add(Variable v) {
+	void add(VarTypes v) {
 		mVariables.emplace_back(v);
 	}
-	void add(std::initializer_list<Variable> i) {
+	void add(std::initializer_list<VarTypes> i) {
 		mVariables.insert(end(), i.begin(), i.end());
 	}
-	template<typename Iterable>
-	void add(const Iterable& container) {
-		mVariables.insert(end(), container.begin(), container.end());
+	template<typename Iterator>
+	void add(const Iterator& b, const Iterator& e) {
+		mVariables.insert(end(), b, e);
 	}
-	template<typename Iterable, typename F>
-	void add(const Iterable& container, F&& f) {
-		for (const auto& elem: container) {
-			mVariables.emplace_back(f(elem));
-		}
+	template<typename Iterator, typename F>
+	void add(const Iterator& b, const Iterator& e, F&& f) {
+		std::for_each(b, e,
+			[this, &f](const auto& elem){ mVariables.emplace_back(f(elem)); }
+		);
 	}
 
-	void erase(Variable v) {
+	void erase(VarTypes v) {
 		mVariables.erase(std::remove(begin(), end(), v));
 	}
 
@@ -71,10 +79,24 @@ public:
 		std::copy_if(begin(), end(), std::back_inserter(res.mVariables), std::forward<F>(f));
 		return res;
 	}
+	std::vector<Variable> underlyingVariables() const {
+		std::vector<Variable> res;
+		std::for_each(begin(), end(), [&res](const auto& var) {
+			std::visit(overloaded {
+				[&res](Variable v){ res.emplace_back(v); },
+				[&res](BVVariable v){ res.emplace_back(v.variable()); },
+				[&res](UVariable v){ res.emplace_back(v.variable()); },
+			}, var);
+		});
+		return res;
+	}
 };
 
 inline bool operator==(const carlVariables& lhs, const carlVariables& rhs) {
 	return lhs.mVariables == rhs.mVariables;
+}
+inline std::ostream& operator<<(std::ostream& os, const carlVariables& vars) {
+	return os << vars.mVariables;
 }
 
 }
