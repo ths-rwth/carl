f549178239d9f42c64d8449f2c9658a181237f39
Gereon <gereon.kremer@cs.rwth-aachen.de>
2018-11-21 10:18:07 +0100

    Refactoring compile info.

---
+++ src/carl/util/CompileInfo.h
@@ -0,0 +1,31 @@
+/**
+ * @file CMakeOptions.h
+ * @author Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
+ */
+
+#pragma once
+
+#include <iostream>
+#include <string>
+
+namespace carl {
+	/**
+	* Compile time generated structure holding information about compiler and system version.
+	*/
+	struct CompileInfo {
+		static const std::string SystemName;
+		static const std::string SystemVersion;
+		static const std::string BuildType;
+		static const std::string CXXCompiler;
+		static const std::string CXXCompilerVersion;
+		static const std::string GitRevisionSHA1;
+	};
+
+	struct CMakeOptionPrinter {
+		bool advanced;
+	};
+	constexpr CMakeOptionPrinter CMakeOptions(bool advanced = false) noexcept {
+		return { advanced };
+	}
+	std::ostream& operator<<(std::ostream& os, CMakeOptionPrinter cmop);
+}
