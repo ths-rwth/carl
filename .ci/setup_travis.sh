2972db95a4a15bc904c4ce6a86aebf1d12e06f09
Gereon <gereon.kremer@cs.rwth-aachen.de>
2018-11-22 08:55:49 +0100

    Using cmake 3.10.2 on travis.

--- .ci/setup_travis.sh
+++ .ci/setup_travis.sh
@@ -8,9 +8,9 @@ if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
 	PREFIX=`cd ~/usr; pwd`
 
 	if [ ! -f $PREFIX/bin/cmake ]; then
-		wget -nv https://cmake.org/files/v3.8/cmake-3.8.2-Linux-x86_64.sh
-		chmod +x cmake-3.8.2-Linux-x86_64.sh
-		./cmake-3.8.2-Linux-x86_64.sh --prefix=$PREFIX --exclude-subdir --skip-license
+		wget -nv https://cmake.org/files/v3.10/cmake-3.10.2-Linux-x86_64.sh
+		chmod +x cmake-3.10.2-Linux-x86_64.sh
+		./cmake-3.10.2-Linux-x86_64.sh --prefix=$PREFIX --exclude-subdir --skip-license
 	fi
 
 	export PATH="$PREFIX/bin:$PATH"
