ba3dbeff0d1bc35e7be84ba1d80d61f3cd7557f1
Gereon <gereon.kremer@cs.rwth-aachen.de>
2019-01-15 15:02:53 +0100

    Added a bunch of tests.

--- src/tests/interval/Test_Contractor.cpp
+++ src/tests/interval/Test_Contractor.cpp
@@ -9,7 +9,7 @@
 
 using namespace carl;
 
-TEST(Contractor, Basic)
+TEST(Evaluation, Basic)
 {
     Interval<double> ia = Interval<double>( 1, 4 );
     Interval<double> ib = Interval<double>( 2, 5 );
@@ -35,34 +35,34 @@ TEST(Contractor, Basic)
     MultivariatePolynomial<Rational> e6({(Rational)12*a,(Rational)3*b, (Rational)1*c*c,(Rational)-1*d*d*d});
     MultivariatePolynomial<Rational> e7({a,c});
 
-	using Contractor = contractor::Contraction<MultivariatePolynomial<Rational>>;
+	using Evaluation = contractor::Evaluation<MultivariatePolynomial<Rational>>;
 	using V = std::vector<Interval<double>>;
 
-	EXPECT_EQ(Contractor(e1, a).evaluate(map), V({ Interval<double>(-10, 0) }));
-	EXPECT_EQ(Contractor(e1, b).evaluate(map), V({ Interval<double>(-9, 1) }));
-	EXPECT_EQ(Contractor(e1, c).evaluate(map), V({ Interval<double>(-11, -3) }));
-	EXPECT_EQ(Contractor(e1, d).evaluate(map), V({ Interval<double>(-12, -1) }));
+	EXPECT_EQ(Evaluation(e1, a).evaluate(map), V({ Interval<double>(-10, 0) }));
+	EXPECT_EQ(Evaluation(e1, b).evaluate(map), V({ Interval<double>(-9, 1) }));
+	EXPECT_EQ(Evaluation(e1, c).evaluate(map), V({ Interval<double>(-11, -3) }));
+	EXPECT_EQ(Evaluation(e1, d).evaluate(map), V({ Interval<double>(-12, -1) }));
 
-	EXPECT_EQ(Contractor(e2, a).evaluate(map), V({ Interval<double>(-3, 2) }));
-	EXPECT_EQ(Contractor(e2, b).evaluate(map), V({ Interval<double>(-6, 4) }));
-	EXPECT_EQ(Contractor(e2, c).evaluate(map), V({ Interval<double>(0, BoundType::INFTY, -1, BoundType::WEAK) }));
-	//EXPECT_EQ(Contractor(e2, d).evaluate(map), V({ Interval<double>(-10, 0) }));
+	EXPECT_EQ(Evaluation(e2, a).evaluate(map), V({ Interval<double>(-3, 2) }));
+	EXPECT_EQ(Evaluation(e2, b).evaluate(map), V({ Interval<double>(-6, 4) }));
+	EXPECT_EQ(Evaluation(e2, c).evaluate(map), V({ Interval<double>(0, BoundType::INFTY, -1, BoundType::WEAK) }));
+	//EXPECT_EQ(Evaluation(e2, d).evaluate(map), V({ Interval<double>(-10, 0) }));
 
-	EXPECT_EQ(Contractor(e3, a).evaluate(map), V({ Interval<double>(0, BoundType::INFTY, 0, BoundType::INFTY) }));
-	EXPECT_EQ(Contractor(e3, b).evaluate(map), V({ Interval<double>(0, BoundType::INFTY, 0, BoundType::INFTY) }));
-	EXPECT_EQ(Contractor(e3, c).evaluate(map), V({ Interval<double>(-1, 0) }));
-	EXPECT_EQ(Contractor(e3, d).evaluate(map), V({ Interval<double>(-60, 40) }));
+	EXPECT_EQ(Evaluation(e3, a).evaluate(map), V({ Interval<double>(0, BoundType::INFTY, 0, BoundType::INFTY) }));
+	EXPECT_EQ(Evaluation(e3, b).evaluate(map), V({ Interval<double>(0, BoundType::INFTY, 0, BoundType::INFTY) }));
+	EXPECT_EQ(Evaluation(e3, c).evaluate(map), V({ Interval<double>(-1, 0) }));
+	EXPECT_EQ(Evaluation(e3, d).evaluate(map), V({ Interval<double>(-60, 40) }));
 
-	EXPECT_EQ(Contractor(e4, a).evaluate(map), V({ Interval<double>(-7, 3) }));
-	EXPECT_EQ(Contractor(e4, b).evaluate(map), V({ Interval<double>(-6, 4) }));
-	EXPECT_EQ(Contractor(e4, c).evaluate(map), V({ Interval<double>(1, 9) }));
-	EXPECT_EQ(Contractor(e4, d).evaluate(map), V({ Interval<double>(0, 11) }));
+	EXPECT_EQ(Evaluation(e4, a).evaluate(map), V({ Interval<double>(-7, 3) }));
+	EXPECT_EQ(Evaluation(e4, b).evaluate(map), V({ Interval<double>(-6, 4) }));
+	EXPECT_EQ(Evaluation(e4, c).evaluate(map), V({ Interval<double>(1, 9) }));
+	EXPECT_EQ(Evaluation(e4, d).evaluate(map), V({ Interval<double>(0, 11) }));
 
-	EXPECT_EQ(Contractor(e5, a).evaluate(map), V({ Interval<double>(-12, -9) }));
-	EXPECT_EQ(Contractor(e5, b).evaluate(map), V({ Interval<double>(-11, -8) }));
+	EXPECT_EQ(Evaluation(e5, a).evaluate(map), V({ Interval<double>(-12, -9) }));
+	EXPECT_EQ(Evaluation(e5, b).evaluate(map), V({ Interval<double>(-11, -8) }));
 
-	//EXPECT_EQ(Contractor(e6, a).evaluate(map), V({ Interval<double>(-10, 0) }));
-	//EXPECT_EQ(Contractor(e6, b).evaluate(map), V({ Interval<double>(-10, 0) }));
-	EXPECT_EQ(Contractor(e6, c).evaluate(map), V());
-	//EXPECT_EQ(Contractor(e6, d).evaluate(map), V({ Interval<double>(-10, 0) }));
+	//EXPECT_EQ(Evaluation(e6, a).evaluate(map), V({ Interval<double>(-10, 0) }));
+	//EXPECT_EQ(Evaluation(e6, b).evaluate(map), V({ Interval<double>(-10, 0) }));
+	EXPECT_EQ(Evaluation(e6, c).evaluate(map), V());
+	//EXPECT_EQ(Evaluation(e6, d).evaluate(map), V({ Interval<double>(-10, 0) }));
 }
