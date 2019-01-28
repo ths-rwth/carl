c1130f7ebba101e72f6a070746b891cdababce62
Gereon <gereon.kremer@cs.rwth-aachen.de>
2019-01-28 10:32:56 +0100

    Fixed evaluation of UEquality and added a test case.

--- src/tests/formula/Test_UVariable.cpp
+++ src/tests/formula/Test_UVariable.cpp
@@ -2,6 +2,10 @@
 
 #include <carl/formula/uninterpreted/UVariable.h>
 #include <carl/formula/SortManager.h>
+#include <carl/formula/Formula.h>
+#include <carl/formula/model/Model.h>
+#include <carl/formula/model/uninterpreted/SortValueManager.h>
+#include <carl/formula/model/evaluation/ModelEvaluation.h>
 
 #include "../Common.h"
 
@@ -26,4 +30,36 @@ TEST(UVariable, Basics)
 	EXPECT_EQ(getOutput(v2), "a");
 	EXPECT_EQ(getOutput(v4), "b");
 	EXPECT_EQ(std::hash<carl::UVariable>()(v2), std::hash<carl::UVariable>()(v3));
+}
+
+TEST(UVariable, Evaluation)
+{
+	carl::SortManager& sm = carl::SortManager::getInstance();
+	sm.clear();
+	carl::Sort sort = sm.addSort("S", carl::VariableType::VT_UNINTERPRETED);
+
+	carl::Variable a = carl::freshUninterpretedVariable("a");
+	carl::Variable b = carl::freshUninterpretedVariable("b");
+	carl::Variable c = carl::freshUninterpretedVariable("c");
+	carl::UVariable v1(a, sort);
+	carl::UVariable v2(b, sort);
+	carl::UVariable v3(c, sort);
+
+	using P = carl::MultivariatePolynomial<mpq_class>;
+	using F = carl::Formula<P>;
+
+	auto f = F(carl::FormulaType::AND, {
+		F(carl::UEquality(v1, v2, false)),
+		F(carl::UEquality(v2, v3, true))
+	});
+
+	auto sv1 = newSortValue(sort);
+	auto sv2 = newSortValue(sort);
+	carl::Model<mpq_class, P> m;
+	m.emplace(v1, sv1);
+	m.emplace(v2, sv1);
+	m.emplace(v3, sv2);
+
+	carl::ModelValue<mpq_class, P> reference(true);
+	EXPECT_EQ(carl::model::evaluate(f, m), reference);
 }
