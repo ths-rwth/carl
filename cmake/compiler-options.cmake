e979409775963db407731bac22e098b8bc213f8c
Gereon <gereon.kremer@cs.rwth-aachen.de>
2019-03-07 15:42:37 +0100

    Also track origins in memory sanitizer.

--- cmake/compiler-options.cmake
+++ cmake/compiler-options.cmake
@@ -21,7 +21,7 @@ if ("${CMAKE_CXX_COMPILER_ID}" MATCHES "Clang") # Matches "Clang" and "AppleClan
 			set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fno-omit-frame-pointer")
 		endif()
 		if ("${CLANG_SANITIZER}" STREQUAL "memory")
-			set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fno-omit-frame-pointer")
+			set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fno-omit-frame-pointer -fsanitize-memory-track-origins")
 		endif()
 	endif()
 elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
