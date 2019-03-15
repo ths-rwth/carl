e455f2db6762cc9ef3f3d118a854611c0a016a97
Gereon <gereon.kremer@cs.rwth-aachen.de>
2019-03-15 15:05:19 +0100

    A bit of debug output in coverage collection.

--- cmake/coverage-clang.sh
+++ cmake/coverage-clang.sh
@@ -10,10 +10,13 @@ rm -f ${CMAKE_BINARY_DIR}/coverage-out/*
 make
 make test LLVM_PROFILE_FILE=${CMAKE_BINARY_DIR}/coverage/%p.profraw
 
+echo "Merging profile data..."
 llvm-profdata merge -sparse ${CMAKE_BINARY_DIR}/coverage/*.profraw -o ${CMAKE_BINARY_DIR}/llvm.profdata
 
+echo "Dumping coverage data..."
 llvm-cov show -instr-profile llvm.profdata libcarl.so > coverage-out/coverage.txt
 for f in `ls bin/` ; do 
 	llvm-cov show -instr-profile llvm.profdata bin/$f > coverage-out/coverage-$f.txt
 done
+echo "Merging coverage data..."
 cat coverage-out/*.txt > coverage.txt
