ab6d2a08ceab0d0c3b53a398fefb1d97bcb8bdd1
Gereon <gereon.kremer@cs.rwth-aachen.de>
2019-03-18 12:40:38 +0100

    Reformatting files.

--- src/carl/formula/uninterpreted/UFInstanceManager.cpp
+++ src/carl/formula/uninterpreted/UFInstanceManager.cpp
@@ -8,30 +8,29 @@
 
 #include "UFInstanceManager.h"
 
-namespace carl
-{
-    UFInstance UFInstanceManager::newUFInstance(std::unique_ptr<UFInstanceContent>&& ufic) {
-        auto iter = mUFInstanceIdMap.find(ufic.get());
-        // Check if this uninterpreted function content has already been created
-        if(iter != mUFInstanceIdMap.end()) {
-            return UFInstance(iter->second);
-        }
-        // Create the uninterpreted function instance
-        mUFInstanceIdMap.emplace(ufic.get(), mUFInstances.size());
-        UFInstance ufi(mUFInstances.size());
-        mUFInstances.emplace_back(std::move(ufic));
-        return ufi;
-    }
-
-    bool UFInstanceManager::argsCorrect(const UFInstanceContent& ufic) {
-        if(!(ufic.uninterpretedFunction().domain().size() == ufic.args().size())) {
-            return false;
-        }
-        for(std::size_t i = 0; i < ufic.uninterpretedFunction().domain().size(); ++i) {
-            if(!(ufic.uninterpretedFunction().domain().at(i) == ufic.args().at(i).domain())) {
-                return false;
-            }
-        }
-        return true;
-    }
+namespace carl {
+UFInstance UFInstanceManager::newUFInstance(std::unique_ptr<UFInstanceContent>&& ufic) {
+	auto iter = mUFInstanceIdMap.find(ufic.get());
+	// Check if this uninterpreted function content has already been created
+	if (iter != mUFInstanceIdMap.end()) {
+		return UFInstance(iter->second);
+	}
+	// Create the uninterpreted function instance
+	mUFInstanceIdMap.emplace(ufic.get(), mUFInstances.size());
+	UFInstance ufi(mUFInstances.size());
+	mUFInstances.emplace_back(std::move(ufic));
+	return ufi;
 }
+
+bool UFInstanceManager::argsCorrect(const UFInstanceContent& ufic) {
+	if (!(ufic.uninterpretedFunction().domain().size() == ufic.args().size())) {
+		return false;
+	}
+	for (std::size_t i = 0; i < ufic.uninterpretedFunction().domain().size(); ++i) {
+		if (!(ufic.uninterpretedFunction().domain().at(i) == ufic.args().at(i).domain())) {
+			return false;
+		}
+	}
+	return true;
+}
+} // namespace carl
