6e44351f598e63a1f1f6aca9e03058abce1e9be6
Gereon <gereon.kremer@cs.rwth-aachen.de>
2019-03-19 09:06:48 +0100

    Fixing coverage collection script.

--- cmake/coverage-clang.sh
+++ cmake/coverage-clang.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 
-mkdir -p coverage/*
-mkdir -p coverage-out/*
+DIR=`pwd`
+
+mkdir -p coverage/
+mkdir -p coverage-out/
 rm -f coverage/*
 rm -f coverage-out/*
 
 make
-make test LLVM_PROFILE_FILE=coverage/%p.profraw
+make test LLVM_PROFILE_FILE=$DIR/coverage/%p.profraw
 
 echo "Merging profile data..."
 llvm-profdata merge -sparse coverage/*.profraw -o llvm.profdata
 
 echo "Dumping coverage data..."
 llvm-cov show -instr-profile llvm.profdata libcarl.so > coverage-out/coverage.txt
+llvm-cov show -instr-profile llvm.profdata libcarl-cad.so > coverage-out/coverage-cad.txt
+llvm-cov show -instr-profile llvm.profdata libcarl-settings.so > coverage-out/coverage-settings.txt
 for f in `ls bin/` ; do 
 	llvm-cov show -instr-profile llvm.profdata bin/$f > coverage-out/coverage-$f.txt
 done
