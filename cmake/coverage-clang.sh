7e8035364af8ba7b8407ff0da2d1d96439be511c
Gereon <gereon.kremer@cs.rwth-aachen.de>
2019-05-15 09:40:43 +0200

    Made coverage collection more robust.

--- cmake/coverage-clang.sh
+++ cmake/coverage-clang.sh
@@ -15,8 +15,18 @@ echo "Merging profile data..."
 llvm-profdata merge -sparse coverage/*.profraw -o llvm.profdata
 
 echo "Dumping coverage data..."
-ARG1=`ls libcarl*.so | /usr/bin/grep -v carl-cad | sed 's/^/ -object /' | xargs`
-ARG2=`find bin/ -type f | sed 's/^/ -object /' | xargs`
-eval "llvm-cov show -instr-profile llvm.profdata libcarl.so $ARG1 $ARG2 > coverage.txt"
+
+OBJS=""
+for obj in `ls libcarl*.so` `find bin/ -type f`
+do
+	if llvm-cov show -instr-profile llvm.profdata -object $obj > /dev/null 2> /dev/null
+	then
+		OBJS="$OBJS -object $obj"
+	else
+		echo "No coverage data for $obj"
+	fi
+done
+
+eval "llvm-cov show -instr-profile llvm.profdata libcarl.so $OBJS > coverage.txt"
 eval "llvm-cov show -Xdemangler c++filt -instr-profile llvm.profdata -format html -output-dir coverage-html/ libcarl.so $ARG1 $ARG2"
 
