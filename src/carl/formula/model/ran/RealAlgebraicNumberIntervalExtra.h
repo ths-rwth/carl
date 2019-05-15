52f9ffb585f0005bbfe8c3721d34f574ea28404f
Jasper Nalbach <derjasper@posteo.de>
2019-05-15 17:40:40 +0200

    add RealAlgebraicNumberEvaluation::vanishes

---
+++ src/carl/formula/model/ran/RealAlgebraicNumberIntervalExtra.h
@@ -0,0 +1,78 @@
+#pragma once
+
+
+#include <map>
+#include <vector>
+
+#include "RealAlgebraicNumber.h"
+#include "../../../core/polynomialfunctions/LazardEvaluation.h"
+
+#include "../../../util/SFINAE.h"
+
+namespace carl {
+
+namespace RealAlgebraicNumberEvaluation {
+
+// TODO move somewhere else, integrate into evaluation
+template<typename Coeff, typename Number>
+bool vanishes(
+		const UnivariatePolynomial<Coeff>& poly,
+		const std::map<Variable, RealAlgebraicNumber<Number>>& varToRANMap
+) {
+	CARL_LOG_FUNC("carl.ran", "Check if " << poly << " vanishes under " << varToRANMap);
+	assert(varToRANMap.find(poly.mainVar()) == varToRANMap.end());
+
+	if (carl::isZero(poly)) {
+		CARL_LOG_TRACE("carl.ran", "poly is 0");
+		return true;
+	}
+	if (poly.isNumber()) {
+		CARL_LOG_TRACE("carl.ran", "poly is constant but not zero");
+		return false;
+	}
+
+	UnivariatePolynomial<Coeff> polyCopy(poly);
+	std::map<Variable, RealAlgebraicNumber<Number>> IRmap;
+
+	for (Variable v: polyCopy.gatherVariables()) {
+		if (v == poly.mainVar()) continue;
+		if (varToRANMap.count(v) == 0) {
+			CARL_LOG_TRACE("carl.ran", "poly still contains unassigned variable " << v);
+			return false;
+		}
+		assert(varToRANMap.count(v) > 0);
+		if (varToRANMap.at(v).isNumeric()) {
+			polyCopy.substituteIn(v, Coeff(varToRANMap.at(v).value()));
+		} else {
+			IRmap.emplace(v, varToRANMap.at(v));
+		}
+	}
+	if (carl::isZero(polyCopy)) {
+		CARL_LOG_TRACE("carl.ran", "poly is 0 after substituting rational assignments");
+		return true;
+	}
+	if (IRmap.empty()) {
+		assert(polyCopy.isUnivariate());
+		CARL_LOG_TRACE("carl.ran", "poly is still univariate");
+		return false;
+	} else {
+		CARL_LOG_TRACE("carl.ran", poly << " in " << poly.mainVar() << ", " << varToRANMap);
+		assert(IRmap.find(polyCopy.mainVar()) == IRmap.end());
+
+		LazardEvaluation<Number,MultivariatePolynomial<Number>> le((MultivariatePolynomial<Number>(polyCopy)));
+		for(auto const& [var, val] : IRmap) {
+			CARL_LOG_TRACE("carl.ran", "Substitute " << var << " -> " << val << " into " << le.getLiftingPoly());
+			le.substitute(var, val, false);
+			CARL_LOG_TRACE("carl.ran", "LE got " << le.getLiftingPoly());
+			if (carl::isZero(le.getLiftingPoly())) {
+				CARL_LOG_TRACE("carl.ran", "poly vanishes after plugging in " << var << " -> " << val);
+				return true;
+			}
+		}
+
+		return false;
+	}
+}
+
+}
+}
