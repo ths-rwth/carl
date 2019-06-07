0269483012139f590d5da1b658e53d3589186820
Gereon <gereon.kremer@cs.rwth-aachen.de>
2019-06-07 10:10:37 +0200

    Make OSX 10.14 job mandatory.

--- .ci/travis_generate.py
+++ .ci/travis_generate.py
@@ -45,7 +45,7 @@ jobs = [
 	job("2-macos", ["build", "xcode9.4", "build.sh"]),
 	job("2-macos", ["build", "xcode10", "build.sh"]),
 	job("2-macos", ["build", "xcode10.1", "build.sh"]),
-	job("2-macos", ["build", "xcode10.2", "build.sh", "mayfail"]),
+	job("2-macos", ["build", "xcode10.2", "build.sh"]),
 	job("3-docs", ["build", "linux", "g++-8", "task.documentation", "j1", "build.sh"]),
 	job("4-tidy", ["dependencies", "linux", "clang-9.0", "task.tidy", "build.sh", "mayfail"]),
 	job("4-tidy", ["check", "linux", "clang-9.0", "task.tidy", "build.sh", "mayfail"]),
