e963b07d1d634a56e4feaf1cf686df42f7e30e04
Gereon <gereon.kremer@cs.rwth-aachen.de>
2019-08-19 14:04:15 +0200

    Added new Variable::safe_name().

--- src/carl/core/Variable.cpp
+++ src/carl/core/Variable.cpp
@@ -8,4 +8,22 @@ namespace carl
 	std::string Variable::name() const {
 		return carl::VariablePool::getInstance().getName(*this, true);
 	}
+	std::string Variable::safe_name() const {
+		switch (type()) {
+			case VariableType::VT_BOOL:
+				return "b" + std::to_string(id());
+			case VariableType::VT_REAL:
+				return "r" + std::to_string(id());
+			case VariableType::VT_INT:
+				return "i" + std::to_string(id());
+			case VariableType::VT_UNINTERPRETED:
+				return "u" + std::to_string(id());
+			case VariableType::VT_BITVECTOR:
+				return "bv" + std::to_string(id());
+			default:
+				CARL_LOG_ERROR("carl", "Variable has invalid type: " << type());
+				assert(false);
+				return "";
+		}
+	}
 }
