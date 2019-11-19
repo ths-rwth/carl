2272861f862330bb82eac39b3e422339f74486b3
Gereon <gereon.kremer@cs.rwth-aachen.de>
2019-11-19 15:40:09 +0100

    Added missing include.

--- src/tests/core/Test_CoCoA.cpp
+++ src/tests/core/Test_CoCoA.cpp
@@ -6,6 +6,7 @@
 #include <carl/core/MultivariatePolynomial.h>
 #include <carl/core/polynomialfunctions/CoprimePart.h>
 #include <carl/core/polynomialfunctions/SquareFreePart.h>
+#include <carl/util/Timer.h>
 
 #include <random>
 #include <map>
