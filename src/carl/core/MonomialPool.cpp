33aad8ed5c0db2b5b03a6354281d4bc24d6931f7
Jasper Nalbach <derjasper@posteo.de>
2020-02-10 22:57:41 +0100

    formatting

--- src/carl/core/MonomialPool.cpp
+++ src/carl/core/MonomialPool.cpp
@@ -7,49 +7,46 @@
 
 #include "../io/streamingOperators.h"
 
-namespace carl
-{	
-	Monomial::Arg MonomialPool::add( Monomial::Content&& c, exponent totalDegree) {
-		CARL_LOG_TRACE("carl.core.monomial", c << ", " << totalDegree);
-		
-		MONOMIAL_POOL_LOCK_GUARD
-		
-		underlying_set::insert_commit_data insert_data;
-		auto res = mPool.insert_check(c, content_hash(), content_equal(), insert_data);
-		if (!res.second) {
-			return res.first->mWeakPtr.lock();
-		} else {
-			auto shared = std::shared_ptr<Monomial>(new Monomial(c, totalDegree));
-			shared.get()->mId = mIDs.get();
-			shared.get()->mWeakPtr = shared;
-			mPool.insert_commit(*shared.get(), insert_data);
-			return shared;
-		}
-	}
-	
-	Monomial::Arg MonomialPool::create( Variable _var, exponent _exp )
-	{
-		CARL_LOG_TRACE("carl.core.monomial", _var << ", " << _exp);
-		return add(Monomial::Content(1, std::make_pair(_var, _exp)), _exp);
-	}
+namespace carl {
 
-	Monomial::Arg MonomialPool::create( std::vector<std::pair<Variable, exponent>>&& _exponents, exponent _totalDegree )
-	{
-		CARL_LOG_TRACE("carl.core.monomial", _exponents << ", " << _totalDegree);
-		return add(std::move(_exponents), _totalDegree);
-	}
+Monomial::Arg MonomialPool::add(Monomial::Content&& c, exponent totalDegree) {
+	CARL_LOG_TRACE("carl.core.monomial", c << ", " << totalDegree);
 
-	Monomial::Arg MonomialPool::create( const std::initializer_list<std::pair<Variable, exponent>>& _exponents )
-	{
-		auto exp = Monomial::Content(_exponents);
-		CARL_LOG_TRACE("carl.core.monomial", _exponents);
-		std::sort(exp.begin(), exp.end(), [](const auto& p1, const auto& p2){ return p1.first < p2.first; });
-		return add(std::move(exp));
-	}
+	MONOMIAL_POOL_LOCK_GUARD
 
-	Monomial::Arg MonomialPool::create( std::vector<std::pair<Variable, exponent>>&& _exponents )
-	{
-		CARL_LOG_TRACE("carl.core.monomial", _exponents);
-		return add(std::move(_exponents), 0);
+	underlying_set::insert_commit_data insert_data;
+	auto res = mPool.insert_check(c, content_hash(), content_equal(), insert_data);
+	if (!res.second) {
+		return res.first->mWeakPtr.lock();
+	} else {
+		auto shared = std::shared_ptr<Monomial>(new Monomial(c, totalDegree));
+		shared.get()->mId = mIDs.get();
+		shared.get()->mWeakPtr = shared;
+		mPool.insert_commit(*shared.get(), insert_data);
+		return shared;
 	}
+}
+
+Monomial::Arg MonomialPool::create(Variable _var, exponent _exp) {
+	CARL_LOG_TRACE("carl.core.monomial", _var << ", " << _exp);
+	return add(Monomial::Content(1, std::make_pair(_var, _exp)), _exp);
+}
+
+Monomial::Arg MonomialPool::create(std::vector<std::pair<Variable, exponent>>&& _exponents, exponent _totalDegree) {
+	CARL_LOG_TRACE("carl.core.monomial", _exponents << ", " << _totalDegree);
+	return add(std::move(_exponents), _totalDegree);
+}
+
+Monomial::Arg MonomialPool::create(const std::initializer_list<std::pair<Variable, exponent>>& _exponents) {
+	auto exp = Monomial::Content(_exponents);
+	CARL_LOG_TRACE("carl.core.monomial", _exponents);
+	std::sort(exp.begin(), exp.end(), [](const auto& p1, const auto& p2) { return p1.first < p2.first; });
+	return add(std::move(exp));
+}
+
+Monomial::Arg MonomialPool::create(std::vector<std::pair<Variable, exponent>>&& _exponents) {
+	CARL_LOG_TRACE("carl.core.monomial", _exponents);
+	return add(std::move(_exponents), 0);
+}
+
 } // end namespace carl
--- src/carl/core/MonomialPool.h
+++ src/carl/core/MonomialPool.h
@@ -11,150 +11,148 @@
 #include "Monomial.h"
 #include "config.h"
 
-#include <memory>
 #include <boost/intrusive/unordered_set.hpp>
+#include <memory>
 
-namespace carl{
+namespace carl {
 
-	inline std::size_t hash_value(const carl::Monomial& monomial) {
-		return monomial.hash();
-	}
+inline std::size_t hash_value(const carl::Monomial& monomial) {
+	return monomial.hash();
+}
 
-	class MonomialPool : public Singleton<MonomialPool>
-	{
-		friend class Singleton<MonomialPool>;
-		friend std::ostream& operator<<(std::ostream& os, const MonomialPool& mp);
+class MonomialPool : public Singleton<MonomialPool> {
+	friend class Singleton<MonomialPool>;
+	friend std::ostream& operator<<(std::ostream& os, const MonomialPool& mp);
 
-		struct content_equal {
-			bool operator()(const Monomial::Content& content, const Monomial& monomial) const { 
-				return content == monomial.mExponents;
-			}
-
-			bool operator()(const Monomial& monomial, const Monomial::Content& content) const {
-				return content == monomial.mExponents;
-			}
-		};
-
-		struct content_hash {
-			std::size_t operator()(const Monomial::Content& content) const {
-				return Monomial::hashContent(content);
-			}
-		};
-
-		private:
-			// Members:
-			/// id allocator
-			IDPool mIDs;
-			//size_t mIdAllocator;
-			/// The pool.
-			using underlying_set = boost::intrusive::unordered_set<Monomial>; 
-			std::unique_ptr<underlying_set::bucket_type[]> mPoolBuckets;
-			underlying_set mPool;
-			/// Mutex to avoid multiple access to the pool
-			mutable std::recursive_mutex mMutex;
-			
-            #ifdef THREAD_SAFE
-			#define MONOMIAL_POOL_LOCK_GUARD std::lock_guard<std::recursive_mutex> lock( mMutex );
-			#define MONOMIAL_POOL_LOCK mMutex.lock();
-			#define MONOMIAL_POOL_UNLOCK mMutex.unlock();
-            #else
-			#define MONOMIAL_POOL_LOCK_GUARD
-			#define MONOMIAL_POOL_LOCK
-			#define MONOMIAL_POOL_UNLOCK
-            #endif
-			
-		protected:
-			/**
-			 * Constructor of the pool.
-			 * @param _capacity Expected necessary capacity of the pool.
-			 */
-			explicit MonomialPool( std::size_t _capacity = 10000 ):
-				mPoolBuckets(new underlying_set::bucket_type[_capacity]),
-				mPool(underlying_set::bucket_traits(mPoolBuckets.get(), _capacity))
-			{
-				mIDs.get();
-				assert(mIDs.largestID() == 0);
-				VariablePool::getInstance();
-				CARL_LOG_DEBUG("carl.pool", "Monomialpool constructed");
-			}
-			
-			~MonomialPool() {
-				// for (const auto& m : mPool) m.mId = 0; // hacky fix for singletons not be destroyed in proper order
-				// CARL_LOG_DEBUG("carl.pool", "Monomialpool destructed");
-			}
-
-			Monomial::Arg add( Monomial::Content&& c, exponent totalDegree = 0 );
-
-		public:
-			/**
-			 * Creates a monomial from a variable and an exponent.
-			 */
-			Monomial::Arg create( Variable _var, exponent _exp );
-
-			/**
-			 * Creates a monomial from a variable and an exponent.
-			 */
-			template<typename Number>
-			Monomial::Arg create( Variable _var, Number&& _exp ) {
-				return create(_var, static_cast<exponent>(std::forward<Number>(_exp)));
-			}
-			
-			/**
-			 * Creates a monomial from a list of variables and their exponents.
-			 * 
-			 * Note that the input is required to be sorted.
-			 * 
-			 * @param _exponents Sorted list of variables and exponents.
-			 * @param _totalDegree Total degree.
-			 */
-			Monomial::Arg create( std::vector<std::pair<Variable, exponent>>&& _exponents, exponent _totalDegree );
-			
-			/**
-			 * Creates a Monomial.
-			 * 
-			 * @param _exponents Possibly unsorted list of variables and epxonents.
-			 */
-			Monomial::Arg create( const std::initializer_list<std::pair<Variable, exponent>>& _exponents );
-			
-			/**
-			 * Creates a monomial from a list of variables and their exponents.
-			 * 
-			 * Note that the input is required to be sorted.
-			 * 
-			 * @param Sorted list of variables and exponents.
-			 */
-			Monomial::Arg create( std::vector<std::pair<Variable, exponent>>&& _exponents );
-
-			void free(const Monomial* m) {
-				if (m == nullptr) return;
-				if (m->id() == 0) return;
-				CARL_LOG_TRACE("carl.core.monomial", "Freeing " << m);
-				MONOMIAL_POOL_LOCK_GUARD;
-				auto it = mPool.find(*m);
-				if (it != mPool.end()) {
-					CARL_LOG_TRACE("carl.core.monomial", "Found " << m->id());
-					mIDs.free(m->id());
-					mPool.erase(it);
-				} else {
-					CARL_LOG_TRACE("carl.core.monomial", "Not found in pool.");
-				}
-			}
-
-			std::size_t size() const {
-				return mPool.size();
-			}
-			std::size_t largestID() const {
-				return mIDs.largestID();
-			}
-	};
-	
-	inline std::ostream& operator<<(std::ostream& os, const MonomialPool& mp) {
-		os << "MonomialPool of size " << mp.size() << std::endl;
-		for (const auto& entry: mp.mPool) {
-			os << "\t" << entry << std::endl;
+	struct content_equal {
+		bool operator()(const Monomial::Content& content, const Monomial& monomial) const {
+			return content == monomial.mExponents;
 		}
-		return os;
+
+		bool operator()(const Monomial& monomial, const Monomial::Content& content) const {
+			return content == monomial.mExponents;
+		}
+	};
+
+	struct content_hash {
+		std::size_t operator()(const Monomial::Content& content) const {
+			return Monomial::hashContent(content);
+		}
+	};
+
+private:
+	// Members:
+	/// id allocator
+	IDPool mIDs;
+	//size_t mIdAllocator;
+	/// The pool.
+	using underlying_set = boost::intrusive::unordered_set<Monomial>;
+	std::unique_ptr<underlying_set::bucket_type[]> mPoolBuckets;
+	underlying_set mPool;
+	/// Mutex to avoid multiple access to the pool
+	mutable std::recursive_mutex mMutex;
+
+#ifdef THREAD_SAFE
+#define MONOMIAL_POOL_LOCK_GUARD std::lock_guard<std::recursive_mutex> lock(mMutex);
+#define MONOMIAL_POOL_LOCK mMutex.lock();
+#define MONOMIAL_POOL_UNLOCK mMutex.unlock();
+#else
+#define MONOMIAL_POOL_LOCK_GUARD
+#define MONOMIAL_POOL_LOCK
+#define MONOMIAL_POOL_UNLOCK
+#endif
+
+protected:
+	/**
+	 * Constructor of the pool.
+	 * @param _capacity Expected necessary capacity of the pool.
+	 */
+	explicit MonomialPool(std::size_t _capacity = 10000)
+		: mPoolBuckets(new underlying_set::bucket_type[_capacity]),
+		  mPool(underlying_set::bucket_traits(mPoolBuckets.get(), _capacity)) {
+		mIDs.get();
+		assert(mIDs.largestID() == 0);
+		VariablePool::getInstance();
+		CARL_LOG_DEBUG("carl.pool", "Monomialpool constructed");
 	}
+
+	~MonomialPool() {
+		// for (const auto& m : mPool) m.mId = 0; // hacky fix for singletons not be destroyed in proper order
+		// CARL_LOG_DEBUG("carl.pool", "Monomialpool destructed");
+	}
+
+	Monomial::Arg add(Monomial::Content&& c, exponent totalDegree = 0);
+
+public:
+	/**
+	 * Creates a monomial from a variable and an exponent.
+	 */
+	Monomial::Arg create(Variable _var, exponent _exp);
+
+	/**
+	 * Creates a monomial from a variable and an exponent.
+	 */
+	template<typename Number>
+	Monomial::Arg create(Variable _var, Number&& _exp) {
+		return create(_var, static_cast<exponent>(std::forward<Number>(_exp)));
+	}
+
+	/**
+	 * Creates a monomial from a list of variables and their exponents.
+	 * 
+	 * Note that the input is required to be sorted.
+	 * 
+	 * @param _exponents Sorted list of variables and exponents.
+	 * @param _totalDegree Total degree.
+	 */
+	Monomial::Arg create(std::vector<std::pair<Variable, exponent>>&& _exponents, exponent _totalDegree);
+
+	/**
+	 * Creates a Monomial.
+	 * 
+	 * @param _exponents Possibly unsorted list of variables and epxonents.
+	 */
+	Monomial::Arg create(const std::initializer_list<std::pair<Variable, exponent>>& _exponents);
+
+	/**
+	 * Creates a monomial from a list of variables and their exponents.
+	 * 
+	 * Note that the input is required to be sorted.
+	 * 
+	 * @param Sorted list of variables and exponents.
+	 */
+	Monomial::Arg create(std::vector<std::pair<Variable, exponent>>&& _exponents);
+
+	void free(const Monomial* m) {
+		if (m == nullptr) return;
+		if (m->id() == 0) return;
+		CARL_LOG_TRACE("carl.core.monomial", "Freeing " << m);
+		MONOMIAL_POOL_LOCK_GUARD;
+		auto it = mPool.find(*m);
+		if (it != mPool.end()) {
+			CARL_LOG_TRACE("carl.core.monomial", "Found " << m->id());
+			mIDs.free(m->id());
+			mPool.erase(it);
+		} else {
+			CARL_LOG_TRACE("carl.core.monomial", "Not found in pool.");
+		}
+	}
+
+	std::size_t size() const {
+		return mPool.size();
+	}
+	std::size_t largestID() const {
+		return mIDs.largestID();
+	}
+};
+
+inline std::ostream& operator<<(std::ostream& os, const MonomialPool& mp) {
+	os << "MonomialPool of size " << mp.size() << std::endl;
+	for (const auto& entry : mp.mPool) {
+		os << "\t" << entry << std::endl;
+	}
+	return os;
+}
 } // end namespace carl
 
 namespace carl {
@@ -164,4 +162,4 @@ inline Monomial::Arg createMonomial(T&&... t) {
 	return MonomialPool::getInstance().create(std::forward<T>(t)...);
 }
 
-}
+} // namespace carl
