2789c7c2b9d0494f96e16c3d07b6b1c3c89bb24d
philippe-specht <philippe-specht@rwth-aachen.de>
2020-12-10 16:28:44 +0100

    Fix test EvaluationAgainstIsolation

--- src/tests/core/Test_EvaluationAgainstIsolation.cpp
+++ src/tests/core/Test_EvaluationAgainstIsolation.cpp
@@ -4,7 +4,6 @@
 #include <carl/core/UnivariatePolynomial.h>
 #include <carl/formula/model/ran/RealAlgebraicNumber.h>
 #include <carl/formula/model/ran/real_roots.h>
-//#include "carl/formula/model/ran/interval/ran_interval_evaluation.h"
 
 #include "../Common.h"
 
@@ -18,19 +17,20 @@ TEST(IsolationAndEvaluation, Comparison) {
 	Variable c = freshRealVariable("c");
 	Variable d = freshRealVariable("d");
 
-	MultivariatePolynomial<Rational> poly(Rational(-11) + Rational(49) * b + Rational(10) * d * a + carl::pow(c, 2) + Rational(-49) * b * c);
+	MultivariatePolynomial<Rational> poly(Rational(-11) + Rational(49) * b + Rational(10) * d * a + carl::pow(c, 2) + Rational(-49) * c * b);
 
-	Rational a_value = Rational(0);
+	Rational d_value = Rational(0);
 
 	Interval<Rational> i1(Rational("-4248605910149717961169920635929051829594223322247294470949631829") / Rational("1102359498361667329021785995346037545330231253734995885016743936"), BoundType::STRICT, -Rational("-3717530171381003216023680556437920350894945406966382662080927793") / Rational("964564561066458912894062745927782852163952347018121399389650944"), BoundType::STRICT);
 	UnivariatePolynomial<Rational> p1(freshRealVariable("__r1"), std::initializer_list<Rational>{-11, 1, 1});
-	RealAlgebraicNumber<Rational> b_value = RealAlgebraicNumber<Rational>::create_safe(p1, i1);
+	RealAlgebraicNumber<Rational> c_value = RealAlgebraicNumber<Rational>::create_safe(p1, i1);
 
 	Interval<Rational> i2(Rational("-1245") / Rational("76832"), BoundType::STRICT, -Rational("-22409") / Rational("1382976"), BoundType::STRICT);
 	UnivariatePolynomial<Rational> p2(freshRealVariable("__r2"), std::initializer_list<Rational>{-11, -1029, -21609});
-	RealAlgebraicNumber<Rational> c_value = RealAlgebraicNumber<Rational>::create_safe(p2, i2);
+	RealAlgebraicNumber<Rational> b_value = RealAlgebraicNumber<Rational>::create_safe(p2, i2);
 
-	Rational d_value = Rational(-1);
+	Rational a_value = Rational(-1);
+
 
 	std::vector<Variable> variableOrder{d, c, b, a};
 	std::map<carl::Variable, RAN> varToRANMap;
@@ -52,5 +52,7 @@ TEST(IsolationAndEvaluation, Comparison) {
 		}
 	}
 
+	//EXPECT_TRUE(b1);
+	//EXPECT_TRUE(b2);
 	EXPECT_EQ(b1, b2);
 }
