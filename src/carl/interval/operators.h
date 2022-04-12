80d8e7547451b07edf4603e8eca3f2100ce5a28c
Stefan Schupp <stefan.schupp@tuwien.ac.at>
2022-04-12 11:34:19 +0200

    fix bug in mul-assign
    Co-Authored-By: Laszlo Antal <antal@informatik.rwth-aachen.de>

--- src/carl/interval/operators.h
+++ src/carl/interval/operators.h
@@ -436,7 +436,8 @@ inline Interval<Number>& operator*=(Interval<Number>& lhs, const Interval<Number
 template<typename Number>
 inline Interval<Number>& operator*=(Interval<Number>& lhs, const Number& rhs) {
 	if (carl::isZero(rhs) && !lhs.isEmpty()) {
-		return Interval<Number>{0};
+		lhs = Interval<Number>{0};
+		return lhs;
 	}
 	lhs.rContent() *= rhs;
 	return lhs;
