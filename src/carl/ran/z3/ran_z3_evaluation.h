508fb825df5c6159760c2ff200aa3ba4e5c0926e
Jasper Nalbach <nalbach@cs.rwth-aachen.de>
2022-04-25 16:04:32 +0200

    remove ran::interval::vanishes

--- src/carl/ran/z3/ran_z3_evaluation.h
+++ src/carl/ran/z3/ran_z3_evaluation.h
@@ -14,18 +14,6 @@ std::optional<real_algebraic_number_z3<Number>> evaluate(const MultivariatePolyn
 template<typename Number, typename Poly>
 boost::tribool evaluate(const Constraint<Poly>& constraint, const std::map<Variable, real_algebraic_number_z3<Number>>& evalMap);
 
-// TODO workaround, implement for z3
-namespace ran::interval {
-template<typename Coeff, typename Number>
-bool vanishes(
-		const UnivariatePolynomial<Coeff>& poly,
-		const std::map<Variable, real_algebraic_number_z3<Number>>& varToRANMap
-) {
-    assert(false && "Not implemented");
-	return false;
-}
-}
-
 }
 
 #endif
--- src/tests/core/Test_EvaluationAgainstIsolation.cpp
+++ src/tests/core/Test_EvaluationAgainstIsolation.cpp
@@ -56,7 +56,7 @@ TEST(IsolationAndEvaluation, Comparison) {
 
 	varToRANMap.erase(a);
 
-	bool is_nullified = carl::ran::interval::vanishes(carl::to_univariate_polynomial(poly, a), varToRANMap);
+	bool is_nullified = carl::ran::real_roots(carl::to_univariate_polynomial(poly, a), varToRANMap).is_nullified();
 	EXPECT_TRUE(is_nullified);
 
 	/*
