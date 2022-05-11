7e38eff4a36238fbe3cf77b890cc3ff09e422e90
Jasper Nalbach <nalbach@cs.rwth-aachen.de>
2022-05-11 15:48:37 +0200

    remove stale todo

--- src/carl-formula/formula/Formula.h
+++ src/carl-formula/formula/Formula.h
@@ -163,7 +163,6 @@ namespace carl
                 Formula( FormulaPool<Pol>::getInstance().create( _type, std::move(Formulas<Pol>(_subasts.begin(), _subasts.end()) ) ))
             {}
 
-            // TODO: Does the following constructor anything more efficient than the one before?
             explicit Formula( FormulaType _type, FormulaSet<Pol>&& _subasts ):
                 Formula( FormulaPool<Pol>::getInstance().create( _type, std::move(Formulas<Pol>(_subasts.begin(), _subasts.end()) ) ))
             {}
