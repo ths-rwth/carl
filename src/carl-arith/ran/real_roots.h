e7934b815826cd4e6fb0318ed0c33bf98028ec6f
Jasper Nalbach <nalbach@cs.rwth-aachen.de>
2022-06-30 15:13:01 +0200

    fix build

--- src/carl-arith/ran/real_roots.h
+++ src/carl-arith/ran/real_roots.h
@@ -17,16 +17,16 @@ namespace carl::ran {
 namespace internal {
 template<typename Poly, typename Number>
 struct real_roots_internal {
-    static real_roots_result<typename Poly::RootType<Number>> real_roots(const Poly& polynomial, const Interval<Number>& interval) {
+    static real_roots_result<typename Poly::template RootType<Number>> real_roots(const Poly& polynomial, const Interval<Number>& interval) {
         CARL_LOG_FATAL("carl.ran", "real_roots not implemented for type: " << typeid(Poly).name() << " " << polynomial << " " << interval);
         assert(false);
-        return real_roots_result<typename Poly::RootType<Number>>::no_roots_response();
+        return real_roots_result<typename Poly::template RootType<Number>>::no_roots_response();
     }
 
-    static real_roots_result<typename Poly::RootType<Number>> real_roots(const Poly& polynomial, const std::map<Variable, typename Poly::RootType<Number>>& assignment, const Interval<Number>& interval) {
+    static real_roots_result<typename Poly::template RootType<Number>> real_roots(const Poly& polynomial, const std::map<Variable, typename Poly::template RootType<Number>>& assignment, const Interval<Number>& interval) {
         CARL_LOG_FATAL("carl.ran", "real_roots not implemented for type: " << typeid(Poly).name() << " " << polynomial << " " << assignment << " " << interval);
         assert(false);
-        return real_roots_result<typename Poly::RootType<Number>>::no_roots_response();
+        return real_roots_result<typename Poly::template RootType<Number>>::no_roots_response();
     }
 };
 
@@ -47,12 +47,12 @@ struct real_roots_internal<LPPolynomial, Number> {
 
 template<typename Number, typename Coeff>
 struct real_roots_internal<UnivariatePolynomial<Coeff>, Number> {
-    static real_roots_result<typename UnivariatePolynomial<Coeff>::RootType<Number>> real_roots(const UnivariatePolynomial<Coeff>& polynomial, const Interval<Number>& interval) {
+    static real_roots_result<typename UnivariatePolynomial<Coeff>::template RootType<Number>> real_roots(const UnivariatePolynomial<Coeff>& polynomial, const Interval<Number>& interval) {
         CARL_LOG_DEBUG("carl.ran", "Called real_roots for UnivariatePolynomial: " << polynomial);
         return carl::ran::interval::real_roots_interval(polynomial, interval);
     }
 
-    static real_roots_result<typename UnivariatePolynomial<Coeff>::RootType<Number>> real_roots(const UnivariatePolynomial<Coeff>& polynomial, const std::map<Variable, typename UnivariatePolynomial<Coeff>::RootType<Number>>& assignment, const Interval<Number>& interval) {
+    static real_roots_result<typename UnivariatePolynomial<Coeff>::template RootType<Number>> real_roots(const UnivariatePolynomial<Coeff>& polynomial, const std::map<Variable, typename UnivariatePolynomial<Coeff>::template RootType<Number>>& assignment, const Interval<Number>& interval) {
         CARL_LOG_DEBUG("carl.ran", "Called real_roots for UnivariatePolynomial: " << polynomial << " " << assignment);
         return carl::ran::interval::real_roots_interval(polynomial, assignment, interval);
     }
@@ -60,12 +60,12 @@ struct real_roots_internal<UnivariatePolynomial<Coeff>, Number> {
 } // namespace internal
 
 template<typename Poly, typename Number = typename UnderlyingNumberType<typename Poly::CoeffType>::type, typename... Coeff>
-real_roots_result<typename Poly::RootType<Number>> real_roots(const Poly& polynomial, const std::map<Variable, typename Poly::RootType<Number>>& assignment, const Interval<Number>& interval = Interval<Number>::unbounded_interval()) {
+real_roots_result<typename Poly::template RootType<Number>> real_roots(const Poly& polynomial, const std::map<Variable, typename Poly::template RootType<Number>>& assignment, const Interval<Number>& interval = Interval<Number>::unbounded_interval()) {
     return internal::real_roots_internal<Poly, Number>::real_roots(polynomial, assignment, interval);
 }
 
 template<typename Poly, typename Number = typename UnderlyingNumberType<typename Poly::CoeffType>::type, typename... Coeff>
-real_roots_result<typename Poly::RootType<Number>> real_roots(const Poly& polynomial, const Interval<Number>& interval = Interval<Number>::unbounded_interval()) {
+real_roots_result<typename Poly::template RootType<Number>> real_roots(const Poly& polynomial, const Interval<Number>& interval = Interval<Number>::unbounded_interval()) {
     return internal::real_roots_internal<Poly, Number>::real_roots(polynomial, interval);
 }
 } // namespace carl::ran
