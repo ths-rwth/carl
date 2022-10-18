69f292f2670f15a639896f56f4f3569550301572
Jasper Nalbach <nalbach@cs.rwth-aachen.de>
2022-10-18 17:01:43 +0200

    complete NNF implementation

--- src/carl-formula/formula/functions/NNF.h
+++ src/carl-formula/formula/functions/NNF.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Negations.h"
+#include "aux.h"
 
 namespace carl {
 
@@ -19,6 +20,34 @@ Formula<Poly> to_nnf(const Formula<Poly>& formula) {
     switch(formula.type()){
         case carl::FormulaType::NOT:
             return to_nnf(resolve_negation(formula, false));
+        case carl::FormulaType::IMPLIES: {
+            auto premise = to_nnf(formula.premise().negated());
+            auto conclusion = to_nnf(formula.conclusion());
+            return Formula<Poly>(carl::FormulaType::OR, Formulas<Poly>{ premise, conclusion});
+        }
+        case carl::FormulaType::IFF: {
+            Formulas<Poly> poss;
+            Formulas<Poly> negs;
+            for (const auto& f : formula.subformulas()) {
+                poss.emplace_back(to_nnf(f));
+                negs.emplace_back(to_nnf(f.negated()));
+            }
+            Formula<Poly> pos(carl::FormulaType::AND, std::move(poss));
+            Formula<Poly> neg(carl::FormulaType::AND, std::move(negs));
+            return Formula<Poly>(carl::FormulaType::OR, Formulas<Poly>({ pos, neg }));
+        }
+        case carl::FormulaType::XOR: {
+            auto lhs = formula::aux::connectPrecedingSubformulas(formula);
+            const auto& rhs = formula.subformulas().back();
+            Formula<Poly> pos(FormulaType::OR, { to_nnf(lhs), to_nnf(rhs) });
+            Formula<Poly> neg(FormulaType::OR, { to_nnf(!lhs), to_nnf(!rhs) });
+            return Formula<Poly>(carl::FormulaType::AND, Formulas<Poly>({ pos, neg }));
+        }
+        case carl::FormulaType::ITE: {
+			Formula<Poly> first(FormulaType::OR, {to_nnf(!formula.condition()), to_nnf(formula.first_case())});
+			Formula<Poly> second(FormulaType::OR, {to_nnf(formula.condition()), to_nnf(formula.second_case())});
+            return Formula<Poly>(carl::FormulaType::AND, Formulas<Poly>({ first, second }));
+        }
         case carl::FormulaType::OR:{
             Formulas<Poly> disjunctions;
             for(auto subformula : formula.subformulas()){
