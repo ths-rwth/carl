243a9b271356a18467a5ac2b969976f2813ef404
Jasper Nalbach <nalbach@cs.rwth-aachen.de>
2022-12-20 15:46:50 +0100

    update eigen

--- resources/eigen3.cmake
+++ resources/eigen3.cmake
@@ -1,11 +1,11 @@
 ExternalProject_Add(
     Eigen3-EP
-	GIT_REPOSITORY "https://github.com/eigenteam/eigen-git-mirror.git"
+	GIT_REPOSITORY "https://gitlab.com/libeigen/eigen.git"
 	GIT_TAG ${EIGEN3_VERSION}
 	DOWNLOAD_NO_PROGRESS 1
 	UPDATE_COMMAND "" # due to https://gitlab.kitware.com/cmake/cmake/issues/17229
-	PATCH_COMMAND git cherry-pick eb592735746d935612caf658ab800e929c4ca6f4
-	COMMAND git cherry-pick f65188960c52da20392d88296cbaeb341d635834
+	#PATCH_COMMAND git cherry-pick eb592735746d935612caf658ab800e929c4ca6f4
+	#COMMAND git cherry-pick f65188960c52da20392d88296cbaeb341d635834
 	CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR> -DPKGCONFIG_INSTALL_DIR=<INSTALL_DIR>/lib/pkgconfig
 	LOG_INSTALL 1
 )
