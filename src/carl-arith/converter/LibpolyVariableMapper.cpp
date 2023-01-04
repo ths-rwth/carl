6432d6422cecc61d68db92eb012c223f4c5aca0a
Jasper Nalbach <nalbach@cs.rwth-aachen.de>
2023-01-04 10:38:36 +0100

    more debug output

--- src/carl-arith/converter/LibpolyVariableMapper.cpp
+++ src/carl-arith/converter/LibpolyVariableMapper.cpp
@@ -13,6 +13,7 @@ carl::Variable VariableMapper::getCarlVariable(const poly::Variable& var) {
 	CARL_LOG_DEBUG("carl.converter", "Converting Libpoly Var: " << var);
 	auto it = mVarsLibolyCarl.find(var);
 	assert(it != mVarsLibolyCarl.end()); // Can only convert already known variable back
+	CARL_LOG_TRACE("carl.converter", "-> " << it->second);
 	return it->second;
 }
 poly::Variable VariableMapper::getLibpolyVariable(const carl::Variable& var) {
@@ -24,6 +25,7 @@ poly::Variable VariableMapper::getLibpolyVariable(const carl::Variable& var) {
 		it = mVarsCarlLibpoly.emplace(var, poly::Variable(var.name().c_str())).first;
 		mVarsLibolyCarl.emplace(it->second, var);
 	}
+	CARL_LOG_TRACE("carl.converter", "-> " << it->second);
 	return it->second;
 }
 } // namespace carl
