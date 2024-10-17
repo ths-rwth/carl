0bdd21693d2b864dd1b97084e61121997c17dfc7
Valentin Promies <valentin.promies@rwth-aachen.de>
2024-10-17 12:20:13 +0200

    allow local cocoa build

--- cmake/FindCoCoA.cmake
+++ cmake/FindCoCoA.cmake
@@ -35,6 +35,37 @@ if(COCOA_INCLUDE_DIR AND COCOA_SHARED AND COCOA_STATIC)
 	endif()
 endif()
 
+if(COCOA_LOCAL_DIR)
+    get_target_property(GMP_LIB GMP_STATIC IMPORTED_LOCATION)
+    set(GMP_LIB_ARG "--with-libgmp=${GMP_LIB}")
+
+    ExternalProject_Add(
+        CoCoALib-EP
+        SOURCE_DIR ${COCOA_LOCAL_DIR}
+        BUILD_IN_SOURCE YES
+        PATCH_COMMAND sh ${CMAKE_SOURCE_DIR}/cmake/patches/cocoa_patch_print_errors.sh
+        COMMAND patch <SOURCE_DIR>/configuration/shell-fns.sh ${CMAKE_SOURCE_DIR}/cmake/patches/cocoa_shell-fns.sh.patch
+        CONFIGURE_COMMAND ./configure --prefix=<INSTALL_DIR> --threadsafe-hack ${GMP_LIB_ARG}
+        BUILD_COMMAND make library
+        INSTALL_COMMAND ${CMAKE_COMMAND} -E touch <SOURCE_DIR>/examples/index.html
+        COMMAND ${CMAKE_COMMAND} -E make_directory <INSTALL_DIR>/include
+        COMMAND ${CMAKE_COMMAND} -E make_directory <INSTALL_DIR>/lib
+        COMMAND ${CMAKE_COMMAND} -E remove_directory <INSTALL_DIR>/include/CoCoA-${COCOA_VERSION}
+        COMMAND ${CMAKE_MAKE_PROGRAM} install
+        BUILD_BYPRODUCTS ${CMAKE_BINARY_DIR}/resources/src/CoCoALib-EP/lib/libcocoa${STATIC_EXT}
+    )
+
+    ExternalProject_Get_Property(CoCoALib-EP INSTALL_DIR)
+
+    add_imported_library(COCOA SHARED "${INSTALL_DIR}/lib/libcocoa${STATIC_EXT}" "${INSTALL_DIR}/include")
+    add_imported_library(COCOA STATIC "${INSTALL_DIR}/lib/libcocoa${STATIC_EXT}" "${INSTALL_DIR}/include")
+
+    add_dependencies(CoCoALib-EP GMP_STATIC)
+    add_dependencies(COCOA_SHARED CoCoALib-EP GMPXX_SHARED)
+    add_dependencies(COCOA_STATIC CoCoALib-EP GMP_STATIC)
+    set(COCOA_FOUND_SYSTEM TRUE)
+endif()
+
 if(NOT COCOA_FOUND_SYSTEM)
 	get_target_property(GMP_LIB GMP_STATIC IMPORTED_LOCATION)
 	set(GMP_LIB_ARG "--with-libgmp=${GMP_LIB}")
